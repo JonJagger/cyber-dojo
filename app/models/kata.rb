ebb6c7e407358bfb44508e7e1b8bafdca1610716
Jon Jagger <jrbjagger@users.sourceforge.net>
2010-03-27 22:11:12 +0000

    see_all_increments view now shows increments in time-order

--- app/models/kata.rb
+++ app/models/kata.rb
@@ -174,10 +174,12 @@ private
   def parse_ruby_test_unit(output)
     ruby_pattern = Regexp.new('^(\d*) tests, (\d*) assertions, (\d*) failures, (\d*) errors')
     if match = ruby_pattern.match(output)
-      if match[3] == "0" 
-        :passed
-      else
+      if match[4] != "0"
+        :error
+      elsif match[3] != "0"
         :failed
+      else
+        :passed
       end
     else
       :error
@@ -221,6 +223,79 @@ private
 
 end
 
+#=================================================================
+
+class MockAvatar
+  def initialize(name,incs)
+    @name,@incs = name,incs
+  end
+
+  def name
+    @name
+  end
+
+  def increments
+    @incs
+  end
+
+  def to_s
+    inspect
+  end
+
+end
+
+class MockKata
+  def initialize(avatars)
+    @avatars = avatars
+  end
+
+  def avatars
+    @avatars
+  end
+end
+
+def gapper(all)
+
+  avs = []
+  all.each do |avatar|
+    avs << MockAvatar.new(avatar.name, avatar.increments)
+  end
+  mc = MockKata.new(avs)
+
+  # record avatar for each increment to reform avatar groups later
+  mc.avatars.each do |avatar|
+    avatar.increments.each {|inc| inc[:avatar] = avatar.name }
+  end
+
+  # merge all increments across avatars
+  flat = []
+  mc.avatars.each {|avatar| avatar.increments.each { |inc| flat << inc } }
+  
+  # sort based on :time
+  flat.sort! {|lhs,rhs| Time.utc(*lhs[:time]) <=> Time.utc(*rhs[:time]) }
+
+  # record ordinal value across all avatars
+  flat.each_with_index {|h,n| h[:ordinal] = n }
+
+  # regroup by :avatar
+  flat = flat.group_by {|inc| inc[:avatar] }
+
+  # calculate ordinal gaps
+  flat.each do |avatar,increments|
+    prev = -1
+    increments.each do |inc|
+      inc[:gap] = inc[:ordinal] - prev - 1
+      prev = inc[:ordinal]
+    end
+  end
+
+  result = []
+  flat.each do |avatar,increments|
+    result << MockAvatar.new(avatar,increments)
+  end
+  result
+
+end
 
 
 
