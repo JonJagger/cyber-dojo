bb06a4364548b6024c01cea08ca7788b0c197584
Jon Jagger <jon@jaggersoft.com>
2010-07-05 19:23:32 +0100

    tidied up C# initial fileset; added readme files on how to install Java and C#

--- languages/C#/manifest.rb
+++ languages/C#/manifest.rb
@@ -3,8 +3,6 @@
   :visible_filenames => %w( Untitled.cs UntitledTest.cs cyberdojo.sh ),
 
   :hidden_filenames => %w(
-    nunit-console.exe
-    nunit-console-runner.dll
     nunit.core.dll
     nunit.core.interfaces.dll
     nunit.framework.dll
---
+++ languages/C#/readme.txt
@@ -0,0 +1,4 @@
+I install mono on Ubuntu as follows
+
+>sudo apt-get install gmcs
+>sudo apt-get install nunit-console
