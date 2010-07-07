6c8f7ba4eb820f89d74ae30480ef36c81bd32cc1
Jon Jagger <jon@jaggersoft.com>
2010-07-07 17:28:31 +0100

    create/enter dojo now driven from home page

--- app/controllers/dojo_controller.rb
+++ app/controllers/dojo_controller.rb
@@ -2,11 +2,44 @@
 class DojoController < ApplicationController
 
   def index
-    @dojo_names = Dojo.names
-	if @dojo_names.size == 1
-	  redirect_to :controller => 'kata',
-                  :dojo => @dojo_names[0]
-	end
+    # offers new/enter/dashboard choice
+  end
+
+  def new
+    name = params[:name]
+    folder = RAILS_ROOT + '/dojos/' + name
+    if !File.exists?(folder)
+      Dir.mkdir(folder)
+    end
+    redirect_to :controller => 'kata', 
+                :action => 'index',
+                :dojo => name
+  rescue
+    flash[:new_notice] = 'Illegal dojo name: ' + name
+    redirect_to :action => 'index'
+  end
+
+  def enter
+    name = params[:name]
+    if !File.exists?(RAILS_ROOT + '/dojos/' + name)
+      flash[:enter_notice] = 'There is no dojo named: ' + name
+      redirect_to :action => 'index'
+    else
+      redirect_to :controller => 'kata', 
+                  :action => 'index',
+                  :dojo => name
+    end
+  end
+
+  def dashboard
+    name = params[:name]
+    if !File.exists?(RAILS_ROOT + '/dojos/' + name)
+      flash[:dashboard_notice] = 'There is no dojo named: ' + name
+      redirect_to :action => 'index'
+    else
+      @dojo = Dojo.new(params[:name])
+      render :layout => 'dashboard_view'
+    end
   end
 
 end
