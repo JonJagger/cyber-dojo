ecb5f1a4f23e9f8e18ac5b59fc37e0cd9a0eefe7
Jon Jagger <jon@jaggersoft.com>
2010-10-25 12:07:42 +0100

    start of refactoring to support IE

--- public/javascripts/cyberdojo_run_tests.js
+++ public/javascripts/cyberdojo_run_tests.js
@@ -3,21 +3,31 @@ var isControl = false;
 
 document.onkeyup = function(e) 
 {
-	if (e.which == 17) 
+	if (keyCode(e) == 17) 
 		isControl = false;
 }
 
 document.onkeydown = function(e)
 {
-	if (e.which == 17) 
+	if (keyCode(e) == 17) 
 		isControl = true;
 	
-	if (e.which == 83 && isControl == true) 
+	if (keyCode(e) == 83 && isControl == true) 
 	{
 		//run code for CTRL+S -- ie, save!
 		alert("There is no Save! Press the play button instead");
 		return false;
 	}
+}
+
+function keyCode(e)
+{
+	var code;
+	if (e.keyCode)
+		code = e.keyCode;
+	else if (e.which)
+		code = e.which;
+	return code;
 }
 
 function runTests()
