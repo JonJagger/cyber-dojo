595ad17bb57b2d196e13282894cecbf87ed1546e
Jon Jagger <jon@jaggersoft.com>
2010-11-28 13:19:01 +0000

    trying to get git to ignore dojos folder

--- dojos/index.rb
+++ dojos/index.rb
@@ -1,1 +1,1 @@
-[{:name=>"Jon Jagger", :created=>[2010, 11, 26, 11, 49, 40]}, {:name=>"Jon Jagger2", :created=>[2010, 11, 28, 12, 8, 27]}, {:name=>"Jon Jagger3", :created=>[2010, 11, 28, 12, 50, 14]}]
+[{:name=>"Jon Jagger", :created=>[2010, 11, 26, 11, 49, 40]}, {:name=>"Jon Jagger2", :created=>[2010, 11, 28, 12, 8, 27]}, {:name=>"Jon Jagger3", :created=>[2010, 11, 28, 12, 50, 14]}, {:name=>"Jon Jagger4", :created=>[2010, 11, 28, 13, 17, 56]}]
