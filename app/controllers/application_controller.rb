5914ea2fb4ad054ed5082ef603376dd7108740fe
Jon Jagger <jon@jaggersoft.com>
2011-04-23 10:01:52 +0100

    still trying to work around chrome form submit only once bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '945dc57b815c0ad35dfe0b403fb13b89'
+  # protect_from_forgery # :secret => '945dc57b815c0ad35dfe0b403fb13b89'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- public/javascripts/cyberdojo_run_tests.js
+++ public/javascripts/cyberdojo_run_tests.js
@@ -21,10 +21,10 @@ function postRunTests()
   // app/views/kata/view.html.erb
   // form_remote_tag :url => {...}, 
   //                 :complete => "postRunTests();"
-  $('run_tests_spinner').setAttribute('style', 'display:none');
   $('play_button').setAttribute('style', 'visibility:visible');
   // new increment could affect layout
   refreshLineNumbering();
   $('editor').focus();
+  $('run_tests_spinner').setAttribute('style', 'display:none');
 }
 
