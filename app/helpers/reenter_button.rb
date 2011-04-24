b707067bae65d8bf29e759393c9b0370510606f3
Jon Jagger <jon@jaggersoft.com>
2011-04-24 10:05:25 +0100

    more css button refactoring

--- app/helpers/reenter_button.rb
+++ app/helpers/reenter_button.rb
@@ -3,7 +3,8 @@ def reenter_button(dojo_name, avatar_name)
   "<form action='/kata/edit' target='_blank'>" +
     "<input type='hidden' name='dojo_name' id='dojo_name' value='#{dojo_name}'/>" +
     "<input type='hidden' name='avatar' id='avatar' value='#{avatar_name}' />" +
-    "<input id='reenter_button'" + 
+    "<input class='button' +
+            id='reenter_button'" + 
             "type='submit'" +
             "value='ReEnter' />" + 
   "</form>"
--- app/helpers/view_button.rb
+++ app/helpers/view_button.rb
@@ -3,7 +3,8 @@ def view_button(dojo_name, avatar_name)
   "<form action='/kata/view' target='_blank'>" +
     "<input type='hidden' name='dojo_name' id='dojo_name' value='#{dojo_name}'/>" +
     "<input type='hidden' name='avatar' id='avatar' value='#{avatar_name}' />" +
-    "<input id='view_button'" + 
+    "<input class='button' +
+            id='view_button'" + 
             "type='submit'" +
             "value='View' />" + 
   "</form>"
