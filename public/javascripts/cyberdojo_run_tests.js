644691854173ba0f16aa913a71e780a5d363707e
Jon Jagger <jon@jaggersoft.com>
2011-05-02 19:18:51 +0100

    refactored some prototype code to jQuery

--- public/javascripts/cyberdojo_run_tests.js
+++ public/javascripts/cyberdojo_run_tests.js
@@ -3,10 +3,14 @@ function preRunTests() {
   // app/views/kata/edit.html.erb
   // form_remote_tag :url => {...}, 
   //                 :before => "preRunTests();",
-  $('run_tests').hide();    
-  $('spinner').show();    
-  $('output').setAttribute('class', 'waiting');
-  $('output').value = "Running tests...";
+
+  $j('#output')
+    .attr('class', 'waiting')
+    .val("Running tests...");
+  
+  $j('#run_tests').fadeOut('slow', function() {   
+      $j('#spinner').show();
+  });
   saveCurrentFile();
 }
 
@@ -14,11 +18,13 @@ function postRunTests() {
   // app/views/kata/edit.html.erb
   // form_remote_tag :url => {...}, 
   //                 :complete => "postRunTests();"
-  // new increment could affect layout
   
-  $('editor').focus();
-  $('spinner').hide();
-  $('run_tests').show();
+  $j('#spinner').fadeOut('slow', function() {
+      $j('#run_tests').show();
+  });
+  
+  $j('#editor').focus();
+  // new increment could affect layout
   refreshLineNumbering();  
 }
 
