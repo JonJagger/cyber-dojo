9711f8a5e066c1f6a6236405f9f847e879dc1b72
Jon Jagger <jon@jaggersoft.com>
2011-05-02 15:17:28 +0100

    put heartbeat back

--- public/javascripts/cyberdojo_run_tests.js
+++ public/javascripts/cyberdojo_run_tests.js
@@ -3,8 +3,8 @@ function preRunTests() {
   // app/views/kata/edit.html.erb
   // form_remote_tag :url => {...}, 
   //                 :before => "preRunTests();",
-  $('run_tests').hide();
-  $('spinner').show();
+  $('run_tests').hide();    
+  $('spinner').show();    
   $('output').setAttribute('class', 'waiting');
   $('output').value = "Running tests...";
   saveCurrentFile();
