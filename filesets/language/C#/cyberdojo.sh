3e4ea530402e116fb97cadcc6c94503cfd8377ad
Jon Jagger <jon@jaggersoft.com>
2011-05-10 12:02:32 +0100

    added -nologo option to nunit

--- filesets/language/C#/cyberdojo.sh
+++ filesets/language/C#/cyberdojo.sh
@@ -2,7 +2,7 @@ gmcs -t:library Untitled.cs
 if [ $? -eq 0 ]; then
   gmcs -t:library -r:Untitled.dll -r:nunit.framework.dll UntitledTest.cs
   if [ $? -eq 0 ]; then
-    nunit-console2 UntitledTest.dll
+    nunit-console -nologo UntitledTest.dll
   fi
 fi
 
