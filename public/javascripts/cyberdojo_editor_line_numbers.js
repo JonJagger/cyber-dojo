e52dd4a38985b74531c2be1d8c2c722757c1184e
Jon Jagger <jon@jaggersoft.com>
2011-05-18 09:41:45 +0100

    moved message-panel to left;editor and output textareas now align vertically;css tidy up

--- public/javascripts/cyberdojo_editor_line_numbers.js
+++ public/javascripts/cyberdojo_editor_line_numbers.js
@@ -15,20 +15,15 @@ function createLineNumbersFor(id)
   var el = document.createElement('textarea'); // line-numbers textarea
   
   el.setAttribute('readonly', 'true');  
-  el.id = 'line_numbers';
-  el.className      = 'textarea_with_line_numbers';
+  el.id = 'editor_line_numbers';
   el.style.height   = (ta.offsetHeight - 3) + "px";
+  
   el.style.position = 'absolute';
   el.style.overflow = 'hidden';
   el.style.textAlign = 'right';
-  // When the textarea has the focus the browser highlights it typically by
-  // adding a small extra faint margin. Counteract this by reducing the
-  // margin. This means the textarea editor and its accompanying line numbers
-  // have better horizontal alignment.
-  el.style.marginTop       = '-1px';
-  el.style.marginBottom    = '-1px';
+  
   el.style.width    = '33px';
-  el.style.paddingRight = '0.2em';
+  
   el.innerHTML      = string;  // Firefox renders \n linebreak
   el.innerText      = string;  // IE6 renders \n line break
   el.value          = string;  // Safari
