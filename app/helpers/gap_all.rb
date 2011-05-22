5fd73059a8471b4e422df1c8ce7e91cb985be1b0
Jon Jagger <jon@jaggersoft.com>
2011-05-22 09:40:54 +0100

    notes on what I need to do to make scrolling traffic-light display work when it needs to display long periods of time

--- app/helpers/gap_all.rb
+++ app/helpers/gap_all.rb
@@ -4,21 +4,32 @@
   # by calculating the number of seconds between traffic lights. However
   # this is not satifactory because each traffic light itself takes up a 
   # certain width. Hence an avatar hitting the run-tests button a lot
-  # will naturally takes up quite a lot of horizontal space. The best
-  # solution I can think of is to split each avatar's traffic light tr into 
-  # the same number of td's and make each td represent a fixed period of time,
+  # will naturally takes up quite a lot of horizontal space and the display 
+  # loses the vital property of relative horizontal position indicating relative
+  # moments in time.
+  #
+  # The best solution I can think of is to split each avatar's traffic light tr
+  # into the same number of td's and make each td represent a period of time,
   # say 15 seconds.
-  # The start time will be the start time of the dojo, and the end time will
-  # be the current time. This means that as time passes, traffic lights from 
-  # the past move backwards.
-  # I don't want scroll-bars since the traffic-lights on the dashboard 
-  # automatically refresh every few seconds and this would interfere with
-  # the scrolling. The best way to combat this is to limit the number of
-  # td's on a tr, and if there are too many, to simply chop off early ones.
-  # This is what I do on the main play page where I limit the number of
-  # traffic-lights to only a few (eg 7) and I don't space them out.
-
+  # The start time will be the start time of the dojo. At first I had the end 
+  # time of the dojo as being the current time. But this is flawed. The reason
+  # is that as time passes, traffic lights from the past move backwards. And it
+  # doesn't make sense to even consider display anything for the time between
+  # the last traffic-light and the current date/time in the non-refreshing, 
+  # scrollbar based view.
+  #
+  # What I need to do
+  # -----------------
+  # Have a function that calculates the td-number of a traffic light
+  # based on the dojo start time and the time of the last traffic-light.
+  # Use this for all traffic-lights of all avatars.
+  # If there are periods greater than 30 mins (which could roughly
+  # correspond to the time period can be shown on the time-shifting auto
+  # updating display), then collapse all these td's into a single td with a
+  # special css class which displays as ellision in some way...
+  
 def gap_all(all_incs, created, now, seconds_per_gap)
+  now = latest(all_incs) # fixme (no need to pass parameter)
   gaps = time_gaps(created, now, seconds_per_gap)
   full = {}
   all_incs.each do |avatar_name, incs|
@@ -44,4 +55,16 @@ def gap_one(lights, gaps)
   chunks
 end
 
+def latest(all_incs)
+  current = nil
+  all_incs.each do |avatar_name, incs|
+    if incs.length != 0
+      at = Time.mktime(*incs.last[:time])
+      if !current  || at > current
+        current = at
+      end
+    end
+  end
+  current
+end
 
