f59c3a17dee280f76310002c1c0fa559a2cb264c
Jon Jagger <jon@jaggersoft.com>
2011-05-28 21:46:26 +0100

    make traffic-light rows slightly narrower; boy scout tidying

--- app/helpers/traffic_light.rb
+++ app/helpers/traffic_light.rb
@@ -1,14 +1,14 @@
  
-def traffic_light(dojo_name, avatar_name, rung)
-  outcome = rung[:outcome].to_s
+def traffic_light(dojo_name, avatar_name, light)
+  outcome = light[:outcome].to_s
 
   red   = on_off(outcome, 'failed')
   amber = on_off(outcome, 'error')
   green = on_off(outcome, 'passed')
 
-  [ bulb(dojo_name, avatar_name, rung, red),
-    bulb(dojo_name, avatar_name, rung, amber),
-    bulb(dojo_name, avatar_name, rung, green),
+  [ bulb(dojo_name, avatar_name, light, red),
+    bulb(dojo_name, avatar_name, light, amber),
+    bulb(dojo_name, avatar_name, light, green),
   ].join('')
 end
 
@@ -16,12 +16,12 @@ def on_off(outcome, is)
   return outcome == is ? is : 'off'
 end
 
-def bulb(dojo_name, avatar_name, rung, colour)
+def bulb(dojo_name, avatar_name, light, colour)
   link_to "<span class='#{colour} traffic_light_bulb'></span>", 
     { :controller => :kata, 
-      :action => "review?dojo_name=#{dojo_name}&avatar=#{avatar_name}&tag=#{rung[:number]}" 
+      :action => "review?dojo_name=#{dojo_name}&avatar=#{avatar_name}&tag=#{light[:number]}" 
     }, 
-    { :title => "#{rung[:number]}", 
+    { :title => "#{light[:number]}", 
       :target => "_blank" 
     } 
 end
