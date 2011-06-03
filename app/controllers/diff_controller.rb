2d291f8d97ffcf931bcce2202de7bb3e275f4de0
Jon Jagger <jon@jaggersoft.com>
2011-06-03 09:42:38 +0100

    diff-view scroll-top/left state handled in javascript

--- app/controllers/diff_controller.rb
+++ app/controllers/diff_controller.rb
@@ -15,11 +15,26 @@ class DiffController < ApplicationController
   end
   
   def scratch
-    @diffs = [
-      { :deleted_line_count => 2, :name => 'gapper.rb',     :added_line_count => 1, :content => "X" },
-      { :deleted_line_count => 1, :name => 'pairs.rb',      :added_line_count => 4, :content => "Y" },
-      { :deleted_line_count => 0, :name => 'test_pairs.rb', :added_line_count => 3, :content => "Z" },
-    ]
+    configure(params)
+    @dojo = Dojo.new(params)
+    @avatar = Avatar.new(@dojo, params[:avatar])
+    @kata = @avatar.kata
+    @manifest = {}
+    @traffic_lights_to_tag = @avatar.read_manifest(@manifest, params[:tag])
+    @all_traffic_lights = @avatar.increments    
+    @current_file = @manifest[:current_filename]
+    @output = @manifest[:output]
+    @diffed_files = git_diff_view(@avatar, params[:tag].to_i)    
+
+    @diffs = []
+    @diffed_files.each do |name,diff|
+      @diffs << {
+        :deleted_line_count => [0,1,2,3,4].shuffle[0],
+        :name => name,
+        :added_line_count => [0,1,2,3,4].shuffle[0],
+        :content => git_diff_html(diff)
+      }
+    end
   end
   
 private
