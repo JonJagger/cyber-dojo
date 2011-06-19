cd34d779d27798f1fd83b7171a90117909a80d5e
Jon Jagger <jon@jaggersoft.com>
2011-06-19 21:19:20 +0100

    names.rb script shows sha1 folder aswell as name

--- names.rb
+++ names.rb
@@ -1,5 +1,16 @@
 
+# ruby names.rb
+# >dojo-name1 is in ca/cfefd7daacd9694bd893527581c59596ebd21e
+# >dojo-name1 is in f6/893527581c59596ebd21ecfefd7daacd9694bd
+
+require 'digest/sha1'
+
 index = eval IO.popen('cat dojos/index.rb').read
 names = index.map{|e| e[:name]}
-p names
+names.each do |name|
+  sha1 = Digest::SHA1.hexdigest(name)
+  inner = sha1[0..1]   
+  outer = sha1[2..-1]
+  print inner + '/' + outer + '    ' + name + "\n"
+end
 
