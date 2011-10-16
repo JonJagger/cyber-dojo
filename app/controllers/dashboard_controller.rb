018347a497aa234419a36a780bd5007dc6f98a4c
Jon Jagger <jon@jaggersoft.com>
2011-10-16 11:55:06 +0100

    minor refactoring

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,27 @@
 
 class DashboardController < ApplicationController
    
+  # TODO: replace updown control this with simple control to enter the
+  #       value you'd like together with a refresh/redraw button.
+  # secs/col [input]
+  #    REDRAW
+  # max cols [input]
+
+  # I'd like the dashboard for a finished dojo to be pure client side.
+  # Create all diffs for all increments for all animals, together with
+  # a client-side dashboard in a single html file.
+  # This will allow a replay of a session to happen without any interaction
+  # with the server. A finished dojo will be one that has timed out
+  # after 60 minutes. I plan to introduce the fixed timeout only when
+  # you can start a new dojo from any traffic-light.
+
   def show_inflated
     board_config(params)
     @secs_per_col = 30
     @max_cols = 60
   end
 
-  def heartbeat
+  def inflated_heartbeat
     board_config(params)
     @secs_per_col = params[:secs_per_col].to_i
     @max_cols = params[:max_cols].to_i
--- starting_filesets/Ruby/LCD Digits/manifest.rb
+++ starting_filesets/Ruby/LCD Digits/manifest.rb
@@ -1,6 +1,6 @@
 
 {
-  :visible_filenames => %w( instructions untitled.rb test_untitled.rb cyberdojo.sh ),
+  :visible_filenames => %w( instructions lcd_digits.rb test_lcd_digits.rb cyberdojo.sh ),
   :unit_test_framework => 'ruby_test_unit',
   :tab_size => 2,
 }
