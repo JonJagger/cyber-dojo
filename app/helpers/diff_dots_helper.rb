0e326f9180311ac358609ea2582345d46953a6bd
Jon Jagger <jon@jaggersoft.com>
2011-10-17 21:21:00 +0100

    minor refactoring of diff-dots on lhs and rhs of filename

--- app/helpers/diff_dots_helper.rb
+++ app/helpers/diff_dots_helper.rb
@@ -1,36 +1,34 @@
 module DiffDotsHelper
   
   def diff_dots(n, kind)
-    max_dots = 3
     html = '<table>'
     html += '<tr>'
-
-    if kind == 'deleted'
-      if n <= max_dots
-        (max_dots - n).times { html += td_dot('spacer') }
-        html += '<td><span class="diff_dots off"></span></td>'
-      else
-        html += '<td><span class="diff_dots on"></span></td>'
-      end
-    end
-
-    [max_dots,n].min.times { html += td_dot(kind) }
-
-    if kind == 'added'
-      if n <= max_dots
-        (max_dots - n).times { html += td_dot('spacer') }
-        html += '<td><span class="diff_dots off"></span></td>'
-      else
-        html += '<td><span class="diff_dots on"></span></td>'
-      end
-    end
-
+    html +=   spaced_ellision(n) if kind == 'deleted'    
+                [MaxDots,n].min.times { html += td_dot(kind) }    
+    html +=   spaced_ellision(n) if kind == 'added'
     html += '</tr>'
     html += '</table>'
   end
-
+    
+  def spaced_ellision(n)
+    html = ""
+    if n <= MaxDots
+      (MaxDots - n).times { html += td_dot('spacer') }
+      html += ellision('off')
+    else
+      html += ellision('on')
+    end
+    html
+  end
+  
+  def ellision(on_off)
+    "<td><span class='diff_dots #{on_off}'></span></td>"
+  end
+  
   def td_dot(kind)
     "<td><div class='#{kind} dot'></div></td>"
   end
+
+  MaxDots = 3
   
 end
