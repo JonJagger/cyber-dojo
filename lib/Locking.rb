0a0669557bda46cf011ae591afdc2a683bf5b446
Jon Jagger <jon@jaggersoft.com>
2011-10-31 12:42:49 +0000

    avatar selection needs to be non-blocking now players select their own avatars

--- lib/Locking.rb
+++ lib/Locking.rb
@@ -5,7 +5,7 @@ module Locking
   def io_lock(path, &block)
     result = nil
     File.open(path, 'r') do |fd|
-      if fd.flock(File::LOCK_EX)
+      if fd.flock(File::LOCK_EX | File::LOCK_NB)
         begin
           result = block.call(fd)
         ensure
