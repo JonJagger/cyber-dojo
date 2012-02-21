90bbba14c41ea6c16e66e42526e78ded5ea9d506
Phil Nash <github@philnash.me>
2012-02-21 13:02:38 +0000

    Wildcarded filenames

--- filesets/language/ObjC Assert/cyberdojo.sh
+++ filesets/language/ObjC Assert/cyberdojo.sh
@@ -1,11 +1,5 @@
-gcc -Wall -Werror -ObjC untitled.m -c
+gcc -Wall -Werror *.m -framework Foundation -o untitled
 if [ $? -eq 0 ]; then 
-  gcc -Wall -Werror -ObjC untitled_tests.m -c
-  if [ $? -eq 0 ]; then 
-    gcc untitled.o untitled_tests.o -framework Foundation -o untitled
-    if [ $? -eq 0 ]; then 
-      ./untitled
-    fi  
-  fi
-fi
+  ./untitled
+fi  
  
