17340c3363181e20b6d5d58898e85bc93406c6aa
Jon Jagger <jon@jaggersoft.com>
2012-03-01 17:20:19 +0000

    test/functional/avatar_tests.rb refactored

--- test/functional/avatar_tests.rb
+++ test/functional/avatar_tests.rb
@@ -1,56 +1,19 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'Files'
 
 # > ruby test/functional/avatar_tests.rb
 
 class AvatarTests < ActionController::TestCase
 
-  include Files
-  extend Files
-  
-  ROOT_TEST_DIR = RAILS_ROOT + '/test/katas'
-
-  def root_test_dir_reset
-    system("rm -rf #{ROOT_TEST_DIR}")
-    Dir.mkdir ROOT_TEST_DIR
-  end
-
-  def make_params(language)
-    params = {
-      :katas_root_dir => ROOT_TEST_DIR,
-      :filesets_root_dir => RAILS_ROOT +  '/filesets',
-      :browser => 'Firefox',
-      'language' => language,
-      'exercise' => 'Yahtzee',
-      'name' => 'Jon Jagger'
-    }
-  end
-
-  def make_kata(language = 'Ruby') 
-    params = make_params(language)
-    fileset = InitialFileSet.new(params)
-    info = Kata::create_new(fileset)
-    params[:id] = info[:id]
-    Kata.new(params)    
-  end
-  
   def test_no_increments_before_first_test_run
-    root_test_dir_reset
-    kata = make_kata
+    kata = make_kata('C assert')
     avatar = Avatar.new(kata, 'wolf')    
     assert_equal [ ], avatar.increments    
   end
   
   def test_increments_does_not_contain_output
-    root_test_dir_reset
-    params = make_params('C Assert')
-    fileset = InitialFileSet.new(params)
-    info = Kata::create_new(fileset)
-    params[:id] = info[:id]
-    kata = Kata.new(params)    
-    
+    kata = make_kata('C assert')
     avatar = Avatar.new(kata, 'wolf')    
-    avatar.run_tests(fileset.visible_files)
+    run_tests(avatar, avatar.visible_files)
     increments = avatar.increments
     assert_equal 1, increments.length
     assert_equal nil, increments.last[:run_tests_output]
