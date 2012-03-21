8f2f77404b942e126549069647923c51a65c627d
Jon Jagger <jon@jaggersoft.com>
2012-03-21 10:04:58 +0000

    changed tab titleto cyber-dojo to match url; added exception handler to names.rb

--- names.rb
+++ names.rb
@@ -22,24 +22,21 @@ def name_or_diff_from(manifest)
   manifest[:name] || "#{manifest[:diff_id]}-#{manifest[:diff_avatar]}-#{manifest[:diff_tag]}"
 end
 
-index = eval IO.popen('cat katas/index.rb').read
-show = (ARGV[0] || "32").to_i
-ids = recent(index, show).map{ |entry| entry[:id] }
-ids.each do |id|
+def show_one(id)
   inner_dir = id[0..1]   
   outer_dir = id[2..9]
   kata_dir = "katas/#{inner_dir}/#{outer_dir}"
   manifest = eval IO.popen("cat #{kata_dir}/manifest.rb").read
   created = Time.mktime(*manifest[:created])
 
-  inc_lengths = []
+  inc_lengths = [ ]
   avatars.each do |avatar|
     if File.directory? kata_dir + '/' + avatar
       incs = eval IO.popen("cat #{kata_dir}/#{avatar}/increments.rb").read
       inc_lengths << incs.length
     end
   end
-
+  
   print created.strftime('%b %d %H:%M') + 
     ' ' + inner_dir + '/' + outer_dir + 
     ' ' + '{' + name_or_diff_from(manifest) + '}, ' +
@@ -48,5 +45,17 @@ ids.each do |id|
     ' ' + '[' + inc_lengths.sort.join(',') + '] -> ' + inc_lengths.reduce(:+).to_s +
     ' ' + (manifest[:browser] || "") +
     "\n"
-
 end
+
+index = eval IO.popen('cat katas/index.rb').read
+show = (ARGV[0] || "32").to_i
+ids = recent(index, show).map{ |entry| entry[:id] }
+ids.each do |id|
+  print "\n"
+  begin
+    show_one(id)
+  rescue
+    print "Exception raised for ID:#{id}\n"
+  end
+end
+print "\n"
