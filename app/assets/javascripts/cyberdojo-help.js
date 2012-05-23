d867fb76feb007210cdbf32326d5320a34d798a0
Jon Jagger <jon@jaggersoft.com>
2012-05-23 09:51:06 +0100

    increased size of start-new-practice-kata-from-here button on diff page

--- app/assets/javascripts/cyberdojo-help.js
+++ app/assets/javascripts/cyberdojo-help.js
@@ -57,7 +57,7 @@ var cyberDojo = (function($cd, $j) {
       + ''
       + '<div class="panel">'
       +   $cd.makeTable(
-            '<input type="button" class="larger button" value="run-the-tests"/>',
+            '<input type="button" class="large button" value="run-the-tests"/>',
             'results go to the',
             $cd.fakeFilenameButton('<i>output</i>'),
             'file')
