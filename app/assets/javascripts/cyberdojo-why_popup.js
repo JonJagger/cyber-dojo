4d0b96b48198ee287973925b08960abc923292dc
Jon Jagger <jon@jaggersoft.com>
2012-09-29 07:33:15 +0100

    why button now entirely client side

---
+++ app/assets/javascripts/cyberdojo-why_popup.js
@@ -0,0 +1,59 @@
+/*jsl:option explicit*/
+
+var cyberDojo = (function($cd, $j) {
+
+  $cd.whyPopup = function() {
+
+    var oslo =
+        'Several years ago I took part in a coding dojo in the Scotsman pub in Oslo.<br/>' +
+        'At the end of the dojo I was struck by a few thoughts:<br/>' +
+        '<br/>';
+
+    var bullet = "&nbsp;&bull;&nbsp;" 
+    var feel = bullet + "It didn't <em>feel</em> like a dojo should feel.";
+    
+    var improve =
+      $cd.divPanel(
+        'In a coding dojo you should <em>not</em> be thinking ' +
+        'about finishing the task; ' +
+        'you should be thinking about <em>improving some aspect of your skill</em>. ' +
+        'The practice should be repeated. The repetition frees up mental capacity, ' +
+        'helping you break out of ' +
+        'a completion mindset and into a practice/improvement mindset.' 
+      );
+
+    var delay = bullet + 'Several groups took a <em>long</em> time to get ' +
+        'started, installing compilers and whatnot.';
+
+    var browser =
+      $cd.divPanel(
+        'I wondered what the minimum install could be for a coding dojo. ' +
+        'I realized it was <em>nothing</em>. ' +
+        'You could write your code and tests in a ' +
+        'web browser and run your tests on a server. ' +
+        'The more I thought about this idea the more I liked it. ' +
+        'I really liked the idea of participants using something ' +
+        'that was so obviously <em>not their normal development environment</em>. ' +
+        'I felt this would reinforce the idea that in a dojo ' +
+        'you should <em>not</em> be doing normal development!'
+      );
+
+    var noCollab = bullet + 'There very little collaboration.';
+    
+    var social =
+      $cd.divPanel(
+        'I started to sense some of the <em>social</em> possibilities of a ' +
+        'browser-based coding dojo, in particular, the idea of treating all the ' +
+        "participants as a single team, with everyone able to see everyone else's code."
+    );
+      
+    var grid = $j($cd.makeTable(oslo,feel,improve,delay,browser,noCollab,social));
+      
+    $cd.dialogPopup(grid.html(), 950, 'why');
+  };
+
+  return $cd;
+})(cyberDojo || {}, $);
+
+
+
