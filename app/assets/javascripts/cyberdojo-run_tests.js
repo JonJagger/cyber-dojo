1b4149fcaaa2d60cae376973e42297492059bca4
Jon Jagger <jon@jaggersoft.com>
2012-10-05 12:50:03 +0100

    spinner refactoring

--- app/assets/javascripts/cyberdojo-run_tests.js
+++ app/assets/javascripts/cyberdojo-run_tests.js
@@ -4,7 +4,7 @@ var cyberDojo = (function($cd, $j) {
 
   $cd.preRunTests = function() {
     $j('#test').attr('disabled', true);
-    $j('#spinner_lhs,#spinner_rhs').show();
+    $j('.spinner').show();
     $j('#tip').hide();
   };
 
@@ -12,10 +12,10 @@ var cyberDojo = (function($cd, $j) {
 
   $cd.postRunTests = function() {
     $j('#tip').hide();
-    $j('#spinner_lhs,#spinner_rhs').hide();
+    $j('.spinner').hide();
     $j('#test').attr('disabled', false);
-    // when the AJAX js replaces output shortcuts are lost
-    // so need to rebind them    
+    // when the AJAX js replaces output shortcuts
+    // are lost so need to rebind them    
     var output = $cd.fileContentFor('output');
     $cd.bindHotKeys(output);        
     $cd.loadFile('output');
