7abe4bcbe62cce9a2bd62102c36dd40bb93c459e
Jon Jagger <jon@jaggersoft.com>
2012-10-05 12:08:35 +0100

    minor duplication refactoring

--- app/assets/javascripts/cyberdojo-run_tests.js
+++ app/assets/javascripts/cyberdojo-run_tests.js
@@ -4,8 +4,7 @@ var cyberDojo = (function($cd, $j) {
 
   $cd.preRunTests = function() {
     $j('#test').attr('disabled', true);
-    $j('#spinner_lhs').show();
-    $j('#spinner_rhs').show();
+    $j('#spinner_lhs,#spinner_rhs').show();
     $j('#tip').hide();
   };
 
@@ -13,8 +12,7 @@ var cyberDojo = (function($cd, $j) {
 
   $cd.postRunTests = function() {
     $j('#tip').hide();
-    $j('#spinner_lhs').hide();
-    $j('#spinner_rhs').hide();
+    $j('#spinner_lhs,#spinner_rhs').hide();
     $j('#test').attr('disabled', false);
     // when the AJAX js replaces output shortcuts are lost
     // so need to rebind them    
