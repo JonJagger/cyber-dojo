a6e36c17020302857864ba225dd224033b6d1bd9
Jon Jagger <jon@jaggersoft.com>
2012-10-06 11:20:23 +0100

    url now appears properly on browser's address bar

--- app/assets/javascripts/cyberdojo-post_to.js
+++ app/assets/javascripts/cyberdojo-post_to.js
@@ -3,25 +3,24 @@
 var cyberDojo = (function($cd, $j) {
 
   $cd.postTo = function(url, params, target) {
+    
+    if (params && params['id'] !== 'undefined') {
+      url += '/' + params['id'];
+    }
+    
+    var sep = '?';
+    for (var key in params) {
+        if (key !== 'id') {
+          url += sep + key + '=' + params[key];
+          sep = '&';
+        }
+    }
+    
     var form = $j('<form>');
     form.attr('action', url);
     form.attr('method', 'POST');
     if (typeof(target) !== 'undefined') {
       form.attr('target', target);
-    }
-
-    var addParam = function(paramName, paramValue) {
-      var input = $j('<input type="hidden">');
-      input.attr({ id:     paramName,
-                   name:   paramName,
-                   value:  paramValue });
-      form.append(input);
-    };
-
-    if (typeof(params) !== 'undefined') {
-      for (var key in params) {
-        addParam(key, params[key]);
-      }
     }
 
     form.appendTo(document.body);
