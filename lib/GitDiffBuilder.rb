0d6b9cebcce6910ab3b2ff639bf73453d82e5318
Jon Jagger <jon@jaggersoft.com>
2012-10-14 11:07:01 +0100

    gitDiffBuilder.rb refactoring

--- lib/GitDiffBuilder.rb
+++ lib/GitDiffBuilder.rb
@@ -15,54 +15,45 @@ module GitDiff
   
   class GitDiffBuilder
   
-    def initialize
-      @line_number = 1
-    end
-
     def build(diff, lines)    
       result = [ ]
+      line_number = 1
       from = 0
       index = 0
       diff[:chunks].each do |chunk|
         to = chunk[:range][:now][:start_line] + chunk[:before_lines].length - 1
-        line_number = fill(result, :same, lines, from, to)
+        line_number = fill(result, :same, lines, from, to, line_number)
         chunk[:sections].each do |section|
           result << { :type => :section, :index => index }
           index += 1
-          build_section(result, section)
+                        fill_all(result, :deleted, section[:deleted_lines], line_number)
+          line_number = fill_all(result, :added, section[:added_lines], line_number)
+          line_number = fill_all(result, :same, section[:after_lines], line_number)
         end
-        from = @line_number - 1      
+        from = line_number - 1      
       end    
-      last_lines = lines[@line_number-1..lines.length]   
-      fill_all(result, :same, last_lines)
+      last_lines = lines[line_number-1..lines.length]   
+      fill_all(result, :same, last_lines, line_number)
       result
     end
     
   private
-  
-    def build_section(result, section)
-      reset = @line_number
-      fill_all(result, :deleted, section[:deleted_lines])
-      @line_number = reset
-      fill_all(result, :added, section[:added_lines])
-      fill_all(result, :same, section[:after_lines])
-    end
-  
-    def fill_all(result, type, lines)
+    
+    def fill_all(result, type, lines, line_number)
       lines ||= [ ] 
-      fill(result, type, lines, 0, lines.length)
+      fill(result, type, lines, 0, lines.length, line_number)
     end
     
-    def fill(into, type, lines, from, to)
+    def fill(into, type, lines, from, to, line_number)
       (from...to).each do |n|
-        line = {
+        into << {
           :type => type,
           :line => lines[n],
-          :number => @line_number
+          :number => line_number
         }
-        @line_number += 1
-        into << line
+        line_number += 1
       end
+      line_number
     end
   
   end
