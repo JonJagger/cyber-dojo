270f0f9c88e498d1635f2596f110b365c2d723d5
Jon Jagger <jon@jaggersoft.com>
2012-11-20 19:19:25 +0000

    added js file for donations button; added fork.png to be used later

---
+++ app/assets/javascripts/cyber-dojo_dialog_donations.js
@@ -0,0 +1,38 @@
+/*global $,cyberDojo*/
+
+var cyberDojo = (function(cd, $) {
+  "use strict";
+  
+  cd.dialog_donations = function() {
+
+    var companies =
+      cd.divPanel(
+          "Schlumberger Beijing Geoscience Center have found cyber-dojo an "
+        + "invaluable tool for training and development of our software community "
+        + "and believe the project has a bright future. Thank for you "
+        + "contribution to our training efforts. We are delighted to "
+        + "sponsor the cyber-dojo project by contributing $1000."
+      );
+
+    var individuals =
+      cd.divPanel(
+          "Olve Maudal, Mike Long, Mathieu Baron, "
+        + "Steve Coates, Johannes van Tonder, Santeri Vesalainen, "
+        + "Alexander Ottesen, Lars Storjord, Anders Schau Knatten, "
+        + "James Grenning, Allan Kelly"
+      );
+
+    var thankYou =
+        "<span id='thank_you'>thank you</span>";
+        
+    var donations = $(cd.makeTable(
+        "companies", companies, "individuals", individuals, thankYou));
+      
+    cd.dialog(donations.html(), 750, 'donations');
+  };
+
+  return cd;
+})(cyberDojo || {}, $);
+
+
+
