bb720bad915559a672be7263aef588511a984616
Jon Jagger <jon@jaggersoft.com>
2012-12-06 20:07:11 +0000

    code_output_parser_tests refactoring

--- test/lib/code_output_parser_tests.rb
+++ test/lib/code_output_parser_tests.rb
@@ -7,34 +7,32 @@ class CodeOutputParserTests < ActionController::TestCase
   include CodeOutputParser
   
   test "was a red ruby case" do
-
-output = <<HERE
-Loaded suite test_get_digits
-Started
-F...........F.F..
-Finished in 0.01447 seconds.
-<"     _   _ \n  | | | |_|\n  | |_|   |">.
-<" _     \n| |   |\n|_|   |">.
-  3) Failure:
-<" _   _ \n  |  _|\n  | |_ ">.
-17 tests, 17 assertions, 3 failures, 0 errors
-
-  2) Failure:
-test_ten(TestGetDigits) [test_get_digits.rb:81]:
-<"     _ \n  | | |\n  | |_|"> expected but was
-<" _     \n| |   |\n|_|   |">.
-
-  3) Failure:
-test_twenty_seven(TestGetDigits) [test_get_digits.rb:95]:
-<" _   _ \n _|   |\n|_    |"> expected but was
-<" _   _ \n  |  _|\n  | |_ ">.
-
-17 tests, 17 assertions, 3 failures, 0 errors
-[[" _ ", " _ "], ["  |", " _|"], ["  |", "|_ "]]
-HERE
-
-    assert_equal :red, CodeOutputParser::parse_ruby_test_unit(output)
-
+    red_output =
+      [
+        "Loaded suite test_get_digits",
+        "Started",
+        "F...........F.F..",
+        "Finished in 0.01447 seconds.",
+        '<"     _   _ \n  | | | |_|\n  | |_|   |">.',
+        '<" _     \n| |   |\n|_|   |">.',
+        '  3) Failure:',
+        '<" _   _ \n  |  _|\n  | |_ ">.',
+        '17 tests, 17 assertions, 3 failures, 0 errors',
+        '',
+        '  2) Failure:',
+        'test_ten(TestGetDigits) [test_get_digits.rb:81]:',
+        '<"     _ \n  | | |\n  | |_|"> expected but was',
+        '<" _     \n| |   |\n|_|   |">.',
+        '',
+        '  3) Failure:',
+        'test_twenty_seven(TestGetDigits) [test_get_digits.rb:95]:',
+        '<" _   _ \n _|   |\n|_    |"> expected but was',
+        '<" _   _ \n  |  _|\n  | |_ ">.',
+        '',
+        '17 tests, 17 assertions, 3 failures, 0 errors',
+        '[[" _ ", " _ "], ["  |", " _|"], ["  |", "|_ "]]'
+      ].join("\n")
+    assert_equal :red, CodeOutputParser::parse_ruby_test_unit(red_output)
   end
 
   #--------------------------------------------------------
@@ -79,12 +77,13 @@ HERE
   end
   
   test "catch AMBER" do
-amber_output = <<HERE    
-untitled.tests.cpp: In function Ôvoid catch_internal_TestFunction5()Õ:
-untitled.tests.cpp:7: error: ÔtypoÕ was not declared in this scope
-untitled.tests.cpp:8: error: expected `;' before Ô}Õ token
-make: *** [run.tests] Error 1
-HERE
+    amber_output =
+      [
+        "untitled.tests.cpp: In function 'void catch_internal_TestFunction5()':",
+        "untitled.tests.cpp:7: error: 'typo' was not declared in this scope",
+        "untitled.tests.cpp:8: error: expected `;' before '}' token",
+        "make: *** [run.tests] Error 1"
+      ].join("\n")
     assert_equal :amber, CodeOutputParser::parse_catch(amber_output)
   end
   
@@ -93,12 +92,15 @@ HERE
   test "go build failed is amber" do
     assert_equal :amber, CodeOutputParser::parse_go_testing("[build failed]")  
   end
+  
   test "go ran but failed is green" do
     assert_equal :red, CodeOutputParser::parse_go_testing("FAIL")
   end
+  
   test "go ran and passed is red" do
     assert_equal :green, CodeOutputParser::parse_go_testing("PASS")
   end
+  
   test "go anything else is amber" do
     assert_equal :amber, CodeOutputParser::parse_go_testing("anything else")
   end
@@ -106,25 +108,27 @@ HERE
   #--------------------------------------------------------
   
   test "was a green C/C++ assert case" do
-amber_output = <<HERE    
-g++ -Wall -Werror -O *.cpp -o run.tests
-./run.tests
-terminate called after throwing an instance of 'std::out_of_range'
-  what():  vector::_M_range_check
-make: *** [run.tests.output] Aborted
-HERE
+    amber_output =
+      [
+        "g++ -Wall -Werror -O *.cpp -o run.tests",
+        "./run.tests",
+        "terminate called after throwing an instance of 'std::out_of_range'",
+        "  what():  vector::_M_range_check",
+        "make: *** [run.tests.output] Aborted"
+      ].join("\n")
     assert_equal :amber, CodeOutputParser::parse_cassert(amber_output)    
   end
   
   #--------------------------------------------------------
   
   test "another green C/C++ assert case" do
-green_output = <<HERE    
-g++ -Wall -Werror -O *.cpp -o run.tests
-./run.tests
-..
-2 tests passed
-HERE
+    green_output =
+      [
+        "g++ -Wall -Werror -O *.cpp -o run.tests",
+        "./run.tests",
+        "..",
+        "2 tests passed"
+      ].join("\n")
     assert_equal :green, CodeOutputParser::parse_cassert(green_output)        
   end
   
