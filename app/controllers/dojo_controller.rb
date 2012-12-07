6246b814050c28535efbc10071246158625bc6fd
Jon Jagger <jon@jaggersoft.com>
2012-12-07 19:26:02 +0000

    moved setup into its own controller and view

--- app/controllers/dojo_controller.rb
+++ app/controllers/dojo_controller.rb
@@ -14,33 +14,6 @@ class DojoController < ApplicationController
   end
  
   #------------------------------------------------
-
-  def create
-    @languages = Folders::in(root_dir + '/languages').sort    
-    @exercises = Folders::in(root_dir + '/exercises').sort
-    @instructions = { }
-    @exercises.each do |exercise|
-      @instructions[exercise] = Exercise.new(root_dir, exercise).instructions
-    end
-    @title = 'Setup'
-  end
-  
-  #------------------------------------------------
-
-  def save
-    info = gather_info
-    language = Language.new(root_dir, info[:language])
-    exercise = Exercise.new(root_dir, info[:exercise])
-    vis = info[:visible_files] = language.visible_files
-    vis['output'] = ''
-    vis['instructions'] = exercise.instructions
-    Kata.create_new(root_dir, info)
-    
-    redirect_to :action => :index, 
-                :id => info[:id]
-  end  
-  
-  #------------------------------------------------
   
   def exists_json
     respond_to do |format|
---
+++ app/controllers/setup_controller.rb
@@ -0,0 +1,35 @@
+
+require 'Folders'
+
+class SetupController < ApplicationController
+  
+  def show
+    @languages = Folders::in(root_dir + '/languages').sort    
+    @exercises = Folders::in(root_dir + '/exercises').sort
+    @instructions = { }
+    @exercises.each do |exercise|
+      @instructions[exercise] = Exercise.new(root_dir, exercise).instructions
+    end
+    @title = 'Setup'
+  end
+  
+  def cancel
+    redirect_to :controller => 'dojo',
+                :action => :index    
+  end
+
+  def save
+    info = gather_info
+    language = Language.new(root_dir, info[:language])
+    exercise = Exercise.new(root_dir, info[:exercise])
+    vis = info[:visible_files] = language.visible_files
+    vis['output'] = ''
+    vis['instructions'] = exercise.instructions
+    Kata.create_new(root_dir, info)
+    
+    redirect_to :controller => 'dojo',
+                :action => :index, 
+                :id => info[:id]
+  end  
+  
+end
