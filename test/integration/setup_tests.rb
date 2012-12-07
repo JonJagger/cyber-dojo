0a4e2e7f648a96bc9d3adc2d512d55d5c3243443
Jon Jagger <jon@jaggersoft.com>
2012-12-07 19:53:18 +0000

    added setup-controller test for cancel

--- test/integration/setup_tests.rb
+++ test/integration/setup_tests.rb
@@ -8,6 +8,11 @@ class SetupControllerTest  < IntegrationTest
     assert_response :success
   end
 
+  test "cancel" do
+    post 'setup/cancel'
+    assert_match @response.redirect_url, /^#{url_for :action => 'index', :controller => 'dojo'}/
+  end
+
   test "save" do
     checked_save_id
   end
