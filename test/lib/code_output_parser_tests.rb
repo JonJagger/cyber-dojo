faebd429b1471856a7ff467bb40c49a2a854e8d8
Jon Jagger <jon@jaggersoft.com>
2012-12-11 08:13:50 +0000

    tweaked the new-rename-delete file button layout

--- test/lib/code_output_parser_tests.rb
+++ test/lib/code_output_parser_tests.rb
@@ -135,7 +135,7 @@ class CodeOutputParserTests < ActionController::TestCase
   test "nunit RED" do
     # There are two NUnit output formats depending on what
     # version you're using and possibly whether you're on
-    # a windows box on are running on Mono
+    # a windows box or are running on Mono
     red_output_1 = 'Tests run: 1, Failures: 1'
     assert_equal :red, CodeOutputParser::parse_nunit(red_output_1)
     red_output_2 = 'Tests run: 3, Errors: 0, Failures: 3'
