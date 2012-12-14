7456d181b8f2604af21a18533ecdbc3a679f1aff
Jon Jagger <jon@jaggersoft.com>
2012-12-14 08:42:36 +0000

    refactoring

---
+++ app/assets/javascripts/cyber-dojo_dialog_start_coding.js
@@ -0,0 +1,38 @@
+/*global $,cyberDojo*/
+
+var cyberDojo = (function(cd, $) {
+  "use strict";
+
+  cd.dialog_startCoding = function(id, avatarName) {
+    var size = 120;
+    var html = '' 
+      + '<div align="center">' 
+      +   '<div class="medium">your animal is the</div>' 
+      +   '<img class="avatar_image"' 
+      +        'src="/images/avatars/' + avatarName + '.jpg"' 
+      +        'width="' + size + '"' 
+      +        'height="' + size + '"/>' 
+      +    '<div align="center">' 
+      +      '<div class="medium">' + avatarName + '</div>' 
+      +    '</div>' 
+      + '</div>';
+      
+    return $('<div class="dialog">')
+      .html(cd.startHtml(avatarName))
+      .dialog({
+        autoOpen: false,
+        title: cd.dialogTitle('start-coding'),
+        width: 400,
+        modal: true,
+        buttons: {
+          ok: function() {
+            cd.postTo('/kata/edit', { id: id, avatar: avatarName  }, '_blank');      
+            $(this).dialog('close');
+          } // ok:
+        } // buttons:
+      }); // .dialog({
+  }; // function() {
+  
+  return cd;
+})(cyberDojo || {}, $);
+
