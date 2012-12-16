d57a8f460334ad823516cd2b2672a5efc9d4272c
Jon Jagger <jon@jaggersoft.com>
2012-12-16 13:46:19 +0000

    more start/resume/review javascript refactoring

--- app/assets/javascripts/cyber-dojo_run.js
+++ app/assets/javascripts/cyber-dojo_run.js
@@ -3,10 +3,22 @@
 var cyberDojo = (function(cd, $) {
   "use strict";
   
-  cd.run = function(id, method, func) {
-    $.getJSON('/dojo/' + method, { id: id }, function(data) {
-      func(data);
-    });
+  cd.run = function(method, func) {    
+    var id = $('#kata_id_input').attr('value');
+    id = $.trim(id);
+    if (id === '') {
+      cd.dialog_noId().dialog('open');
+    }
+    else {
+      $.getJSON('/dojo/' + method, { id: id }, function(dojo) {
+        if (!dojo.exists) {
+          cd.dialog_cantFindDojo(id).dialog('open');
+        }
+        else {
+          func(id, dojo);
+        }
+      });
+    }
   };
 
   return cd;
