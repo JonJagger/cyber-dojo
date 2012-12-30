fa39a8c23c6c18b34037e2bb52c6476e6d3f1c57
Jon Jagger <jon@jaggersoft.com>
2012-12-30 23:30:39 +0000

    first cut at Clojure now live

---
+++ languages/Clojure/cyber-dojo.sh
@@ -0,0 +1,2 @@
+java -cp ./clojure-1.4.0.jar:. -Dclojure.compile.path=. clojure.main -i untitled.clj test_untitled.clj
+
---
+++ languages/Clojure/test_untitled.clj
@@ -0,0 +1,8 @@
+
+(ns untitled
+  (:use clojure.test))
+  
+(deftest test-answer
+  (is (= (* 6 9)  (answer))))
+
+(run-all-tests #"untitled")
