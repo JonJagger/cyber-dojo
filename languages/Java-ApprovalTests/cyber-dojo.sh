a121c86a2ac9611bd6ddb5fca2f0ca72cd8cc42c
emilybache <emily@bacheconsulting.com>
2013-02-04 13:30:18 +0100

    renamed Java-ApprovalTests to Java-Approval. Tests is kind of superfluous

--- languages/Java-ApprovalTests/cyber-dojo.sh
+++
@@ -1,4 +0,0 @@
-javac -cp .:./junit-4.11.jar:./ApprovalTests.012.jar *.java 
-if [ $? -eq 0 ]; then
-  java -cp .:./junit-4.11.jar:./ApprovalTests.012.jar org.junit.runner.JUnitCore `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
-fi
