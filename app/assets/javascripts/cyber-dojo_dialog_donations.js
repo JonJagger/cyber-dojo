ae0978b008f28e67d4554a50ccb464370840fab4
Jon Jagger <jon@jaggersoft.com>
2013-02-08 15:24:45 +0000

    another donator :-)

--- app/assets/javascripts/cyber-dojo_dialog_donations.js
+++ app/assets/javascripts/cyber-dojo_dialog_donations.js
@@ -21,7 +21,7 @@ var cyberDojo = (function(cd, $) {
         + "Alexander Ottesen, Lars Storjord, Anders Schau Knatten, "
         + "Mike Sutton, James Grenning, Allan Kelly, Randall White, "
         + "Lucas Videla, Mark Sargent, Jason Jolly, Mark Grimes, "
-        + "Pedro Gimenez, Marc Kannegiesser."
+        + "Pedro Gimenez, Marc Kannegiesser, Oliver Baylis."
       );
 
     var thankYou =
