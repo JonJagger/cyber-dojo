b6bd97fb4d2f3c3103db2b427afbada89a49f216
Jon Jagger <jon@jaggersoft.com>
2013-02-11 11:09:36 +0000

    Emily has added C# to Tennis refactoring page

--- app/assets/javascripts/cyber-dojo_dialog_refactoring.js
+++ app/assets/javascripts/cyber-dojo_dialog_refactoring.js
@@ -9,9 +9,9 @@ var cyberDojo = (function(cd, $) {
       cd.divPanel(''
         + 'Here are some refactoring katas for:'
         + '<br>'
-        + 'the <a href="http://jonjagger.blogspot.co.uk/2012/05/yahtzee-cyber-dojo-refactoring-in-java.html">Yahtzee kata</a> in Java, C#, C++, Python. '
+        + 'the <a href="http://jonjagger.blogspot.co.uk/2012/05/yahtzee-cyber-dojo-refactoring-in-java.html">Yahtzee kata</a> in C#, C++, Java, Python. '
         + '<br>'
-        + 'the <a href="http://emilybache.blogspot.co.uk/2013/01/setting-up-new-code-kata-in-cyber-dojo.html">Tennis kata</a> in C++, Ruby, Java, Python. '
+        + 'the <a href="http://emilybache.blogspot.co.uk/2013/01/setting-up-new-code-kata-in-cyber-dojo.html">Tennis kata</a> in C#, C++, Java, Python, Ruby. '
       );
 
     var table = $(cd.makeTable(refactor));
