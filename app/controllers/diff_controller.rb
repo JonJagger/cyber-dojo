4127e2cc7e50133f9076c35fb17a6c37fa3105be
Jon Jagger <jon@jaggersoft.com>
2013-02-15 10:46:58 +0000

    diff-page now has heartbeat which auto-refreshes the next/last buttons

--- app/controllers/diff_controller.rb
+++ app/controllers/diff_controller.rb
@@ -5,17 +5,10 @@ class DiffController < ApplicationController
   include GitDiff
   
   def show
-    @kata = Kata.new(root_dir, id)
-    @avatar = Avatar.new(@kata, params[:avatar])
-    traffic_lights = @avatar.increments    
+    setup_parameters    
     
-    @min_tag = 0
-    @was_tag = params[:was_tag].to_i
-    @now_tag = params[:now_tag].to_i
-    @max_tag = traffic_lights.length
-    
-    @was_traffic_light = traffic_lights[@was_tag - 1]
-    @now_traffic_light = traffic_lights[@now_tag - 1]
+    @was_traffic_light = @traffic_lights[@was_tag - 1]
+    @now_traffic_light = @traffic_lights[@now_tag - 1]
         
     visible_files = @avatar.visible_files(@now_tag)
     diffed_files = git_diff_view(@avatar, @was_tag, @now_tag, visible_files)    
@@ -24,6 +17,26 @@ class DiffController < ApplicationController
     @title = id[0..4] + ' ' + @avatar.name + ' diff' 
   end
    
+  def heartbeat
+    setup_parameters    
+    respond_to do |format|
+      format.js if request.xhr?
+    end
+  end
+  
+private
+
+  def setup_parameters
+    @kata = Kata.new(root_dir, id)    
+    @avatar = Avatar.new(@kata, params[:avatar])
+    @traffic_lights = @avatar.increments    
+    
+    @min_tag = 0
+    @was_tag = params[:was_tag].to_i
+    @now_tag = params[:now_tag].to_i
+    @max_tag = @traffic_lights.length
+  end
+  
 end
 
 
