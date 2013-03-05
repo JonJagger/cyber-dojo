fe8920afa97714f3dc305045a75feca259a8e8e4
Frej Connolly <frej@connolly.se>
2013-03-06 00:10:45 +0100

    Sentences end with a period. :)

--- languages/Python/test_untitled.py
+++ languages/Python/test_untitled.py
@@ -5,7 +5,7 @@ import untitled
 class TestUntitled(unittest.TestCase):
 
     def test_str(self):
-        """simple example to start you off"""
+        """Simple example to start you off."""
         obj = untitled.Untitled()
         self.assertEqual(6 * 9, obj.answer())
 
