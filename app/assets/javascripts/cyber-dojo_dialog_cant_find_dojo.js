65a1e25b7eb94fa9f961f05c2dee1fbb7efaded0
Jon Jagger <jon@jaggersoft.com>
2013-04-24 12:22:03 +0200

    simplified dialog messages for empty/duff id

--- app/assets/javascripts/cyber-dojo_dialog_cant_find_dojo.js
+++ app/assets/javascripts/cyber-dojo_dialog_cant_find_dojo.js
@@ -10,21 +10,13 @@ var cyberDojo = (function(cd, $) {
       +    '<tr>'
       +      '<td>'
       +        "I can't find a cyber-dojo with that id.<br/>"
-      +        '<br/>'
-      +        'A full id is always 10 characters long, '
-      +        'contains only the digits 0123456789 '
-      +        'and letters ABCDEF, and is case insensitive.<br/>'
-      +        '<br/>'
-      +        'You usually only need to enter the '
-      +        'first 5 characters of an id.'
-      +        '<br/>'
       +        'Click <div id="setup" class="button">setup</div> to get a new id<br/>'      
       +      '</td>'
       +    '</tr>'
       +  '</table>'
       + '</div>';
     var width, title;
-    return cd.dialog(cantFindDojo, width = 400, title = '? ' + id + ' ?');
+    return cd.dialog(cantFindDojo, width = 400, title = id);
   };
 
   return cd;
