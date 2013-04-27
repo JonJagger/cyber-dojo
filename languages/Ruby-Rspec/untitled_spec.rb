d5408e25b7dc9e6a8057d8fc8297a7ac6a067efb
Jon Jagger <jon@jaggersoft.com>
2013-04-27 08:56:56 +0100

    more idiomatic starting point for ruby-rspec files

--- languages/Ruby-Rspec/untitled_spec.rb
+++ languages/Ruby-Rspec/untitled_spec.rb
@@ -1,13 +1,11 @@
 require 'rspec'
-require 'test/unit/assertions'
 require './untitled'
 
 describe "untitled" do
-  include Test::Unit::Assertions
   
   context "when doing sums" do
     it "multiplies correctly" do
-      assert_equal 6 * 9, answer
+      (6 * 9).should == answer
     end
   end  
 
