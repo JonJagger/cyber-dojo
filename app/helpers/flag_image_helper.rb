8904bedc1bf3685ed1ca83e7ff59efeea1b7d5cf
Jon Jagger <jon@jaggersoft.com>
2013-06-11 11:33:26 +0100

    prepared English and French flags for i18n

---
+++ app/helpers/flag_image_helper.rb
@@ -0,0 +1,13 @@
+
+module FlagImageHelper
+  
+  def flag_image(name)
+    image_tag "/images/countries/#{name}.png",
+      :class => 'flag',
+      :alt   => '#{name} flag',
+      :title => '#{name}',
+      :width => 50,
+      :height => 30
+  end
+  
+end
