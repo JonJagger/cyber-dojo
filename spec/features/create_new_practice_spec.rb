fd58c872f5217e03074c1cb1197d5ec546d2a8c5
Michel Grootjans <michel.grootjans@gmail.com>
2013-06-11 14:05:56 +0200

    cleanup

--- spec/features/create_new_practice_spec.rb
+++ spec/features/create_new_practice_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# this is a huge badass feature test that verifies everyting at once
 feature "Start a practice" do
   scenario "Practice doesn't exist yet" do
     visit "/"
@@ -13,7 +14,12 @@ feature "Start a practice" do
     new_window = page.driver.browser.window_handles.last
 
     page.within_window new_window do
-      page.should have_text 'cyber-dojo.sh'
+      within '#filename_list' do
+        page.should have_text 'cyber-dojo.sh'
+        page.should have_text 'instructions'
+        page.should have_text 'untitled.rb'
+        page.should have_text 'untitled_spec.rb'
+      end
     end
 
   end
