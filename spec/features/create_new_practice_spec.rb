aae27b28e2e67a3ee0bdc93be321bc01462372e2
Michel Grootjans <michel.grootjans@gmail.com>
2013-06-11 10:42:13 +0200

    added the possibility to test http://www.cyber-dojo.com

--- spec/features/create_new_practice_spec.rb
+++ spec/features/create_new_practice_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 feature "Start a practice" do
   scenario "Practice doesn't exist yet" do
     visit "/"
-    
 
     find('#setup').click
     find('#language_ruby_rspec').click
