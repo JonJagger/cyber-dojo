c13954d373a62bf63248d808240fa854333bd821
Jon Jagger <jon@jaggersoft.com>
2013-07-10 10:46:29 +0100

    tweaked cyber-dojo.sh files for various languages; now that sandboxes are not deleted the way some of them determined what were tests wasn't working, eg old .class files were left in sandbox if .java files are renamed

--- app/controllers/kata_controller.rb
+++ app/controllers/kata_controller.rb
@@ -20,8 +20,8 @@ class KataController < ApplicationController
   def run_tests    
     incoming_hashes = params[:file_hashes_incoming]
     outgoing_hashes = params[:file_hashes_outgoing]
-    #Rails.logger.debug('FILE_HASHES_INCOMING:' + incoming_hashes.inspect);
-    #Rails.logger.debug('FILE_HASHES_OUTGOING:' + outgoing_hashes.inspect);    
+    Rails.logger.debug('FILE_HASHES_INCOMING:' + incoming_hashes.inspect);
+    Rails.logger.debug('FILE_HASHES_OUTGOING:' + outgoing_hashes.inspect);    
     delta = FileHashDiffer.diff(incoming_hashes, outgoing_hashes)
     
     @kata   = Kata.new(root_dir, id)
--- languages/Java-Cucumber/cyber-dojo.sh
+++ languages/Java-Cucumber/cyber-dojo.sh
@@ -1,4 +1,4 @@
-
+rm *.class
 javac -cp .:`ls *.jar | tr '\n' ':'`  *.java   
 if [ $? -eq 0 ]; then
   java -cp .:`ls *.jar | tr '\n' ':'` org.junit.runner.JUnitCore RunCukesTest
