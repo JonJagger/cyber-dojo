9c10d2fe3b13a9c739636005d5ad77d3a18a145d
Jon Jagger <jon@jaggersoft.com>
2013-07-12 14:00:29 +0100

    tweaked pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# This script contains the commands used to install the latest
+# cyberdojo git repo onto cyber-dojo.com
 
 git pull
-chown -R www-data cyberdojo
-chgrp -R www-data cyberdojo
+chown -R www-data /var/www/cyberdojo
+chgrp -R www-data /var/www/cyberdojo
 bundle install
 service apache2 restart
