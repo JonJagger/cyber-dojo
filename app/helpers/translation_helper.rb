890c1440642e2cf74f96b1d927633839b63fe727
Jon Jagger <jon@jaggersoft.com>
2013-08-05 18:50:43 +0100

    language/exercise/id now displayed rather than being content of dialog

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -3,10 +3,5 @@ module TranslationHelper
   def translate_avatar(name)
     t "views.avatars.#{name}"
   end
-  
-  def translate_month_kata_created(kata)
-    month_name = kata.created.strftime("%B")
-    t "views.months.#{month_name}"
-  end
 
 end
