0f3af7768a00c5a9ced358794b1535513d4afac3
Jon Jagger <jon@jaggersoft.com>
2013-08-08 13:55:01 +0100

    diff-view autoscroll improvement

--- app/assets/javascripts/cyber-dojo_build_diff_filenames.js
+++ app/assets/javascripts/cyber-dojo_build_diff_filenames.js
@@ -29,28 +29,21 @@ var cyberDojo = (function(cd, $) {
     };
 
     var previousFilenameNode;
+    var alreadyOpened = [ ];
     
-    var loadFrom = function(filenameNode, diff) {
-      // assert filename.val() == diff[:filename]
+    var loadFrom = function(filenameNode, id, sectionCount) {
       
       var diffSheet = cd.fileContentFor(filenameNode.val());
       var sectionIndex = 0;
-      var sectionCount = diff.section_count;
-      var id = diff.id;
       
       if (sectionCount > 0) {
           filenameNode.parent().attr('title', 'Auto-scroll through diffs');
       }
       
       return function() {
-        
-        // .hide() and .show() calls restore the cursorPos and scrollPos
-        // correctly (automatically) on files that have no diffs.
-        // However, if the file has a diff it will auto-scroll to the
-        // first diff-section (when opened) rather than leaving the
-        // position where it was. This could be refactored so autoscroll
-        // only happens when a file is selected for the first time
-        // or when the current file is explicitly reselected.
+        var filename = filenameNode.val();
+        var reselected =
+          previousFilenameNode !== undefined && previousFilenameNode.val() === filename;
         
         cd.radioEntrySwitch(previousFilenameNode, filenameNode);
         if (previousFilenameNode !== undefined) {
@@ -58,26 +51,28 @@ var cyberDojo = (function(cd, $) {
         }
         cd.fileDiv(filenameNode.val()).show();
         previousFilenameNode = filenameNode;
-        // some files have no diffs
-        if (sectionCount > 0) {
+        
+        if (sectionCount > 0 && (reselected || !cd.inArray(filename, alreadyOpened))) {
           var section = $('#' + id + '_section_' + sectionIndex);           
-          var downFromTop = 100;
-          var halfSecond = 500;          
+          var downFromTop = 150;
+          var halfSecond = 500;
           diffSheet.animate({
             scrollTop: section.offset().top - diffSheet.offset().top - downFromTop
             }, halfSecond);
           sectionIndex += 1;
           sectionIndex %= sectionCount;
-        }        
+        }
+        alreadyOpened.push(filename);                
       };
     };
     
     $.each(diffs, function(n, diff) {
       // _filenames.html.erb contains an
-      // <input type="radio" id="radio_<%= diff[:id] %>" />
+      // <input id="radio_<%= diff[:id] %>" ... />
       // for each file in the current diff.
       var filenameNode = $('#radio_' + diff.id);
-      filenameNode.parent().click(loadFrom(filenameNode, diff));
+      filenameNode.parent().click(loadFrom(filenameNode, diff.id, diff.section_count));
+      // assert filenameNode.val() == diff.filename
       bindLineNumbers(diff.filename);
     });
   };
