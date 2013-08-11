cee6dc2cb98239c7e3e0498e2e47d853b1d6f37c
Jon Jagger <jon@jaggersoft.com>
2013-08-11 09:12:23 +0100

    updated pull.sh so it doesn't try to chmod katas folder

--- pull.sh
+++ pull.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-# This script contains the commands used to install the latest
-# cyberdojo git repo onto cyber-dojo.com
+# Use to install the latest cyberdojo git repo onto cyber-dojo.com
 
+# get latest source from https://github.com/JonJagger/cyberdojo
+# if it asks for a password just hit return
 git pull
-chown -R www-data /var/www/cyberdojo
-chgrp -R www-data /var/www/cyberdojo
+
+# ensure pulled files have correct rights
+# don't chmod or chgrp the katas folder
+for folder in app config exercises languages lib public script spec test
+do
+  eval "chown -R www-data /var/www/cyberdojo/$folder"
+  eval "chgrp -R www-data /var/www/cyberdojo/$folder"
+done
+
+eval "chown www-data /var/www/cyberdojo/*"
+eval "chgrp www-data /var/www/cyberdojo/*"
+
+# poke rails
 bundle install
+
+# restart apache
 service apache2 restart
