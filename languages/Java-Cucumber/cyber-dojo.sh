6f135d8523b792c15c722134a41ae68a7a7e5887
Jon Jagger <jon@jaggersoft.com>
2013-09-22 16:03:15 +0100

    removed unneeded 'rm -tf *.class' line from start of cyber-dojo.sh files for all Java languages - bug fix from earlier where spaces in filenames showed need for ' quoted filenames means rm is not needed and will slow down execution

--- languages/Java-Cucumber/cyber-dojo.sh
+++ languages/Java-Cucumber/cyber-dojo.sh
@@ -1,4 +1,3 @@
-rm -f *.class
 javac -cp .:`ls *.jar | tr '\n' ':'`  *.java   
 if [ $? -eq 0 ]; then
   java -cp .:`ls *.jar | tr '\n' ':'` org.junit.runner.JUnitCore RunCukesTest
--- languages/Java-JUnit/cyber-dojo.sh
+++ languages/Java-JUnit/cyber-dojo.sh
@@ -1,4 +1,3 @@
-rm -f *.class
 javac -cp .:./junit-4.11.jar *.java 
 if [ $? -eq 0 ]; then
   java -cp .:./junit-4.11.jar org.junit.runner.JUnitCore `ls -1 *Test*.class | grep -v '\\$' | sed 's/\(.*\)\..*/\1/'`
