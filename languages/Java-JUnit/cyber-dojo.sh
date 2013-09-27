8fecb2fabac84b7091de8695b936d203d20839a8
Jon Jagger <jon@jaggersoft.com>
2013-09-27 14:05:23 +0100

    added rm *.class to Java shell files

--- languages/Java-JUnit/cyber-dojo.sh
+++ languages/Java-JUnit/cyber-dojo.sh
@@ -1,3 +1,4 @@
+rm -f *.class
 javac -cp .:./junit-4.11.jar *.java 
 if [ $? -eq 0 ]; then
   java -cp .:./junit-4.11.jar org.junit.runner.JUnitCore `ls -1 *Test*.class | grep -v '\\$' | sed 's/\(.*\)\..*/\1/'`
