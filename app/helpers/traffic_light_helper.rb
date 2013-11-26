70f2b92586612d9f054d4102c7f392033236180e
Jon Jagger <jon@jaggersoft.com>
2013-11-26 09:32:39 +0000

    diff-review dialog now opens to animals current code when the animals image is clicked on dashboard

--- app/helpers/traffic_light_helper.rb
+++ app/helpers/traffic_light_helper.rb
@@ -1,8 +1,7 @@
 
 module TrafficLightHelper
 
-  def diff_traffic_light(kata, avatar_name, light, max_lights)
-    colour = light[:colour].to_s    
+  def diff_traffic_light(kata, avatar_name, light, max_lights)    
     ("<span class='diff-traffic-light'" +
           " title='#{tool_tip(avatar_name,light)}'" +
           " data-id='#{kata.id}'" +
@@ -10,14 +9,30 @@ module TrafficLightHelper
           " data-was-tag='#{light[:number]-1}'" +
           " data-now-tag='#{light[:number]}'" +
           " data-max-tag='#{max_lights}'>" +
-      "<img src='/images/traffic_light_#{colour}.png'" +
-          " alt='#{colour} traffic-light'" +
+      "<img src='/images/traffic_light_#{light[:colour].to_s}.png'" +
+          " alt='#{light[:colour].to_s} traffic-light'" +
           " width='20'" +
           " height='62'/>" +           
      "</span>"
-    ).html_safe
+    ).html_safe    
   end
 
+  def no_diff_avatar_image(kata, avatar_name, light, max_lights)
+    ("<span class='diff-traffic-light'" +
+          " title='Show #{avatar_name}'s current code'" +
+          " data-id='#{kata.id}'" +
+          " data-avatar-name='#{avatar_name}'" +
+          " data-was-tag='#{light[:number]}'" +
+          " data-now-tag='#{light[:number]}'" +
+          " data-max-tag='#{max_lights}'>" +
+      "<img src='/images/avatars/#{avatar_name}.jpg'" +
+          " alt='#{avatar_name}'" +
+          " width='45'" +
+          " height='45'/>" +           
+     "</span>"
+    ).html_safe    
+  end
+  
   def linked_traffic_light(kata, avatar_name, inc, in_new_window)
     new_window = in_new_window ? { :target => '_blank' } : { }
     
@@ -59,13 +74,13 @@ module TrafficLightHelper
      "</span>").html_safe
   end
  
-  def tool_tip(avatar_name, inc)
-    n = inc[:number]
-    "Show #{avatar_name}s diff #{n-1} <-> #{n} (#{at(inc)})"
+  def tool_tip(avatar_name, light)
+    n = light[:number]
+    "Show #{avatar_name}s diff #{n-1} <-> #{n} (#{at(light)})"
   end
     
-  def at(inc)
-    Time.mktime(*inc[:time]).strftime("%Y %b %-d, %H:%M:%S")    
+  def at(light)
+    Time.mktime(*light[:time]).strftime("%Y %b %-d, %H:%M:%S")    
   end
   
 end
