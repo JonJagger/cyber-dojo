c0bbb508c956fa4fd1dd9b22b020072ff7ec42a1
Jon Jagger <jon@jaggersoft.com>
2013-11-26 10:05:15 +0000

    removed dead code

--- app/helpers/traffic_light_helper.rb
+++ app/helpers/traffic_light_helper.rb
@@ -3,7 +3,7 @@ module TrafficLightHelper
 
   def diff_traffic_light(kata, avatar_name, light, max_lights)    
     ("<span class='diff-traffic-light'" +
-          " title='#{tool_tip(avatar_name,light)}'" +
+          ' title="' + tool_tip(avatar_name,light) + '"' +
           " data-id='#{kata.id}'" +
           " data-avatar-name='#{avatar_name}'" +
           " data-was-tag='#{light[:number]-1}'" +
@@ -32,51 +32,10 @@ module TrafficLightHelper
      "</span>"
     ).html_safe    
   end
-  
-  def linked_traffic_light(kata, avatar_name, inc, in_new_window)
-    new_window = in_new_window ? { :target => '_blank' } : { }
-    
-    link_to untitled_unlinked_traffic_light(inc), 
-    {   :controller => :diff, 
-        :action => :show,
-        :id => kata.id,
-        :avatar => avatar_name,
-        :was_tag => inc[:number]-1,
-        :now_tag => inc[:number] 
-    }, 
-    { :title => tool_tip(avatar_name, inc),
-    }.merge(new_window)
-  end
-  
-  def untitled_unlinked_traffic_light(inc, width = nil, height = nil)
-    width ||= 20
-    height ||= 62
-    colour = inc[:colour].to_s
-    filename = "traffic_light_#{colour}"
-    base_traffic_light_image(filename + '.png',colour,width,height)
-  end
-  
-  def traffic_light_image(colour, width, height)
-    base_traffic_light_image("traffic_light_#{colour}.png",colour,width,height)
-  end
-  
-  def base_traffic_light_image(filename, colour, width, height)
-    ("<img src='/images/#{filename}'" +
-      " alt='#{colour} traffic-light'" +
-      " width='#{width}'" +
-      " height='#{height}'/>").html_safe    
-  end
-  
-  def unlinked_traffic_light(inc, width = nil, height = nil)
-    bulb = inc[:colour].to_s
-    ("<span title='#{at(inc)}'>" +
-     untitled_unlinked_traffic_light(inc, width, height) +
-     "</span>").html_safe
-  end
  
   def tool_tip(avatar_name, light)
     n = light[:number]
-    "Show #{avatar_name}s diff #{n-1} <-> #{n} (#{at(light)})"
+    "Show #{avatar_name}'s diff #{n-1} <-> #{n} (#{at(light)})"
   end
     
   def at(light)
--- test/helpers/traffic_light_tests.rb
+++ test/helpers/traffic_light_tests.rb
@@ -1,89 +1,12 @@
 require File.dirname(__FILE__) + '/../test_helper'
 require 'traffic_light_helper'
 
-class StubKata
-  
-  def initialize(id)
-    @id = id
-  end
-  
-  def id
-    @id
-  end
-  
-end
-
 class TrafficLightTests < ActionView::TestCase
 
-  include TrafficLightHelper
-    
-  test "linked_traffic_light" do
-    kata = StubKata.new(id=5335)
-    avatar_name = 'lion'
-    inc = make_inc
-    inc[:colour] = :red
-    tag = 45
-    inc[:number] = tag
-    inc[:revert_tag] = nil
-    in_new_window = false
-    html = linked_traffic_light(kata, avatar_name, inc, in_new_window)
-    assert html.start_with?("<a "), '<a :' + html
-    assert html.match("href=\"/diff/show/#{id}"), 'href: ' + html
-    assert html.match("title=\"Show #{avatar_name}'s diff #{tag-1} -&gt; #{tag}"), 'title: ' + html
-    assert html.match("src='/images/traffic_light_red.png"), 'src: ' + html
-    assert html.match("alt='red traffic-light'"), 'alt: ' + html
-    assert html.match("width='20'"), 'width: ' + html
-    assert html.match("height='62'"), 'height: ' + html
-  end
-
-  test "traffic_light_image" do
-    colour = 'red'
-    width = 44
-    height = 34
-    actual = traffic_light_image(colour, width, height)
-    assert actual.start_with? '<img '
-    assert actual.match "src='/images/traffic_light_#{colour}.png'"
-    assert actual.match "alt='#{colour} traffic-light'"
-    assert actual.match "width='#{width}'"
-    assert actual.match "height='#{height}'"
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    
-  test "unlinked traffic light red" do
-    inc = make_inc
-    inc[:colour] = :red
-    expected =
-      "<span title='2012 May 1, 23:20:45'>" +    
-      "<img src='/images/traffic_light_red.png' alt='red traffic-light' width='20' height='62'/>" +
-      "</span>"
-    assert_equal expected, unlinked_traffic_light(inc) 
-  end
-  
-  test "unlinked traffic light amber" do
-    inc = make_inc
-    inc[:colour] = :amber
-    expected =
-      "<span title='2012 May 1, 23:20:45'>" +    
-      "<img src='/images/traffic_light_amber.png' alt='amber traffic-light' width='20' height='62'/>" +
-      "</span>"
-    assert_equal expected, unlinked_traffic_light(inc) 
-  end
-  
-  test "unlinked traffic light green" do
-    inc = make_inc
-    inc[:colour] = :green
-    expected =
-      "<span title='2012 May 1, 23:20:45'>" +
-      "<img src='/images/traffic_light_green.png' alt='green traffic-light' width='20' height='62'/>" +
-      "</span>"
-    assert_equal expected, unlinked_traffic_light(inc) 
-  end
-  
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  include TrafficLightHelper    
   
   test "tool tip" do
-    assert_equal "Show hippo's diff 1 -> 2 (2012 May 1, 23:20:45)",
+    assert_equal "Show hippo's diff 1 <-> 2 (2012 May 1, 23:20:45)",
       tool_tip('hippo', make_inc)
   end
   
