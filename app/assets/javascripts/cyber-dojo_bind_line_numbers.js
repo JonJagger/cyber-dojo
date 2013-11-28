8e7aae4a0a58836e1d87e515123ae4999eeb6cd2
Jon Jagger <jon@jaggersoft.com>
2013-11-28 23:22:17 +0000

    refactoring

--- app/assets/javascripts/cyber-dojo_bind_line_numbers.js
+++ app/assets/javascripts/cyber-dojo_bind_line_numbers.js
@@ -3,12 +3,6 @@
 var cyberDojo = (function(cd, $) {
   "use strict";
   
-  cd.lineNumbersFor = function(filename) {
-    return cd.id(filename + '_line_numbers');  
-  };
-
-  //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
   cd.bindAllLineNumbers = function() {
     $.each(cd.filenames(), function(i, filename) {
       cd.bindLineNumbers(filename);
@@ -17,22 +11,6 @@ var cyberDojo = (function(cd, $) {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  cd.bindLineNumbersEvents = function(filename) {
-    var content = cd.fileContentFor(filename);
-    var numbers = cd.lineNumbersFor(filename);
-    var setLine = function() {
-      numbers.scrollTop(content.scrollTop());   
-    }    
-    content.bind({
-      keydown   : setLine,
-      scroll    : setLine,
-      mousewheel: setLine,
-      mousemove : setLine,
-      mousedown : setLine,
-      mouseup   : setLine 
-    });    
-  };
-  
   cd.bindLineNumbers = function(filename) {
     var numbers = cd.lineNumbersFor(filename);
     cd.bindLineNumbersEvents(filename);    
@@ -42,6 +20,30 @@ var cyberDojo = (function(cd, $) {
 
   //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+  cd.lineNumbersFor = function(filename) {
+    return cd.id(filename + '_line_numbers');  
+  };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  cd.bindLineNumbersEvents = function(filename) {
+    var content = cd.fileContentFor(filename);
+    var numbers = cd.lineNumbersFor(filename);
+    var setLine = function() {
+      numbers.scrollTop(content.scrollTop());   
+    };
+    content.bind({
+      keydown   : setLine,
+      scroll    : setLine,
+      mousewheel: setLine,
+      mousemove : setLine,
+      mousedown : setLine,
+      mouseup   : setLine 
+    });    
+  };
+
+  //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
   cd.lineNumbers = (function() {
     var number, lines = '1';
     for (number = 2; number < 999; number += 1) {
