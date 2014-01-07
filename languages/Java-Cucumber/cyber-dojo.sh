aa3b83d33469b88dceff63e67f68c175634989ee
Seb Rose <seb@claysnow.co.uk>
2014-01-07 19:25:48 +0000

    Ensure that all files named *Test* get run

--- languages/Java-Cucumber/cyber-dojo.sh
+++ languages/Java-Cucumber/cyber-dojo.sh
@@ -2,7 +2,6 @@ rm -f *.class
 CLASSES=.:`ls *.jar | tr '\n' ':'`
 javac -cp $CLASSES  *.java   
 if [ $? -eq 0 ]; then
-  java -cp $CLASSES org.junit.runner.JUnitCore RunCukesTest
+  java -cp $CLASSES org.junit.runner.JUnitCore `ls -1 *Test*.class | grep -v '\\$' | sed 's/\(.*\)\..*/\1/'`
 fi
-
 
