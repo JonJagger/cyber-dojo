73337d57fd4e61b50ef85b5e8a055d1282893492
raphael meyer <raphael.meyer@bbv.ch>
2014-02-06 00:04:18 +0100

    Deploy google mock example

--- languages/C++-GoogleMock/untitled.hpp
+++ languages/C++-GoogleMock/untitled.hpp
@@ -1,6 +1,16 @@
 #ifndef UNTITLED_INCLUDED
 #define UNTITLED_INCLUDED
 
-int hhg();
+class UntitledHelper;
+
+class Untitled {
+public:
+    Untitled(UntitledHelper const & _helper);
+
+    int answer();
+
+private:
+    UntitledHelper const & helper;
+};
 
 #endif
--- languages/C++-GoogleMock/untitled.tests.cpp
+++ languages/C++-GoogleMock/untitled.tests.cpp
@@ -1,9 +1,21 @@
 #include "untitled.hpp"
+#include "untitled_helper.hpp"
 #include "gmock/gmock.h"
 
 using namespace ::testing;
 
+class UntitledHelperMock : public UntitledHelper {
+public:
+    MOCK_CONST_METHOD0(answer, int());
+};
+
 TEST(UntitledTest, HitchHiker)
 {
-    ASSERT_THAT(6 * 9, hhg());
+    UntitledHelperMock helper;
+    Untitled target(helper);
+
+    EXPECT_CALL(helper, answer())
+      .WillOnce(Return(42));
+
+    ASSERT_THAT(target.answer(), Eq(6 * 9));
 }
