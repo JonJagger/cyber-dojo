5da2e7d0df6629999d892c92670a9b2bd32cce83
Jon Jagger <jon@jaggersoft.com>
2014-02-06 09:51:13 +0000

    increased max_duration of C++Catch in installation test

--- test/installation/cpp_catch_tests.rb
+++ test/installation/cpp_catch_tests.rb
@@ -3,7 +3,8 @@ require File.dirname(__FILE__) + '/one_language_checker'
 class CppCatchTests < ActionController::TestCase
   
   test "C++-Catch" do
-    OneLanguageChecker.new.check('C++-Catch')        
+    options = { :verbose => true, :max_duration => 15 }
+    OneLanguageChecker.new(options).check('C++-Catch')        
   end
   
 end
