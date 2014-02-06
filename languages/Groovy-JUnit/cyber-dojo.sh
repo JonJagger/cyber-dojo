445da46dcc3969c44ccb0d7389438367de8a5edf
Jon Jagger <jon@jaggersoft.com>
2014-02-06 20:58:06 +0000

    add -f option when using rm *.class so you don't get a warning if there are no .class files

--- languages/Groovy-JUnit/cyber-dojo.sh
+++ languages/Groovy-JUnit/cyber-dojo.sh
@@ -1,4 +1,4 @@
-rm *.class
+rm -f *.class
 groovyc *.groovy
 if [ $? -eq 0 ]; then
   java -cp .:$(ls *.jar | xargs | sed -e 's/ /:/g') org.junit.runner.JUnitCore `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
