e86abf200f758b6b6179ae0f156ed68000ed2c99
Jon Jagger <jon@jaggersoft.com>
2014-02-07 13:55:09 +0000

    added missing helper test

---
+++ app/helpers/my_translation_helper.rb
@@ -0,0 +1,11 @@
+
+# if I call this TranslationHelper
+# then I am unable to access it from its tests!?
+
+module MyTranslationHelper
+  
+  def translate_avatar(name)
+    t "views.avatars.#{name}"
+  end
+  
+end
---
+++ test/app_helpers/translation_tests.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'my_translation_helper'
+
+class TranslationTests < ActionView::TestCase
+
+  include MyTranslationHelper
+  
+  test "frog" do
+    assert_equal "frog", translate_avatar('frog')
+  end
+
+end
+
+
