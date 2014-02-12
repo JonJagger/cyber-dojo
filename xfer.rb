5e6630fb1992c6fa7816cd364180848d9782b273
Jon Jagger <jon@jaggersoft.com>
2014-02-12 11:12:03 +0000

    added lo-hi parameters to xfer.rb to limit the xfers

--- xfer.rb
+++ xfer.rb
@@ -1,9 +1,16 @@
-# A ruby script to download zips of dojos identified by the output of
-# the prunt_large.rb script
+# A ruby script to download zips of dojos identified by
+# the output of the prunt_large.rb script
 
-`cat #{ARGV[0]}`.scan(/^will rm ([A-Z0-9]*)/) do |md|
-  id = md[0]
-  p id
-  xfer_cmd = "wget -O #{id}.tar.gz http://cyber-dojo.com/dashboard/download/#{id}"
-  `#{xfer_cmd}`
+txt_file = ARGV[0]
+lo = (ARGV[1] || "0").to_i
+hi = (ARGV[2] || "10000").to_i
+
+`cat #{ARGV[0]}`.scan(/^will rm ([A-Z0-9]*) ([0-9]*)/) do |matches|
+  id = matches[0]
+  rags = matches[1].to_i
+  if lo <= rags && rags <= hi
+    p id + " " + rags.to_s
+    xfer_cmd = "wget -O #{id}.tar.gz http://cyber-dojo.com/dashboard/download/#{id}"
+    `#{xfer_cmd}`
+  end
 end
