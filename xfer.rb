c6c60a3ac186ef5c849a2cc14c0a7d7072c7006f
Jon Jagger <jon@jaggersoft.com>
2014-02-12 11:30:12 +0000

    added -q to xfer's wget

--- xfer.rb
+++ xfer.rb
@@ -10,7 +10,7 @@ hi = (ARGV[2] || "10000").to_i
   rags = matches[1].to_i
   if lo <= rags && rags <= hi
     p id + " " + rags.to_s
-    xfer_cmd = "wget -O #{id}.tar.gz http://cyber-dojo.com/dashboard/download/#{id}"
+    xfer_cmd = "wget -q -O #{id}.tar.gz http://cyber-dojo.com/dashboard/download/#{id}"
     `#{xfer_cmd}`
   end
 end
