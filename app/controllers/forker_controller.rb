a8155f0ff24e73dbde54f9cf5e06af7e659f6c9a
Jon Jagger <jon@jaggersoft.com>
2014-02-18 14:57:43 +0000

    refactoring

--- app/controllers/forker_controller.rb
+++ app/controllers/forker_controller.rb
@@ -2,43 +2,39 @@
 class ForkerController < ApplicationController
 
   def fork    
-    result = { }    
-    bad = false
+    result = { :forked => false }    
+    error = false
     
     kata = dojo[params['id']]
     
-    if !bad && !kata.exists?      
-      result[:forked] = false
+    if !error && !kata.exists?
       result[:reason] = "id"
-      bad = true
+      error = true
     end
     
-    if !bad && !dojo.language(kata.language.name).exists?
-      result[:forked] = false
+    if !error && !dojo.language(kata.language.name).exists?
       result[:reason] = "language"
       result[:language] = kata.language.name
-      bad = true
+      error = true
     end
     
-    if !bad && !Avatar.new(kata, params['avatar']).exists?
-      result[:forked] = false
+    if !error && !Avatar.new(kata, params['avatar']).exists?
       result[:reason] = "avatar"
-      bad = true
+      error = true
     end
 
-    if !bad
+    if !error
       avatar = Avatar.new(kata, params['avatar'])
       traffic_lights = avatar.traffic_lights
       is_tag = params['tag'].match(/^\d+$/)
       tag = params['tag'].to_i;
       if !is_tag || tag <= 0 || tag > traffic_lights.length
-        result[:forked] = false
         result[:reason] = "tag"
-        bad = true        
+        error = true        
       end
     end
 
-    if !bad    
+    if !error    
       manifest = make_manifest(kata.language.name, kata.exercise.name)
       manifest[:visible_files] = avatar.visible_files(params['tag'])    
       dojo.create_kata(manifest)
