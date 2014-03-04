ce5658b782f5e0f0e81a6581d696c7488cefef76
Jon Jagger <jon@jaggersoft.com>
2014-03-04 18:13:07 +0000

    added Xlint option to javac calls in cyber-dojo.sh files

--- languages/Java-Approval/cyber-dojo.sh
+++ languages/Java-Approval/cyber-dojo.sh
@@ -1,6 +1,6 @@
 rm -f *Test*.class
 CLASSES=.:`ls *.jar | tr '\n' ':'`
-javac -cp $CLASSES *.java 
+javac -Xlint:unchecked -cp $CLASSES *.java
 if [ $? -eq 0 ]; then
   java -cp $CLASSES org.junit.runner.JUnitCore `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
 fi
