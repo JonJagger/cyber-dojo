3a0c94f91dc9e5653453409425de4f7f95bcdcea
Jon Jagger <jon@jaggersoft.com>
2014-03-28 16:43:55 +0000

    updated small docker test

--- test/app_models/docker_parse_tests.rb
+++ test/app_models/docker_parse_tests.rb
@@ -3,28 +3,25 @@ require __DIR__ + '/../test_helper'
 
 class DockerParseTests < ActionController::TestCase
 
-  test "parse 'docker ps -a' output" do
+  test "parse 'docker images' output in prep for languages_each()" do
     output = [
-      "ID                  IMAGE               COMMAND               CREATED             STATUS              PORTS",
-      "d93657f6b9ff        base:latest         /bin/bash -c ls -al   3 seconds ago       Exit 0          ",
-      "3c639694a878        base:latest         /bin/bash -c ls -al   22 seconds ago      Exit 0          "
+      "REPOSITORY                            TAG                 IMAGE ID            CREATED             VIRTUAL SIZE",
+      "cyberdojo/language_ruby-1.9.3         latest              48f93a0a2bb4        3 hours ago         328.3 MB",
+      "cyberdojo/language_gcc-4.8.1_assert   latest              e8ac00b6fa7b        3 hours ago         310.6 MB",
+      "cyberdojo/language_gpp-4.8.1_assert   latest              e8ac00b6fa7b        3 hours ago         310.6 MB",
+      "cyberdojo/build-essential             latest              bc54c666c7c1        6 hours ago         310.6 MB",
+      "ubuntu                                13.10               9f676bd305a4        7 weeks ago         178 MB",
+      "ubuntu                                saucy               9f676bd305a4        7 weeks ago         178 MB",
     ].join("\n")
 
-    lines = output.lines.entries[1..-1]
-    ids = lines.each.collect {|line| line.split[0] }
-    assert_equal [ 'd93657f6b9ff', '3c639694a878' ], ids
-  end
-
-  test "parse 'docker images' output" do
-    output = [
-      "REPOSITORY          TAG                 ID                  CREATED             SIZE",
-      "node                latest              d8c41048dc9a        6 months ago        293.3 MB (virtual 473.5 MB)",
-      "base                latest              b750fe79269d        11 months ago       24.65 kB (virtual 180.1 MB)"
-    ].join("\n")
-
-    lines = output.lines.entries[1..-1]
-    ids = lines.each.collect {|line| line.split[2] }
-    assert_equal [ 'd8c41048dc9a', 'b750fe79269d' ], ids
+    repos = output.lines.each.collect{|line| line.split[0]}.select{|repo|
+      repo.start_with? 'cyberdojo/language_'
+    }
+    assert_equal [
+      'cyberdojo/language_ruby-1.9.3',
+      'cyberdojo/language_gcc-4.8.1_assert',
+      'cyberdojo/language_gpp-4.8.1_assert',
+    ].sort, repos.sort
   end
 
 end
