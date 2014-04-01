ba0c6deab31ce9f268d61f506e2316ae1a100da8
Jon Jagger <jon@jaggersoft.com>
2014-04-01 19:40:35 +0100

    updated RawRunner tests

--- test/lib/raw_runner_tests.rb
+++ test/lib/raw_runner_tests.rb
@@ -3,18 +3,26 @@ require 'RawRunner'
 
 class RawRunnerTests < ActionController::TestCase
 
+  class StubPaas
+    def path(sandbox)
+      '.'
+    end
+  end
+
   test "command executes within timeout and returns command output" do
-    path = '.'
+    paas = StubPaas.new
+    sandbox = Object.new
     command = 'echo "Hello"'
     max_duration = 2 # seconds
-    assert_equal "Hello\n", RawRunner.new.run(path,command,max_duration)
+    assert_equal "Hello\n", RawRunner.new.run(paas, sandbox, command,max_duration)
   end
 
   test "command times-out returns termination output" do
-    path = '.'
+    paas = StubPaas.new
+    sandbox = Object.new
     command = 'sleep 10000'
     max_duration = 1 # second
-    output = RawRunner.new.run(path,command,max_duration)
+    output = RawRunner.new.run(paas, sandbox, command, max_duration)
     assert_not_nil output =~ /Terminated by the cyber-dojo server after 1 seconds/, output
   end
 
