280fa2a78a132af7433e6e12dbc455c4acf073f8
Jon Jagger <jon@jaggersoft.com>
2014-04-02 12:53:05 +0100

    remove word Docker from DockerRunner output

--- app/lib/DockerRunner.rb
+++ app/lib/DockerRunner.rb
@@ -24,7 +24,6 @@ class DockerRunner
     # longer than the max_seconds limit.
 
     kill = 9
-    # timeout not available on OSX ...
     `timeout --signal=#{kill} #{max_seconds}s #{cmd}`
 
     exitstatus = $?.exitstatus
@@ -32,30 +31,7 @@ class DockerRunner
     cid = `cat #{cid_filename}`
 
     output = (exitstatus != killed_by_timeout) ? `docker logs #{cid}` :
-       "Terminated by the cyber-dojo Docker server after #{max_seconds} seconds."
+       "Terminated by the cyber-dojo server after #{max_seconds} seconds."
   end
 
-private
-
 end
-
-# To run docker on OSX
-# http://zaiste.net/2014/02/lightweight_docker_experience_on_osx/
-#
-# $curl -o docker http://get.docker.io/builds/Darwin/x86_64/docker-latest
-# $chmod +x ./docker
-# $mv docker ~/bin
-#
-# $curl https://raw.github.com/steeve/boot2docker/master/boot2docker > boot2docker
-# $chmod a+x boot2docker
-# $mv boot2docker ~/bin/boot2docker
-#
-# $~/bin/boot2docker init
-# $~/bin/boot2docker up
-# $export DOCKER_HOST=tcp://localhost:4243
-#
-# $~/bin/docker version
-#
-# and I do not get a
-# dial unix /var/run/docker.sock: no such file or directory
-# error message
