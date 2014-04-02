fa803898aaeb2dbc7247b2adc57768c481bde136
Jon Jagger <jon@jaggersoft.com>
2014-04-02 15:44:43 +0100

    that worked. Now trying without --rm

--- app/lib/DockerRunner.rb
+++ app/lib/DockerRunner.rb
@@ -6,15 +6,15 @@ class DockerRunner
   def run(paas, sandbox, command, max_seconds)
     Rails.logger.warn("DockerRunner")
     # TODO: move this out of katas/ subfolder
-    cid_filename = paas.path(sandbox) + 'store.cid'
+    #cid_filename = paas.path(sandbox) + 'store.cid'
 
     `rm -f #{cid_filename}`
     language = sandbox.avatar.kata.language
-    cmd = "docker run -u www-data" +
+    cmd = "docker run -u www-data --rm" +
           " -v #{paas.path(sandbox)}:/sandbox:rw" +
           " -v #{paas.path(language)}:#{paas.path(language)}:ro" +
           " -w /sandbox" +
-          " --cidfile=\"#{cid_filename}\"" +
+          #" --cidfile=\"#{cid_filename}\"" +
           " #{language.image_name} /bin/bash -c \"#{with_stderr(command)}\""
 
     # timeout must go on 'docker run' command and not on
@@ -26,21 +26,19 @@ class DockerRunner
     Rails.logger.warn("DockerRunner.run " + cmd)
 
     kill = 9
-    raw = `timeout --signal=#{kill} #{max_seconds}s #{cmd}`
-    exitstatus = $?.exitstatus
+    output = `timeout --signal=#{kill} #{max_seconds}s #{cmd}`
+    exit_status = $?.exitstatus
     killed_by_timeout = 128+kill
 
-    Rails.logger.warn("raw = #{raw}")
-    cid = `cat #{cid_filename}`
-    Rails.logger.warn("cid = #{cid}")
-    log = `docker logs #{cid}`
-    Rails.logger.warn("log = #{log}")
-    `docker rm #{cid}`
+    Rails.logger.warn("output = #{output}")
+    #cid = `cat #{cid_filename}`
+    #Rails.logger.warn("cid = #{cid}")
+    #log = `docker logs #{cid}`
+    #Rails.logger.warn("log = #{log}")
+    #`docker rm #{cid}`
 
     timed_out_message = "Terminated by the cyber-dojo server after #{max_seconds} seconds."
-    output = (exitstatus != killed_by_timeout) ? log : timed_out_message
-    Rails.logger.warn("output = #{output}")
-    output
+    output = timed_out_message if exit_status == killed_by_timeout
   end
 
   def with_stderr(cmd)
