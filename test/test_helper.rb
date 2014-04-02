92c0a4e36ad534cc6d0096b90b7e0113b1fc67d5
Jon Jagger <jon@jaggersoft.com>
2014-04-02 17:22:38 +0100

    worked around a SimpleCov glitch

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,11 +12,17 @@ SimpleCov.start do
   add_filter "/cyberdojo/config/"
 
   add_group 'app/controllers', 'app/controllers'
-  add_group 'app/models',      'app/models'
+  add_group 'app/models'      do |src_file|
+      # for some reason SimpleCov is seeing
+      # cyberdojo/app/models/Dojo.rb and
+      # cyberdojo/app/models/Ddjo.rb
+      # when the later file does not exist!
+      src_file.filename.include?('cyberdojo/app/models') &&
+      !src_file.filename.include?('dojo.rb')
+  end
   add_group 'app/helpers',     'app/helpers'
   add_group 'app/lib',         'app/lib'
-  add_group 'integration',     'integration' # hack
-
+  add_group 'integration',     'integration'
   add_group 'lib'             do |src_file|
     src_file.filename.include?('lib') &&
     !src_file.filename.include?('app/lib')
