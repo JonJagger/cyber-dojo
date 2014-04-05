c3be8eb25333a4267396f47c2865a4dcae1071d2
Jon Jagger <jon@jaggersoft.com>
2014-04-05 18:21:32 +0100

    more dockerfile work

--- languages/Ruby-TestUnit/build.sh
+++ languages/Ruby-TestUnit/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp Dockerfile-1.9.3 Dockerfile
+cp Dockerfile_ruby_1.9.3 Dockerfile
 docker build -t cyberdojo/ruby-1.9.3  .
 
-cp Dockerfile-1.9.3_test_unit Dockerfile
+cp Dockerfile_ruby_1.9.3_test_unit Dockerfile
 docker build -t cyberdojo/ruby-1.9.3_test_unit  .
 
 docker push cyberdojo/ruby-1.9.3
