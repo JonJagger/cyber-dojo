bfed3af1d7c81ae06dd7e3f8ca2bf4bec8d47dbb
Jon Jagger <jon@jaggersoft.com>
2014-04-06 20:03:20 +0100

    dockerfile perl prep

--- languages/Perl-TestSimple/build.sh
+++ languages/Perl-TestSimple/build.sh
@@ -2,8 +2,8 @@
 cp Dockerfile_perl Dockerfile
 docker build -t cyberdojo/perl .
 
-cp Dockerfile_perl_testsimple Dockerfile
-docker build -t cyberdojo/perl_testsimple .
+cp Dockerfile_perl_test_simple Dockerfile
+docker build -t cyberdojo/perl_test_simple .
 
 docker push cyberdojo/perl
-docker push cyberdojo/perl_testsimple
+docker push cyberdojo/perl_test_simple
