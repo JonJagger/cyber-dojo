2f9f34bc92c216fc3a2c89eeca209dd5f9be8e11
Jon Jagger <jon@jaggersoft.com>
2014-04-10 21:34:22 +0100

    DockerRunner - does it have to have timeout on the command passed to docker run... that would makes sense

--- app/lib/DockerRunner.rb
+++ app/lib/DockerRunner.rb
@@ -14,54 +14,16 @@ class DockerRunner
 
   def run(paas, sandbox, command, max_seconds)
     language = sandbox.avatar.kata.language
-    cmd = 'docker run -u root --rm' +
+    kill = 9
+    inner_cmd = "timeout --signal=#{kill} #{max_seconds}s #{with_stderr(command)}"
+
+    outer_cmd = 'docker run -u root --rm' +
           " -v #{paas.path(sandbox)}:/sandbox:rw" +
           " -v #{paas.path(language)}:#{paas.path(language)}:ro" +
           ' -w /sandbox' +
-          " #{language.image_name} /bin/bash -c \"#{with_stderr(command)}\""
+          " #{language.image_name} /bin/bash -c \"#{inner_cmd}\""
 
-    pipe = nil
-    begin
-      pipe = IO::popen(cmd, 'r')
-    rescue Exception => e
-      Rails.logger.warn "Execution of command #{cmd} unsuccessful"
-    end
-
-    output = ''
-    begin
-      status = Timeout::timeout(max_seconds) {
-        Process.waitpid2(pipe.pid)
-        output = pipe.gets(nil)
-      }
-    rescue Timeout::Error
-      Process.kill('-KILL', pipe.pid)
-      output = terminated(max_seconds)
-    end
-    pipe.close
-    output
-
-
-=begin
-    kill = 9
-    pid = Process.spawn(cmd)
-    begin
-      Timeout.timeout(max_seconds) do
-        Process.wait(pid)
-      end
-    rescue TimeoutError
-      Process.kill("-#{kill}", pid)
-    end
-
-
-    output = `timeout --signal=#{kill} --kill-after=1 #{max_seconds}s #{cmd}`
-    exit_status = $?.exitstatus
-    # kill process and all children
-    msg = `kill -TERM -#{$?.pid}`
-    fatal_error_signal = 128
-    killed_by_timeout = fatal_error_signal + kill
-
-    exit_status != killed_by_timeout ? output : terminated(max_seconds)
-=end
+    `outer_cmd`
   end
 
 end
