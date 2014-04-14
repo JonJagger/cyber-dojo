9e86abee58ba9ba5c55fa4dae0bc27349b49d320
Jon Jagger <jon@jaggersoft.com>
2014-04-14 18:21:13 +0100

    removed copyright notice from CppUTest .cpp file

--- languages/C++-CppUTest/AllTests.cpp
+++ languages/C++-CppUTest/AllTests.cpp
@@ -1,8 +1,3 @@
-//- Copyright (c) 2008-2013 James Grenning --- All rights reserved
-//- For exclusive use by participants in Renaissance Software Consulting training courses.
-//- Cannot be used by attendees to train others without written permission.
-//- www.renaissancesoftware.net james@renaissancesoftware.net
-
 #include "CppUTest/CommandLineTestRunner.h"
 
 int main(int ac, char** av)
