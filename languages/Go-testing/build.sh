d37c719192158235f93f8ca02fc0d26cb68ce5ea
Jon Jagger <jon@jaggersoft.com>
2014-04-18 08:36:23 +0100

    Docker Go work

---
+++ languages/Go-testing/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp Dockerfile_go Dockerfile
+docker build -t cyberdojo/go  .
+rm Dockerfile
+
+cp Dockerfile_go_testing Dockerfile
+docker build -t cyberdojo/go_testing .
+rm Dockerfile
+
+#docker push cyberdojo/go
+#docker push cyberdojo/go_testing
