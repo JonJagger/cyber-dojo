596405823a4ca95a1cc2670ce1c0655154e5e978
Jon Jagger <jon@jaggersoft.com>
2014-04-18 09:53:07 +0100

    Docker clojure work

--- languages/Clojure-.test/cyber-dojo.sh
+++ languages/Clojure-.test/cyber-dojo.sh
@@ -1,1 +1,3 @@
-java -cp ./clojure-1.4.0.jar:. -Dclojure.compile.path=. clojure.main -i *.clj
+CLJ_FILES=`ls *.clj | grep -v 'test' | tr '\n' ' '`
+TEST_CLJ_FILES=`ls *.clj | grep 'test' | tr '\n' ' '`
+java -cp ./clojure-1.4.0.jar:. -Dclojure.compile.path=. clojure.main -i $CLJ_FILES $TEST_CLJ_FILES
