fab2c13c60f719aab81c32d0e922543b8a073c5b
Jon Jagger <jon@jaggersoft.com>
2014-04-18 11:43:20 +0100

    Docker Groovy work

--- languages/Groovy-JUnit/build.sh
+++ languages/Groovy-JUnit/build.sh
@@ -7,5 +7,5 @@ cp Dockerfile_groovy-2.2.0_junit Dockerfile
 docker build -t cyberdojo/groovy-2.2.0_junit  .
 rm Dockerfile
 
-#docker push cyberdojo/groovy-2.2.0
-#docker push cyberdojo/groovy-2.2.0_junit
+docker push cyberdojo/groovy-2.2.0
+docker push cyberdojo/groovy-2.2.0_junit
