94bdd73545f02d2932121e27152363743d736f69
Jon Jagger <jon@jaggersoft.com>
2014-04-19 16:15:41 +0100

    language-manifest-checker now checks group/owner/permissions of files

--- test/installation/language_manifest_checker.rb
+++ test/installation/language_manifest_checker.rb
@@ -1,5 +1,6 @@
 CYBER_DOJO_ROOT = File.absolute_path(File.dirname(__FILE__) + '/../../')
 require 'json'
+require 'etc'
 require CYBER_DOJO_ROOT + "/app/lib/OutputParser"
 
 class LanguageManifestChecker
@@ -11,11 +12,11 @@ class LanguageManifestChecker
 
   def check?(language)
     @language = language
-    @language_dir = @root_path + '/languages/' + language + "/"
+    @language_dir = @root_path + '/languages/' + language
 
     print "  #{language} " + ('.' * (35-language.to_s.length))
 
-    @manifest_filename = @language_dir + 'manifest.json'
+    @manifest_filename = @language_dir + '/' + 'manifest.json'
     return false if !manifest_file_exists?
     raw = IO.read(@manifest_filename)
     @manifest = JSON.parse(raw)
@@ -30,9 +31,9 @@ class LanguageManifestChecker
     return false if !all_support_files_exist?
     return false if !highlight_filenames_are_subset_of_visible_filenames?
     return false if !parse_method_for_unit_test_framework_output_exists?
-    #return false if any_files_owner_is_root?
-    #return false if any_files_group_is_root?
-    #return false if any_file_is_unreadable?
+    return false if any_files_owner_is_root?
+    return false if any_files_group_is_root?
+    return false if any_file_is_unreadable?
     return true
   end
 
@@ -126,7 +127,7 @@ private
 
   def all_files_exist?(symbol)
     (@manifest[symbol] || [ ]).each do |filename|
-      if !File.exists?(@language_dir + filename)
+      if !File.exists?(@language_dir + '/' + filename)
         message =
           alert +
           "  #{@manifest_filename} contains a '#{symbol}' entry [#{filename}]\n" +
@@ -146,8 +147,8 @@ private
            !visible_filenames.include?(filename)
         message =
           alert +
-          "  #{@manifest_filename} contains a 'highlight_filenames' entry [\"#{filename}\"]\n" +
-          "  but visible_filenames does not include \"#{filename}\""
+          "  #{@manifest_filename} contains a 'highlight_filenames' entry ['#{filename}']\n" +
+          "  but visible_filenames does not include '#{filename}'"
         puts message
         return false
       end
@@ -171,7 +172,7 @@ private
   end
 
   def cyberdojo_sh_has_execute_permission?
-    if !File.stat(@language_dir + 'cyber-dojo.sh').executable?
+    if !File.stat(@language_dir + '/' + 'cyber-dojo.sh').executable?
       message =
         alert +
           " 'cyber-dojo.sh does not have execute permission"
@@ -201,6 +202,51 @@ private
     true
   end
 
+  def any_files_owner_is_root?
+    (visible_filenames + support_filenames).each do |filename|
+      uid = File.stat(@language_dir + '/' + filename).uid
+      owner = Etc.getpwuid(uid).name
+      if owner == 'root'
+        message =
+          alert +
+            "owner of #{@language_dir}/#{filename} is root"
+        puts message
+        return true
+      end
+    end
+    print "."
+    return false
+  end
+
+  def any_files_group_is_root?
+    (visible_filenames + support_filenames).each do |filename|
+      gid = File.stat(@language_dir + '/' + filename).gid
+      owner = Etc.getgrgid(gid).name
+      if owner == 'root'
+        message =
+          alert +
+            "owner of #{@language_dir}/#{filename} is root"
+        puts message
+        return true
+      end
+    end
+    print "."
+    return false
+  end
+
+  def any_file_is_unreadable?
+    (visible_filenames + support_filenames).each do |filename|
+      if !File.stat(@language_dir + '/' + filename).world_readable?
+        message =
+          alert +
+            "#{@language_dir}/#{filename} is not world-readable"
+        puts message
+        return true
+      end
+    end
+    print "."
+    return false
+  end
 
 private
 
