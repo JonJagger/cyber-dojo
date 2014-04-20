7118b03ae64fd67a3d0374e9a4b4e7c0d093b92f
Jon Jagger <jon@jaggersoft.com>
2014-04-20 12:28:17 +0100

    updated admin_scripts/docker_pull_all.rb with new docker container names

--- admin_scripts/docker_pull_all.rb
+++ admin_scripts/docker_pull_all.rb
@@ -1,5 +1,7 @@
 
 containers = %w(
+  clojure-1.4.0
+  clojure-1.4.0_test
   csharp-2.10.8.1
   csharp-2.10.8.1_nunit
   erlang-5.10.2
@@ -17,7 +19,10 @@ containers = %w(
   haskell-7.6.3
   haskell-7.6.3_hunit
   java-1.8
+  java-1.8_approval
+  java-1.8_cucumber
   java-1.8_junit
+  java-1.8_mockito
   perl-5.14.2
   perl-5.14.2_test_simple
   python-3.3.5
