cc213256811ab7749efcd71678a0baee8a602e8c
Jon Jagger <jon@jaggersoft.com>
2014-04-21 17:06:35 +0100

    added comment on untarring with --owner and --group options

--- admin_scripts/untar.rb
+++ admin_scripts/untar.rb
@@ -1,5 +1,6 @@
 
 # run from /var/www/cyberdojo/tmp_scp_all
+
 n = 0
 `ls`.split.each do |id_tgz|
   p "#{n} #{id_tgz}"
@@ -7,3 +8,11 @@ n = 0
   n += 1
   # break if n == 1
 end
+
+# to untar and force the files to belong to a specific user
+#
+# uid = `id -u www-data`.rstrip
+# gid = `id -g www-data`.rstrip
+#
+# `tar ....... --owner=#{uid}  --group=#{gid}`
+#
