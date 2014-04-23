c6f9d63b2b83d56d9313fc9359f612e4f4e5c166
Jon Jagger <jon@jaggersoft.com>
2014-04-23 16:05:04 +0100

    removed animal name from dashboard page traffic lights

--- app/helpers/traffic_light_helper.rb
+++ app/helpers/traffic_light_helper.rb
@@ -2,7 +2,7 @@
 module TrafficLightHelper
 
   def diff_traffic_light(kata, avatar_name, light, max_lights)
-    # used from test page and from dashboard page    
+    # used from test page and from dashboard page
     number = light['number'].to_i
     ("<div class='tipped diff-traffic-light'" +
          " data-id='#{kata.id}'" +
@@ -15,14 +15,14 @@ module TrafficLightHelper
         "</div>" +
         traffic_light_image(colour(light), 20, 62) +
      "</div>"
-    ).html_safe    
+    ).html_safe
   end
 
   def colour(light)
      # old dojos used 'outcome'
      (light['colour'] || light['outcome']).to_s
   end
-  
+
   def no_diff_avatar_image(kata, avatar_name, light, max_lights)
     ("<div class='tipped diff-traffic-light'" +
          " data-id='#{kata.id}'" +
@@ -36,17 +36,18 @@ module TrafficLightHelper
         "</div>" +
         "<img src='/images/avatars/#{avatar_name}.jpg'" +
             " alt='#{avatar_name}'" +
-            " width='45'" +
-            " height='45'/>" +           
+            " title='#{avatar_name}'" +
+            " width='52'" +
+            " height='52'/>" +
      "</div>"
-    ).html_safe    
+    ).html_safe
   end
- 
+
   def traffic_light_image(colour, width, height)
     ("<img src='/images/traffic_light_#{colour}.png'" +
       " alt='#{colour} traffic-light'" +
       " width='#{width}'" +
-      " height='#{height}'/>").html_safe    
+      " height='#{height}'/>").html_safe
   end
 
   def tool_tip(avatar_name, light)
@@ -54,5 +55,5 @@ module TrafficLightHelper
     "review #{avatar_name}'s<br>" +
     "#{n-1} &harr; #{n} diff"
   end
-  
+
 end
