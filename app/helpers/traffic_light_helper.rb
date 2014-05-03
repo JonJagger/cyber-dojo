67ab905f98835ccadf81e7d6b78e7a8f8e7d009b
Jon Jagger <jon@jaggersoft.com>
2014-05-03 07:24:25 +0100

    added pie-chart for dashboard team-bulb-counts

--- app/helpers/traffic_light_helper.rb
+++ app/helpers/traffic_light_helper.rb
@@ -1,12 +1,15 @@
 
 module TrafficLightHelper
 
-  def pie_chart(traffic_lights, size=35)
+  def pie_chart(traffic_lights, size, counts={})
+     counts['red'] = tally(counts,traffic_lights,'red')
+     counts['amber'] = tally(counts,traffic_lights,'amber')
+     counts['green'] = tally(counts,traffic_lights,'green')
      ("<canvas" +
         " class='pie'" +
-        " data-red-count='#{count(traffic_lights,'red')}'" +
-        " data-amber-count='#{count(traffic_lights,'amber')}'" +
-        " data-green-count='#{count(traffic_lights,'green')}'" +
+        " data-red-count='#{counts['red']}'" +
+        " data-amber-count='#{counts['amber']}'" +
+        " data-green-count='#{counts['green']}'" +
         " width='#{size}'" +
         " height='#{size}'>" +
       "</canvas>").html_safe
@@ -68,6 +71,10 @@ module TrafficLightHelper
   end
 
 private
+
+  def tally(counts, traffic_lights, colour)
+     counts.include?(colour) ? counts[colour] : count(traffic_lights,colour)
+  end
 
   def count(traffic_lights, colour)
      traffic_lights.count{|light| light['colour'] === colour}
