03f8cf5a739d5228d9d514fff280b9070f6cf5ec
Jon Jagger <jon@jaggersoft.com>
2014-05-05 06:56:00 +0100

    added comment on using rsync

--- admin_scripts/xfer.rb
+++ admin_scripts/xfer.rb
@@ -1,3 +1,15 @@
+
+# Update - it is much simpler and faster to do this using rsync
+# eg
+# $ rsync --progress -prltgo local-folder/ root@IP-ADDRESS:/destination-folder
+#
+# -r = recursive
+# -l = links
+# -t = timestamp
+# -g = groups
+# -o = owner
+
+
 # A ruby script to download zips of dojos identified by
 # the output of the prunt_large.rb script
 #
