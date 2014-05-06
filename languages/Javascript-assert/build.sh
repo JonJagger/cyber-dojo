03cd6440e4367ec95835600dfa1bdb7c3668d745
Jon Jagger <jon@jaggersoft.com>
2014-05-06 09:35:29 +0100

    Dockerfile Javascript prep

--- languages/Javascript-assert/build.sh
+++ languages/Javascript-assert/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cp Dockerfile_python-3.3.5 Dockerfile
-docker build -t cyberdojo/python-3.3.5  .
-rm Dockerfile
-
-cp Dockerfile_python-3.3.5_unittest Dockerfile
-docker build -t cyberdojo/python-3.3.5_unittest .
+cp Dockerfile_javascript_node-0.10.15 Dockerfile
+docker build -t cyberdojo/javascript_node-0.10.15  .
 rm Dockerfile
