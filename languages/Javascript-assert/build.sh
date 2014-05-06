473353c6a7f4b673e60d785e3e6481456cc73a0d
Jon Jagger <jon@jaggersoft.com>
2014-05-06 11:00:22 +0100

    refactoring

--- languages/Javascript-assert/build.sh
+++ languages/Javascript-assert/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cp Dockerfile_javascript_node-0.10.15 Dockerfile
-docker build -t cyberdojo/javascript_node-0.10.15  .
+cp Dockerfile_javascript-0.10.15 Dockerfile
+docker build -t cyberdojo/javascript-0.10.15  .
+rm Dockerfile
+
+cp Dockerfile_javascript-0.10.15_assert Dockerfile
+docker build -t cyberdojo/javascript-0.10.15_assert  .
 rm Dockerfile
