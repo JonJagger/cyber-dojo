466aaa67684ad5cb5383c5ea6ae5088eb162345e
Jon Jagger <jon@jaggersoft.com>
2014-05-06 12:06:50 +0100

    Dockerfile Scala prep

--- languages/Java-JUnit/build.sh
+++ languages/Java-JUnit/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cp Dockerfile_java_1.7 Dockerfile
+docker build -t cyberdojo/java-1.7 .
+rm Dockerfile
+
 cp Dockerfile_java_1.8 Dockerfile
 docker build -t cyberdojo/java-1.8 .
 rm Dockerfile
