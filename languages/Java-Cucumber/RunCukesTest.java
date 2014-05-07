7d727fdf095111c84c3b7e9bdec94f4517c65770
Seb Rose <seb@claysnow.co.uk>
2014-05-07 08:13:48 +0100

    Updated Cucumber-JVM to 1.1.6

--- languages/Java-Cucumber/RunCukesTest.java
+++ languages/Java-Cucumber/RunCukesTest.java
@@ -1,8 +1,9 @@
-import cucumber.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.SnippetType;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(strict=true)
+@Cucumber.Options(strict=true, format="junit:output", snippets=SnippetType.CAMELCASE)
 public class RunCukesTest {
     // this is the adapter/bridge code
     // between cucumber jvm and junit
