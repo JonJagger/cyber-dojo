09d4117c55263165e38f436e5d8c379bb0569ff9
Jon Jagger <jon@jaggersoft.com>
2014-05-08 09:31:53 +0100

    refactoring languages/

--- languages/PHP-PHPUnit/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_php_5.5.3 Dockerfile
-docker build -t cyberdojo/php-5.5.3 .
-rm Dockerfile
-
-cp Dockerfile_php_5.5.3_phpunit Dockerfile
-docker build -t cyberdojo/php-5.5.3_phpunit .
-rm Dockerfile
---
+++ languages/PHP/build-docker-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t cyberdojo/php-5.5.3 .
