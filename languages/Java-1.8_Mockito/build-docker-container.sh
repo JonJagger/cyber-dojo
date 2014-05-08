00d178d1fa792b7a0d987d279a1a726794e8a4a9
Jon Jagger <jon@jaggersoft.com>
2014-05-08 08:45:55 +0100

    refactoring languages/

--- languages/Java-1.8_Mockito/build-docker-container.sh
+++ languages/Java-1.8_Mockito/build-docker-container.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cp Dockerfile_java_1.8_mockito Dockerfile
 docker build -t cyberdojo/java-1.8_mockito .
-rm Dockerfile
--- languages/Java-1.8_Powermockito/build-docker-container.sh
+++ languages/Java-1.8_Powermockito/build-docker-container.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cp Dockerfile_java_1.8_powermockito Dockerfile
 docker build -t cyberdojo/java-1.8_powermockito .
-rm Dockerfile
