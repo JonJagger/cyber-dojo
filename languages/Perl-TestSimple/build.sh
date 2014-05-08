1e0dfcd2a1e84eff7d081deb0faba53227244ff4
Jon Jagger <jon@jaggersoft.com>
2014-05-08 08:53:05 +0100

    refactoring languages/

--- languages/Perl-TestSimple/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_perl_5.14.2 Dockerfile
-docker build -t cyberdojo/perl-5.14.2 .
-rm Dockerfile
-
-cp Dockerfile_perl_5.14.2_test_simple Dockerfile
-docker build -t cyberdojo/perl-5.14.2_test_simple .
-rm Dockerfile
---
+++ languages/Perl/build-docker-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t cyberdojo/perl-5.14.2 .
