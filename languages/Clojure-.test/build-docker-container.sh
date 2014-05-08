9057b6025620b6d6a8583ea3461337650cbff041
Jon Jagger <jon@jaggersoft.com>
2014-05-08 09:21:26 +0100

    refactoring languages/

---
+++ languages/Clojure-.test/build-docker-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t cyberdojo/clojure-1.4.0_test .
--- languages/Clojure-.test/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_clojure_1.4.0 Dockerfile
-docker build -t cyberdojo/clojure-1.4.0 .
-rm Dockerfile
-
-cp Dockerfile_clojure_1.4.0_test Dockerfile
-docker build -t cyberdojo/clojure-1.4.0_test .
-rm Dockerfile
