b1487c4c04aad3c64905fa5fe2feef7217ecea27
Jon Jagger <jon@jaggersoft.com>
2014-05-08 09:20:03 +0100

    refactoring languages/

--- languages/CoffeeScript-jasmine/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_coffeescript-1.14.3 Dockerfile
-docker build -t cyberdojo/coffeescript-1.14.3  .
-rm Dockerfile
-
-cp Dockerfile_coffeescript-1.14.3_jasmine Dockerfile
-docker build -t cyberdojo/coffeescript-1.14.3_jasmine .
-rm Dockerfile
