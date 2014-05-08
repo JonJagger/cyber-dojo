268b3b2ca10d5a6720548e24c899127516b69f92
Jon Jagger <jon@jaggersoft.com>
2014-05-08 07:37:36 +0100

    refactoring languages/

--- languages/Scala-scalatest/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_scala-2.9.2 Dockerfile
-docker build -t cyberdojo/scala-2.9.2  .
-rm Dockerfile
-
-cp Dockerfile_scala-2.9.2_scalatest Dockerfile
-docker build -t cyberdojo/scala-2.9.2_scalatest  .
-rm Dockerfile
