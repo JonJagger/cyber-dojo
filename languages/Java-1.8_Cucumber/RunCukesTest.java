55f1ba0f4083b272d4985f23857ba0f41a2595d1
Jon Jagger <jon@jaggersoft.com>
2014-05-08 18:40:04 +0100

    dont use colour output codes in Java-Cucumber

--- languages/Java-1.8_Cucumber/RunCukesTest.java
+++ languages/Java-1.8_Cucumber/RunCukesTest.java
@@ -4,7 +4,7 @@ import cucumber.api.SnippetType;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict=true, format="junit:output", snippets=SnippetType.CAMELCASE)
+@CucumberOptions(strict=true, monochrome=true, format="junit:output", snippets=SnippetType.CAMELCASE)
 public class RunCukesTest {
     // this is the adapter/bridge code
     // between cucumber jvm and junit
