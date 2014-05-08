420b72c01fd164228c614c0bac40d9859856b8ff
Jon Jagger <jon@jaggersoft.com>
2014-05-08 09:28:00 +0100

    refactoring languages/

--- languages/C++-CppUTest/build-docker-container.sh
+++ languages/C++-CppUTest/build-docker-container.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cp Dockerfile_gpp_4.8.1_cpputest Dockerfile
 docker build -t cyberdojo/gpp-4.8.1_cpputest  .
-rm Dockerfile
 
--- languages/C++-GoogleTest/build-docker-container.sh
+++ languages/C++-GoogleTest/build-docker-container.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cp Dockerfile_gpp_4.8.1_googletest Dockerfile
 docker build -t cyberdojo/gpp-4.8.1_googletest  .
-rm Dockerfile
