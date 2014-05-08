76b5118cdd0df3da3fba20f916bc54746ab8a78b
Jon Jagger <jon@jaggersoft.com>
2014-05-08 09:34:23 +0100

    refactoring languages/

--- languages/Ruby-TestUnit/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_ruby_1.9.3 Dockerfile
-docker build -t cyberdojo/ruby-1.9.3  .
-rm Dockerfile
-
-cp Dockerfile_ruby_1.9.3_test_unit Dockerfile
-docker build -t cyberdojo/ruby-1.9.3_test_unit  .
-rm Dockerfile
