969bc5be1e5413625e460f726acaaea9d9806bb6
Seb Rose <seb@claysnow.co.uk>
2014-05-08 18:08:44 +0100

    Update deprecated API

--- languages/Java-Cucumber/RunCukesTest.java
+++ languages/Java-Cucumber/RunCukesTest.java
@@ -1,9 +1,10 @@
 import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(strict=true, format="junit:output", snippets=SnippetType.CAMELCASE)
+@CucumberOptions(strict=true, format="junit:output", snippets=SnippetType.CAMELCASE)
 public class RunCukesTest {
     // this is the adapter/bridge code
     // between cucumber jvm and junit
