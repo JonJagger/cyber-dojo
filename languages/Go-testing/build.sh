ecf7486655adb2eaa04814bdeb3124e69be73d97
Jon Jagger <jon@jaggersoft.com>
2014-05-08 08:59:01 +0100

    refactoring languages/

--- languages/Go-testing/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_go_1.1.2 Dockerfile
-docker build -t cyberdojo/go-1.1.2  .
-rm Dockerfile
-
-cp Dockerfile_go_1.1.2_testing Dockerfile
-docker build -t cyberdojo/go-1.1.2_testing .
-rm Dockerfile
