96c7169ef39d3f8320fb25a5412a695fed036a25
Jon Jagger <jon@jaggersoft.com>
2014-05-08 09:17:53 +0100

    refactoring languages/

--- languages/Erlang-eunit/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp Dockerfile_erlang_5.10.2 Dockerfile
-docker build -t cyberdojo/erlang-5.10.2  .
-rm Dockerfile
-
-cp Dockerfile_erlang_5.10.2_eunit Dockerfile
-docker build -t cyberdojo/erlang-5.10.2_eunit .
-rm Dockerfile
