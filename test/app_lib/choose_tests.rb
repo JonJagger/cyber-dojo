2f2a993d5cac55dc71ff7be20f5640d35cbcfeb0
Jon Jagger <jon@jaggersoft.com>
2014-05-11 17:18:31 +0100

    refactoring

--- test/app_lib/choose_tests.rb
+++ test/app_lib/choose_tests.rb
@@ -24,45 +24,53 @@ class ChooseTests < ActionController::TestCase
     assert_equal languages.length, counts.length
   end
 
-  test "when no params[:id] then choose random entry from languages" do
-    languages = ['C', 'Ruby', 'Python', 'C++']
+  def test_languages_names
+    [ 'C#-NUnit',
+      'C++-GoogleTest',
+      'Ruby-installed-and-working',
+      'test-Java-JUnit'
+    ].sort
+  end
+
+  test "when no params[:id] " +
+       "then choose random known language" do
     kata = @dojo.katas['01234556789']
     assert !kata.exists?
-    assert_is_randomly_chosen_language(languages, params_id=nil, kata)
+    assert_is_randomly_chosen_language(test_languages_names, params_id=nil, kata)
   end
 
-  test "when params[:id] but kata(id) does not exist then choose random entry from languages" do
-    languages = ['C', 'Ruby', 'Python', 'C++']
+  test "when params[:id] " +
+       "and !katas[id].exists " +
+       "then choose random known language" do
     kata = @dojo.katas['0123456789']
     assert !kata.exists?
-    assert_is_randomly_chosen_language(languages, params_id='012345', kata)
+    assert_is_randomly_chosen_language(test_languages_names, params_id='012345', kata)
   end
 
-  test "when params[:id] and kata(id) exists but its language is not one of languages then choose random entry from languages" do
-    languages = [ 'C', 'Python', 'Ruby', 'Clojure', 'Java' ]
-    language = 'Ruby-installed-and-working'
-    assert !languages.include?(language)
-    kata = make_kata(@dojo, language, 'Yahtzee')
-    assert kata.exists?
-    assert_is_randomly_chosen_language(languages, params_id=kata.id.to_s, kata)
-  end
-
-  def assert_chosen_language(languages, id, kata, choice)
-    (1..100).each do
-      n = Choose::language(languages, id, kata)
-      assert_equal choice, n, "#{languages[choice]} #{kata.language.name}"
+  test "when params[:id] " +
+       "and katas[id].exists? " +
+       "but language is unknown " +
+       "then choose random known language" do
+    test_languages_names.each do |language|
+      languages = test_languages_names - [language]
+      assert !languages.include?(language)
+      kata = make_kata(@dojo, language, 'Yahtzee')
+      assert kata.exists?
+      assert_is_randomly_chosen_language(languages, params_id=kata.id.to_s, kata)
     end
   end
 
-  test "when params[:id] and kata(id) exists and its language is one of languages choose it" do
-    languages = [ 'C#-NUnit',
-                  'C++-GoogleTest',
-                  'Ruby-installed-and-working',
-                  'test-Java-JUnit' ]
-    languages.each_with_index do |language,n|
+  test "when params[:id] " +
+       "and katas[id].exists? " +
+       "and language is known " +
+       "then choose that language" do
+    test_languages_names.each_with_index do |language,n|
       kata = make_kata(@dojo, language, 'Yahtzee')
+      id = kata.id.to_s
       assert kata.exists?
-      assert_chosen_language(languages, kata.id.to_s, kata, n)
+      (1..100).each do
+        assert_equal n, Choose::language(test_languages_names, id, kata)
+      end
     end
   end
 
@@ -89,28 +97,38 @@ class ChooseTests < ActionController::TestCase
     ].sort
   end
 
-  test "when no params[:id] then choose random entry from exercises" do
+  test "when no params[:id] " +
+       "then choose random known exercise" do
     kata = @dojo.katas['01234556789']
     assert !kata.exists?
     assert_is_randomly_chosen_exercise(test_exercises_names, params_id=nil, kata)
   end
 
-  test "when params[:id] but kata(id) does not exist then choose random entry from exercises" do
+  test "when params[:id] " +
+       "but !katas[id].exist? " +
+       "then choose random known exercise" do
     kata = @dojo.katas['0123456789']
     assert !kata.exists?
     assert_is_randomly_chosen_exercise(test_exercises_names, params_id='012345', kata)
   end
 
-  test "when params[:id] and kata(id) exists but its exercise is not one of exercises then choose random entry from exercises" do
-    exercise = 'test_Yahtzee'
-    exercises = test_exercises_names - [exercise]
-    assert !exercises.include?(exercise)
-    kata = make_kata(@dojo, 'Ruby-installed-and-working', exercise)
-    assert kata.exists?
-    assert_is_randomly_chosen_exercise(exercises, params_id=kata.id.to_s, kata)
+  test "when params[:id] " +
+       "and katas[id].exists? " +
+       "but exercise is unknown " +
+       "then choose random known exercise" do
+    test_exercises_names.each do |exercise|
+      exercises = test_exercises_names - [exercise]
+      assert !exercises.include?(exercise)
+      kata = make_kata(@dojo, 'Ruby-installed-and-working', exercise)
+      assert kata.exists?
+      assert_is_randomly_chosen_exercise(exercises, params_id=kata.id.to_s, kata)
+    end
   end
 
-  test "when params[:id] and kata(id) exists and its exercise is one of exercises choose it" do
+  test "when params[:id] " +
+       "and katas[id].exists? " +
+       "and exercise is known " +
+       "then choose that exercise" do
     test_exercises_names.each_with_index do |exercise,n|
       kata = make_kata(@dojo, 'Ruby-installed-and-working', exercise)
       id = kata.id.to_s
