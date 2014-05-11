de9be4a964839d54ee8bea4d2c705c66729d7bdf
Jon Jagger <jon@jaggersoft.com>
2014-05-11 17:26:59 +0100

    refactoring

--- app/lib/Choose.rb
+++ app/lib/Choose.rb
@@ -7,22 +7,19 @@ module Choose
   # kata with that id - if they still exist.
 
   def self.language(languages, params_id, kata)
-    choice = [*0..languages.length-1].shuffle[0]
-    if params_id && kata.exists?
-      language_index = languages.index(kata.language.name)
-      if language_index != nil
-        choice = language_index;
-      end
-    end
-    choice
+    self.chooser(languages, params_id, kata) {|kata| kata.language.name}
   end
 
   def self.exercise(exercises, params_id, kata)
-    choice = [*0..exercises.length-1].shuffle[0]
+    self.chooser(exercises, params_id, kata) {|kata| kata.exercise.name}
+  end
+
+  def self.chooser(choices, params_id, kata)
+    choice = [*0..choices.length-1].shuffle[0]
     if params_id && kata.exists?
-      exercise_index = exercises.index(kata.exercise.name)
-      if exercise_index != nil
-        choice = exercise_index
+      index = choices.index(yield(kata))
+      if index != nil
+        choice = index
       end
     end
     choice
