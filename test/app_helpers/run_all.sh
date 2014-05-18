0bb6a8d6123836d14f62716bed2d8b92316c2221
Jon Jagger <jon@jaggersoft.com>
2014-05-18 17:19:46 +0100

    test refactoring

--- test/app_helpers/run_all.sh
+++ test/app_helpers/run_all.sh
@@ -1,14 +1,5 @@
+#!/bin/bash
 
-# after running this check the coverage in index.html
-
-echo '' > log.tmp
-echo '' > run_all.tmp
-
-for TEST in *.rb ; do
-    cat $TEST >> run_all.tmp
-done
-ruby run_all.tmp 2>&1 | tee -a log.tmp
+testrb . 2>&1 | tee log.tmp
 cp -R ../../coverage/* .
-ruby ../perc.rb index.html app/helpers| tee -a log.tmp
-
-rm run_all.tmp
+ruby ../perc.rb index.html app/helpers | tee -a log.tmp
