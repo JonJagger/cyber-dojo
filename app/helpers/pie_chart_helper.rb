a53d1cb49423b3743e18118466816969a47c7caf
Jon Jagger <jon@jaggersoft.com>
2014-06-01 08:44:22 +0100

    refactoring

--- app/helpers/pie_chart_helper.rb
+++ app/helpers/pie_chart_helper.rb
@@ -3,18 +3,18 @@ module PieChartHelper
 
   def pie_chart(traffic_lights, size, key)
      pie_chart_from_counts({
-        'red'   => count(traffic_lights,'red'),
-        'amber' => count(traffic_lights,'amber'),
-        'green' => count(traffic_lights,'green')
+        :red   => count(traffic_lights,'red'),
+        :amber => count(traffic_lights,'amber'),
+        :green => count(traffic_lights,'green')
      }, size, key)
   end
 
   def pie_chart_from_counts(counts, size, key)
      "<canvas" +
         " class='pie'" +
-        " data-red-count='#{counts['red']}'" +
-        " data-amber-count='#{counts['amber']}'" +
-        " data-green-count='#{counts['green']}'" +
+        " data-red-count='#{counts[:red]}'" +
+        " data-amber-count='#{counts[:amber]}'" +
+        " data-green-count='#{counts[:green]}'" +
         " data-key='#{key}'" +
         " width='#{size}'" +
         " height='#{size}'>" +
