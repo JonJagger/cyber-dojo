8ec54e43125b45ccd6b0bfcd6d3ce19614be7671
Jon Jagger <jon@jaggersoft.com>
2014-06-05 22:21:58 +0100

    test refactoring

--- test/app_models/test_exercise.rb
+++ test/app_models/test_exercise.rb
@@ -4,33 +4,33 @@ require File.dirname(__FILE__) + '/model_test_case'
 
 class ExerciseTests < ModelTestCase
 
-  test "exists? is true only if dir and instructions exist" do
+  test 'exists? is true only if dir and instructions exist' do
     json_and_rb do
-      exercise = @dojo.exercises['Salmo']
+      exercise = @dojo.exercises['test_Yahtzee']
       assert !exercise.exists?
       @paas.dir(exercise).make
       assert !exercise.exists?
-      filename = 'instructions'
-      content = 'fishing for Salmon on the Verdal'
-      @paas.dir(exercise).spy_read(filename,content)
+      @paas.dir(exercise).spy_exists?('instructions')
       assert exercise.exists?
-      # force spy to read instructions
-      exercise.instructions
     end
   end
 
-  test "name is as set in ctor" do
+  #- - - - - - - - - - - - - - - - - - - - - -
+
+  test 'name is as set in ctor' do
     json_and_rb do
       exercise = @dojo.exercises['test_Yahtzee']
       assert_equal 'test_Yahtzee', exercise.name
     end
   end
 
-  test "instructions are loaded" do
+  #- - - - - - - - - - - - - - - - - - - - - -
+
+  test 'instructions are loaded from file of same name' do
     json_and_rb do
       exercise = @dojo.exercises['test_Yahtzee']
       filename = 'instructions'
-      content = 'fishing for Salmon on the Verdal'
+      content = 'The game of Yahtzee...'
       @paas.dir(exercise).spy_read(filename, content)
       assert_equal content, exercise.instructions
       assert @paas.dir(exercise).log.include?(['read',filename,content])
