f9bfa05dcfcd750ed02e56e4b5690c01dae63fee
Jon Jagger <jon@jaggersoft.com>
2014-06-10 18:36:21 +0100

    s/unntitled/hiker

--- languages/C++-Catch/untitled.tests.cpp
+++
@@ -1,8 +0,0 @@
-#include "untitled.hpp"
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"  // https://github.com/philsquared/Catch
-
-TEST_CASE( "The answer to life the universe and everything", "[hhg]" )
-{
-    REQUIRE( hhg() == 6*9 );
-}
