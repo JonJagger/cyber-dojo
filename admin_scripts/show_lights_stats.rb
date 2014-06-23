6202a631e137d2f781842d35a240686ac8a75727
Jon Jagger <jon@jaggersoft.com>
2014-06-23 13:45:48 +0100

    script tweak

--- admin_scripts/show_lights_stats.rb
+++ admin_scripts/show_lights_stats.rb
@@ -23,6 +23,7 @@ dojo = create_dojo
 print "\n"
 stats = { }
 dot_count = 0
+exceptions = [ ]
 dojo.katas.each do |kata|
   begin
     count = kata.avatars.inject(0){|sum,avatar| sum + avatar.lights.length }
@@ -31,14 +32,30 @@ dojo.katas.each do |kata|
     dot_count += 1
     print "\r " + dots(dot_count)
   rescue Exception => e
-    puts e.message
+    exceptions << e.message
   end
 end
-print "\n"
-print "\n"
+
+puts
+puts
+puts "    34 2 [5,6]"
+puts
+puts "means there were 2 katas with 34 traffic lights"
+puts "and those 2 katas had 5 and 6 avatars respectively."
+puts
+puts
 
 stats.sort.each do |count,tallies|
   printf("%3d %3d ",count, tallies.length)
   print tallies.sort.inspect if tallies.length <= 20
   printf("\n")
+end
+
+if exceptions != [ ]
+  puts
+  puts
+  print "Exceptions"
+  exceptions.each do |message|
+    puts message
+  end
 end
