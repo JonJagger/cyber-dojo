36a7f8071f8d1d2f9c33d8eefa6b2e74116a9adf
Jon Jagger <jon@jaggersoft.com>
2014-06-24 13:57:44 +0100

    test refactoring

--- test/app_helpers/run_all.sh
+++ test/app_helpers/run_all.sh
@@ -2,6 +2,7 @@
 
 # after running this check the coverage in index.html
 
+rm ../../coverage/.resultset.json
 testrb . 2>&1 | tee log.tmp
 cp -R ../../coverage/* .
 ruby ../perc.rb index.html app/helpers | tee -a log.tmp
