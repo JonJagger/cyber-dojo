0ec4ae2c1760c3dd7ae02a3756c9af748ab505d2
Jon Jagger <jon@jaggersoft.com>
2014-06-24 09:30:42 +0100

    test refactoring

--- test/app_lib/run_all.sh
+++ test/app_lib/run_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# after running this check the coverage in index.html
+
 testrb . 2>&1 | tee log.tmp
 cp -R ../../coverage/* .
-ruby ../perc.rb index.html app/helpers | tee -a log.tmp
+ruby ../perc.rb index.html app/lib | tee -a log.tmp
