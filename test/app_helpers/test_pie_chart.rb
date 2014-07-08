64916f2d259f3de927b58828a78b9b7d18a67d20
Jon Jagger <jon@jaggersoft.com>
2014-07-08 08:08:15 +0100

    added failing test for pie-chart bug

--- test/app_helpers/test_pie_chart.rb
+++ test/app_helpers/test_pie_chart.rb
@@ -6,11 +6,48 @@ class PieChartTests < CyberDojoTestBase
 
   include PieChartHelper
 
-  def   red_light; Light.new(nil,{'colour'=> :red  }); end
-  def amber_light; Light.new(nil,{'colour'=> :amber}); end
-  def green_light; Light.new(nil,{'colour'=> :green}); end
+  test 'pie-chart from avatar.lights' do
 
-  test "pie-chart from traffic-lights" do
+    externals = {
+      :disk => @disk = SpyDisk.new,
+      :git => @git = SpyGit.new,
+      :runner => StubTestRunner.new
+    }
+    @dojo = Dojo.new(root_path,externals)
+    kata = @dojo.katas['123456789A']
+    lion = kata.avatars['lion']
+    lion.dir.spy_read('increments.json', JSON.unparse([
+      {
+        'colour' => 'red',
+        'time' => [2014, 2, 15, 8, 54, 6],
+        'number' => 1
+      },
+      {
+        'colour' => 'green',
+        'time' => [2014, 2, 15, 8, 54, 34],
+        'number' => 2
+      }
+      ]))
+
+    expected = "" +
+      "<canvas" +
+      " class='pie'" +
+      " data-red-count='1'" +
+      " data-amber-count='0'" +
+      " data-green-count='1'" +
+      " data-key='lion'" +
+      " width='40'" +
+      " height='40'>" +
+      "</canvas>"
+
+    actual = pie_chart(lion.lights, 40, 'lion')
+
+    assert_equal expected, actual
+  end
+
+  #- - - - - - - - - - - - - - - - - - - -
+
+  test 'pie-chart from traffic-lights' do
     lights = [
       red_light,
       red_light,
@@ -32,7 +69,9 @@ class PieChartTests < CyberDojoTestBase
     assert_equal expected, actual
   end
 
-  test "pie-chart from counts" do
+  #- - - - - - - - - - - - - - - - - - - -
+
+  test 'pie-chart from counts' do
     counts = {
       :red => 5,
       :amber => 0,
@@ -51,5 +90,11 @@ class PieChartTests < CyberDojoTestBase
     actual = pie_chart_from_counts(counts, 42, 'lion')
     assert_equal expected, actual
   end
+
+  #- - - - - - - - - - - - - - - - - - - -
+
+  def   red_light; Light.new(nil,{'colour'=> :red  }); end
+  def amber_light; Light.new(nil,{'colour'=> :amber}); end
+  def green_light; Light.new(nil,{'colour'=> :green}); end
 
 end
