4ba7fd325c13a17dad01dbe41c1a3e58d18dd538
Jon Jagger <jon@jaggersoft.com>
2014-07-15 17:56:21 +0100

    F# docker container work

--- languages/F#-FsUnit/cyber-dojo.sh
+++ languages/F#-FsUnit/cyber-dojo.sh
@@ -1,7 +1,4 @@
-fsharpc  -r:/usr/lib/cli/FSharp.Core-4.3/FSharp.Core.dll \
-         -r:/usr/lib/mono/4.0/mscorlib.dll \
-         --noframework \
-         -o run-tests.exe *.fs
+fsharpc -o run-tests.exe *.fs
 if [ $? -eq 0 ]; then
   run-tests.exe
 fi
