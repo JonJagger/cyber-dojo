03b24da744eb1b0aa778eac3c57ddb04bb5be586
Jon Jagger <jon@jaggersoft.com>
2014-08-02 11:29:51 +0100

    added comments explaining difference between Tag and Light

--- app/models/Light.rb
+++ app/models/Light.rb
@@ -1,4 +1,17 @@
 
+# What is the difference between a Tag and a Light?
+# A Tag is an entry in increments.json which indicates
+# a single git commit+tag.
+# A Light corresponds to a [test] event which moves
+# from one tag to another tag, creating a red/amber/green
+# traffic-light as it does so.
+# So tag0 does not have a traffic-light (it is white)
+# and (tagN -> tagN+1) has a red/amber/green traffic-light.
+# I plan to create finer grained tags than just [test] events
+# for example, renaming a file. If this happens the
+# difference between a Tag and a Light will be much more
+# pronounced.
+
 class Light
 
   def initialize(avatar,hash)
