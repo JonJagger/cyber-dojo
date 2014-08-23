3c7e4d5d38acf0317a11e1db381e11c2d905a984
Jon Jagger <jon@jaggersoft.com>
2014-08-23 23:35:04 +0100

    refactoring

--- test/languages/check_one_language.rb
+++ test/languages/check_one_language.rb
@@ -4,8 +4,8 @@ require File.dirname(__FILE__) + '/one_language_checker'
 
 def show_use(message = "")
   puts
-  puts "USE: ruby check_one_language.rb [language]"
-  puts "   ERROR: #{message}" if message != ""
+  puts 'USE: ruby check_one_language.rb [language]'
+  puts "   ERROR: #{message}" if message != ''
   puts
 end
 
@@ -13,7 +13,7 @@ root_path = File.dirname(__FILE__) + '/../../'
 language = ARGV[0]
 
 if language == nil
-  show_use("no [language] specified")
+  show_use('no [language] specified')
   exit
 end
 
@@ -22,6 +22,6 @@ if !File.directory?(root_path + '/languages/' + language)
   exit
 end
 
-rag = OneLanguageChecker.new(root_path,"quiet").check(language)
-state = rag == ['red','amber','green'] ? "PASS" : "FAIL"
+rag = OneLanguageChecker.new(root_path,'quiet').check(language)
+state = rag == ['red','amber','green'] ? 'PASS' : 'FAIL'
 puts "#{language} --> #{rag} ==> #{state}"
