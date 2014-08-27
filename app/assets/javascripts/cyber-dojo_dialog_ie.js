45949f48e2c928e2df5d0589859c813f15a6deee
Jon Jagger <jon@jaggersoft.com>
2014-08-27 15:32:19 +0100

    ie detection working

---
+++ app/assets/javascripts/cyber-dojo_dialog_ie.js
@@ -0,0 +1,37 @@
+/*global $,cyberDojo*/
+
+var cyberDojo = (function(cd, $) {
+  "use strict";
+
+  cd.dialog_ie = function(yesAction) {
+    if ($.browser.msie) {
+      var html = "<div class='dialog'>" +
+        "cyber-dojo is untested for Internet Explorer.<br/>" +
+        "Do you wish to continue?" +
+        "</div>";
+
+      var buttons = { };
+      buttons['yes'] = function() {
+        yesAction();
+        $(this).dialog('close');
+      };
+      buttons['no'] = function() {
+        $(this).dialog('close');
+      };
+      var warning = $('<div>')
+        .html(html)
+        .dialog({
+          title: cd.dialogTitle('browser warning'),
+          autoOpen: false,
+          width: 500,
+          modal: true,
+          buttons: buttons
+        });
+      warning.dialog('open');
+    } else {
+      yesAction();
+    }
+  };
+
+  return cd;
+})(cyberDojo || {}, $);
