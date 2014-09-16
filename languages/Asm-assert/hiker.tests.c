b202299a0081a27fe0cc1afa9bc87013460a2d9e
Jon Jagger <jon@jaggersoft.com>
2014-09-16 13:56:19 +0100

    Asm docker work

---
+++ languages/Asm-assert/hiker.tests.c
@@ -0,0 +1,16 @@
+#include "hiker.h"
+#include <assert.h>
+#include <stdio.h>
+
+static void life_the_universe_and_everything(void)
+{
+    // a simple example to start you off
+    assert(answer() == 42);
+}
+
+int main(void)
+{
+    life_the_universe_and_everything();
+    // green-traffic light pattern...
+    puts("All tests passed");
+}
