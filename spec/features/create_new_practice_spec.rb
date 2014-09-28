d9ce0d7c12b043c83b2d99402087daf1f4e766fa
Jon Jagger <jon@jaggersoft.com>
2014-09-28 15:54:02 +0100

    looking to resurrect rspec tests

--- spec/features/create_new_practice_spec.rb
+++ spec/features/create_new_practice_spec.rb
@@ -2,22 +2,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 # this is a huge badass feature test that verifies everything at once
-feature "Start a practice" do
+feature "Create a practice" do
 
   scenario "Practice doesn't exist yet" do
     visit "/"
-    find('#setup_button').click
+    find('#create-button').click
     find('[data-language="Ruby-Rspec"]').click
     find('#ok').click
-    find('#start_button').click
+    find('#enter-button').click
     click_button('ok')
     new_window = page.driver.browser.window_handles.last
     page.within_window new_window do
       within '#filename_list' do
         page.should have_text 'cyber-dojo.sh'
         page.should have_text 'instructions'
-        page.should have_text 'untitled.rb'
-        page.should have_text 'untitled_spec.rb'
+        page.should have_text 'hiker.rb'
+        page.should have_text 'hiker_spec.rb'
       end
     end
   end
--- spec/test_cyber_dojo.rb
+++ spec/test_cyber_dojo.rb
@@ -2,3 +2,4 @@ require 'capybara'
 
 Capybara.run_server = false
 Capybara.app_host = 'http://www.cyber-dojo.com/'
+#Capybara.app_host = 'localhost:3000'
