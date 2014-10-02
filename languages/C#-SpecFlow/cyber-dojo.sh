e2452a72198e5889a79f540aec5eb3715b27cec8
Seb Rose <seb@claysnow.co.uk>
2014-10-02 17:10:09 +0100

    Move build script into cyber-dojo.sh

--- languages/C#-SpecFlow/cyber-dojo.sh
+++ languages/C#-SpecFlow/cyber-dojo.sh
@@ -1,8 +1,24 @@
 #build project file
-./buildCsprojFile.sh
+CSPROJ=RunTests.csproj
+
+echo "<Project xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">" > $CSPROJ
+echo "  <ItemGroup>" >> $CSPROJ
+
+shopt -s nullglob
+
+for f in *.feature
+do
+  echo "    <None Include=\"$f\">" >> $CSPROJ
+  echo "      <Generator>SpecFlowSingleFileGenerator</Generator>" >> $CSPROJ
+  echo "      <LastGenOutput>$f.cs</LastGenOutput>" >> $CSPROJ
+  echo "    </None>" >> $CSPROJ
+done
+
+echo "  </ItemGroup>" >> $CSPROJ
+echo "</Project>" >> $CSPROJ
 
 #generate 'code behind'
-mono specflow RunTests.csproj
+mono specflow.exe RunTests.csproj
 
 dmcs -t:library -r:/usr/lib/cli/nunit.framework-2.6/nunit.framework.dll -out:RunTests.dll *.cs
 if [ $? -eq 0 ]; then
