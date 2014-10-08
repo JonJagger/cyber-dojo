54d0b5dc38bbfabd2e450849dddb9771e836e5ab
Steve Coffman <gears@umich.edu>
2014-10-08 15:51:53 -0400

    Make should style be the default, give assert example for sinon

--- languages/Javascript-mocha_chai_sinon/hikerTest.js
+++ languages/Javascript-mocha_chai_sinon/hikerTest.js
@@ -7,8 +7,8 @@ var answer = require('./hiker.js');
 // It's only to help you get started.
 //----------------------------------------------------
 
-describe('Assert Style: Answer', function () {
+describe('Should Style: Answer', function () {
   it('to life the universe and everything', function () {
-    assert.equal(answer(), 42);
+    answer().should.equal(42);
   });
 });
