3e637774597509d76c1fe82661dea731329ff120
Matteo Vaccari <vaccari@pobox.com>
2014-10-09 21:16:13 +0200

    Add JMock support

---
+++ languages/Java-1.8_JMock/Dog.java
@@ -0,0 +1,12 @@
+
+public class Dog {
+
+    public Dog(Waggable waggable) {
+        this.waggable = waggable;
+    }
+
+    public void expressHappiness() {
+    }
+
+    private Waggable waggable;
+}
