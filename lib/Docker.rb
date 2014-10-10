2b73d2a9dc960ab8fe2f4c33e2a46d1f92b7a444
Jon Jagger <jon@jaggersoft.com>
2014-10-10 16:36:38 +0100

    undo refactoring which wasnt

--- lib/Docker.rb
+++ lib/Docker.rb
@@ -4,17 +4,13 @@ module Docker
   def self.installed?
     command = self.stderr2stdout('docker info > /dev/null')
     `#{command}`
-    $?.exitstatus === success
+    $?.exitstatus === 0
   end
 
 private
 
   def self.stderr2stdout(cmd)
     cmd + ' 2>&1'
-  end
-
-  def success
-    0
   end
 
 end
