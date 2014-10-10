29f4bd232365c6c73b05e146e5910945827f9b6f
Jon Jagger <jon@jaggersoft.com>
2014-10-10 16:30:00 +0100

    refactoring

--- lib/Docker.rb
+++ lib/Docker.rb
@@ -4,13 +4,17 @@ module Docker
   def self.installed?
     command = self.stderr2stdout('docker info > /dev/null')
     `#{command}`
-    $?.exitstatus === 0
+    $?.exitstatus === success
   end
 
 private
 
   def self.stderr2stdout(cmd)
     cmd + ' 2>&1'
+  end
+
+  def success
+    0
   end
 
 end
