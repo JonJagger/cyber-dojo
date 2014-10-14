ab7ccdfe9e396949335f1e4de5b244ebac32894d
Jon Jagger <jon@jaggersoft.com>
2014-10-14 20:02:46 +0100

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -16,26 +16,26 @@ CyberDojo::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   get 'dojo/index(/:id)' => 'dojo#index'
-  get 'dojo/valid_id_json(/:id)' => 'dojo#valid_id_json'
-  get 'dojo/enter_json(/:id)' => 'dojo#enter_json'
-  get 'dojo/re_enter_json(/:id)' => 'dojo#re_enter_json'
+  get 'dojo/valid_id_json' => 'dojo#valid_id_json'
+  get 'dojo/enter_json' => 'dojo#enter_json'
+  get 'dojo/re_enter_json' => 'dojo#re_enter_json'
 
   get 'setup/show(/:id)' => 'setup#show'
   get 'setup/save' => 'setup#save'
 
-  get  'kata/edit(/:id)' => 'kata#edit'
-  post 'kata/run_tests(/:id)' => 'kata#run_tests'
-  get  'kata/help_dialog(/:id)' => 'kata#help_dialog'
+  get  'kata/edit/:id' => 'kata#edit'
+  post 'kata/run_tests/:id' => 'kata#run_tests'
+  get  'kata/help_dialog' => 'kata#help_dialog'
 
-  get 'dashboard/show(/:id)' => 'dashboard#show'
-  get 'dashboard/progress(/:id)' => 'dashboard#progress'
-  get 'dashboard/heartbeat(/:id)' => 'dashboard#heartbeat'
+  get 'dashboard/show/:id' => 'dashboard#show'
+  get 'dashboard/progress' => 'dashboard#progress'
+  get 'dashboard/heartbeat' => 'dashboard#heartbeat'
 
-  get 'differ/diff(/:id)' => 'differ#diff'
-  get 'forker/fork(/:id)' => 'forker#fork'
-  get 'reverter/revert(/:id)' => 'reverter#revert'
+  get 'differ/diff' => 'differ#diff'
+  get 'forker/fork' => 'forker#fork'
+  get 'reverter/revert' => 'reverter#revert'
 
-  get 'downloader/download(/:id)' => 'downloader#download'
+  get 'downloader/download/:id' => 'downloader#download'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
