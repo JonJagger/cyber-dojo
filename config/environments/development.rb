986340251aad340a1aef162c7edc1a8fb2300ddb
Jon Jagger <jon@jaggersoft.com>
2014-10-19 10:48:21 +0100

    set config.eager_load=true for development config to avoid NameErrors when controllers are auto reloaded

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@
 CyberDojo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.eager_load = false
+  config.eager_load = true
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+
 CyberDojo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
