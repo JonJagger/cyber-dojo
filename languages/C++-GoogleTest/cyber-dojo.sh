6ed855ab0945395d172efaf805efffefd103b308
Jon Jagger <jon@jaggersoft.com>
2014-10-28 07:28:01 +0000

    improved C++-GoogleTest makefile

--- languages/C++-GoogleTest/cyber-dojo.sh
+++ languages/C++-GoogleTest/cyber-dojo.sh
@@ -1,2 +1,1 @@
-LANG=c
 make
--- languages/C++-GoogleTest/hiker.tests.cpp
+++ languages/C++-GoogleTest/hiker.tests.cpp
@@ -5,6 +5,9 @@ using namespace ::testing;
 
 TEST(Hiker, Life_the_universe_and_everything)
 {
-    // a simple example to start you off
+    // These initial code and test files are completely
+    // *unrelated* to the chosen exercise. They are
+    // merely a simple example to start you off.
+    // You should now delete this comment!
     ASSERT_EQ(42, answer());
 }
