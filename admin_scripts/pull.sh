7b8c17b4e9f4449fffd79445595d7fda8e461543
Jon Jagger <jon@jaggersoft.com>
2014-11-02 16:12:42 +0000

    made git command explicit in admin_scripts/pull.sh

--- admin_scripts/pull.sh
+++ admin_scripts/pull.sh
@@ -4,7 +4,7 @@
 # get latest source from https://github.com/JonJagger/cyberdojo
 # if it asks for a password just hit return
 cd /var/www/cyberdojo
-git pull --no-edit
+git pull origin master --no-edit
 ret=$?
 if [ $ret -ne 0 ]; then
   exit
