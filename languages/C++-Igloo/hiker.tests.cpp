f39423b513e3bc758c22361df363c8d97dcc01d8
Byran Wills-Heath <byran@adgico.co.uk>
2014-11-07 21:18:33 +0000

    Changed Igloo default test so it uses the context syntax

--- languages/C++-Igloo/hiker.tests.cpp
+++ languages/C++-Igloo/hiker.tests.cpp
@@ -1,10 +1,10 @@
 #include "hiker.hpp"
-#include <igloo/igloo_alt.h>
+#include <igloo/igloo.h>
 using namespace igloo;
 
-Describe(Hiker)
+Context(Hiker)
 {
-  It(Life_the_universe_and_everything)
+  Spec(Life_the_universe_and_everything)
   {
     Assert::That(answer(), Equals(42));
   }
