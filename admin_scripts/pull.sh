2400abf3c671b8a312f5271a3ea4ac530999e3c6
Jon Jagger <jon@jaggersoft.com>
2014-11-19 18:20:33 +0000

    folder rename

--- admin_scripts/pull.sh
+++ admin_scripts/pull.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Use to install the latest cyberdojo git repo onto a cyber-dojo server
 
-# get latest source from https://github.com/JonJagger/cyberdojo
+# get latest source from https://github.com/JonJagger/cyber-dojo
 # if it asks for a password just hit return
-cd /var/www/cyberdojo
+cd /var/www/cyber-dojo
 git pull --no-edit origin master
 ret=$?
 if [ $ret -ne 0 ]; then
@@ -18,22 +18,22 @@ chmod g+s katas
 for folder in app config exercises languages lib log notes public script spec test
 do
   echo "chown/chgrp www-data ${folder}"
-  eval "chown -R www-data /var/www/cyberdojo/$folder"
-  eval "chgrp -R www-data /var/www/cyberdojo/$folder"
+  eval "chown -R www-data /var/www/cyber-dojo/$folder"
+  eval "chgrp -R www-data /var/www/cyber-dojo/$folder"
 done
 
 echo "chown/chgrp www-data *"
-chown www-data /var/www/cyberdojo/*
-chgrp www-data /var/www/cyberdojo/*
+chown www-data /var/www/cyber-dojo/*
+chgrp www-data /var/www/cyber-dojo/*
 
 echo "chown/chgrp www-data .*"
-chown www-data /var/www/cyberdojo/.*
-chgrp www-data /var/www/cyberdojo/.*
+chown www-data /var/www/cyber-dojo/.*
+chgrp www-data /var/www/cyber-dojo/.*
 
 echo "chown/chgrp www-data tmp/cache"
 mkdir -p tmp/cache
-chown www-data /var/www/cyberdojo/tmp/cache
-chgrp www-data /var/www/cyberdojo/tmp/cache
+chown www-data /var/www/cyber-dojo/tmp/cache
+chgrp www-data /var/www/cyber-dojo/tmp/cache
 
 # poke rails
 bundle install
