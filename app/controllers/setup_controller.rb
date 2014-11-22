597509b1d557c1081eceb18ded878367f3f5a2af
Jon Jagger <jon@jaggersoft.com>
2014-11-22 21:45:04 +0000

    refactoring

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -2,8 +2,8 @@
 class SetupController < ApplicationController
 
   def show
-    languages_cache || languages_read
-    exercises_cache || exercises_read
+    read_languages
+    read_exercises
     @selected_language_index = choose_language(@languages_names, id, dojo.katas)
     @selected_exercise_index = choose_exercise(@exercises_names, id, dojo.katas)
     @id = id
@@ -23,44 +23,35 @@ private
 
   include Chooser
 
-  def languages_cache
+  def read_languages
     dir = disk[dojo.languages.path]
-    # cache.json is created with cyber-dojo/languages/cache.rb
     if dir.exists?(cache_filename)
+      # cache.json is created with cyber-dojo/languages/cache.rb
       @languages = JSON.parse(dir.read(cache_filename)).sort
-      @languages_names = @languages.map{|array| array[0]}
-      return true
+    else
+      @languages = dojo.languages.select{|language| language.runnable?}.map{|language|
+        [language.name,language.display_name]
+      }.sort
     end
-    return false
-  end
-
-  def languages_read
-    @languages = dojo.languages.select{|language| language.runnable?}.map{|language|
-      [language.name,language.display_name]
-    }.sort
     @languages_names = @languages.map{|array| array[0]}
   end
 
-  def exercises_cache
+  def read_exercises
     dir = disk[dojo.exercises.path]
-    # cache.json is created with cyber-dojo/exercises/cache.rb
     if dir.exists?(cache_filename)
+      # cache.json is created with cyber-dojo/exercises/cache.rb
       cache = JSON.parse(dir.read(cache_filename)).sort
       @exercises_names = cache.map{|one| one[0]}
       @instructions = { }
       cache.each do |one|
         @instructions[one[0]] = one[1]
       end
-      return true
-    end
-    return false
-  end
-
-  def exercises_read
-    @exercises_names = dojo.exercises.map{|exercise| exercise.name}.sort
-    @instructions = { }
-    @exercises_names.each do |name|
-      @instructions[name] = dojo.exercises[name].instructions
+    else
+      @exercises_names = dojo.exercises.map{|exercise| exercise.name}.sort
+      @instructions = { }
+      @exercises_names.each do |name|
+        @instructions[name] = dojo.exercises[name].instructions
+      end
     end
   end
 
