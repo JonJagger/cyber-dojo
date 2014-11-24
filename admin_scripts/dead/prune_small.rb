5017abdd6394161b598e5c87f981e41cc0312404
Jon Jagger <jon@jaggersoft.com>
2014-11-24 19:58:26 +0000

    hub notes update

--- admin_scripts/dead/prune_small.rb
+++ admin_scripts/dead/prune_small.rb
@@ -1,19 +1,25 @@
-# A ruby script to prune folders containing old katas with very few traffic lights.
-# Katas with a few traffic lights don't use a lot of inodes but there are a lot
-# of them and unless regular pruning occurs the server will run out of inodes.
-# Note: There is no real need to zipup old small katas before pruning.
+#!/usr/bin/env ruby
+
+# A ruby script to prune folders containing old
+# katas with very few traffic lights.
+# Katas with a few traffic lights don't use a lot
+# of inodes but there are a lot of them and unless
+# regular pruning occurs a small server could run
+# out of inodes. And they arguably don't want to
+# appear on a read-only hub anyway.
 #
-# e.g. start by not deleting with max_traffic_lights==2 and min_days_old==7
-#   >ruby prune_small.rb false 2 7
+# e.g. start by not deleting,
+# eg with max_traffic_lights==1 and min_days_old==7
+#   $./prune_small.rb 1 7 false
 # this will print what will be deleted. Then
-#   >ruby prune_small.rb true 2 7
+#   $./prune_small.rb 1 7 true
 # to actually do the pruning
 
 require './script_lib.rb'
 
-do_delete = (ARGV[0] || "false")
-max_traffic_light_count = (ARGV[1] || "2").to_i
-min_days_old = (ARGV[2] || "7").to_i
+max_traffic_light_count = ARGV[0].to_i
+min_days_old = ARGV[1].to_i
+do_delete = (ARGV[2] || "false")
 
 prune(do_delete) { |traffic_light_count,days_old|
   traffic_light_count <= max_traffic_light_count and days_old >= min_days_old
