5536939162b9fadf56187811e1c201fc15baa88a
Jon Jagger <jon@jaggersoft.com>
2014-12-13 14:08:48 +0000

    removed dead comments

--- test/languages/test_all_languages.rb
+++ test/languages/test_all_languages.rb
@@ -23,25 +23,3 @@ class AllLanguagesTests < CyberDojoTestBase
 
 end
 
-# There is currently a rights issue for this test
-# If you run this test as root then it won't work
-# because it creates new sub folders under test/cyberdojo/katas
-# which are then owned by root and not www-data
-# You have to run the tests as www-data
-# However, www-data does not have a shell in /etc/passwd
-# So you have to temporaily edit etc/passwd so the shell
-# for www-data is /bin/bash
-# Then
-# $ su - www-data
-# $ cd /var/www/cyber-dojo/test/languages
-# $ ./check_one_language.rb C-assert verbose
-# Then exit
-# Then edit www-data in etc/passwd back to no shell
-# /usr/sbin/nologin
-#
-# Of course I should create a cyber-dojo user and
-# 1) ensure rails create folders/files owned by it
-# 2) docker run -u cyber-dojo
-# 3) then I could run the tests as this user
-#
-#
