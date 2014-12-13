d3d19c66822291d47a52ddf1b9b0abe8e9b03e79
Jon Jagger <jon@jaggersoft.com>
2014-12-13 11:14:29 +0000

    refactoring

--- test/languages/run_all.sh
+++ test/languages/run_all.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# after running this check the coverage in index.html
-
-rm -rf ../../coverage/.resultset.json
-testrb . 2>&1 | tee log.tmp
-cp -R ../../coverage/* .
-ruby ../print_coverage_percent.rb index.html languages | tee -a log.tmp
+../run_one.sh languages
