6e0a6e5770d4d4aacfbd61f9ee5d4a68efb2d25d
Jon Jagger <jon@jaggersoft.com>
2014-12-13 11:28:57 +0000

    reinstated failure and skip count in test summary

--- test/print_summary.rb
+++ test/print_summary.rb
@@ -45,7 +45,9 @@ def gather_stats
     m = log.match(Regexp.new(pattern))
     h[:test_count] = m[1].to_i
     h[:assertion_count] = m[2].to_i
+    h[:failure_count] = m[3].to_i
     h[:error_count] = m[4].to_i
+    h[:skip_count] = m[5].to_i
     # Coverage = 100.0%
     pattern = 'Coverage = ' + tally + '%'
     m = log.match(Regexp.new(pattern))
@@ -59,7 +61,9 @@ def print_heading
   print_left(15,'')
   print_right( 5,'#t')        # number of tests
   print_right( 7,'#ass')      # number of assertions
-  print_right( 8,'#errors')   # number of errors
+  print_right( 3,'#f')        # number of failures
+  print_right( 3,'#e')        # number of errors
+  print_right( 3,'#s')        # number of skips
   print_right( 9,'took(s)')   # time in seconds
   print_right( 9,'t/s')       # tests per second
   print_right( 9,'ass/s')     # assertions per second
@@ -74,7 +78,9 @@ def print_module(stats)
     print_left(15,mod)
     print_right( 5,h[:test_count])
     print_right( 7,h[:assertion_count])
-    print_right( 8,h[:error_count])
+    print_right( 3,h[:failure_count])
+    print_right( 3,h[:error_count])
+    print_right( 3,h[:skip_count])
     print_right( 9,h[:took])
     print_right( 9,h[:tests_per_sec])
     print_right( 9,h[:assertions_per_sec])
@@ -84,11 +90,13 @@ def print_module(stats)
 end
 
 def print_totals(stats)
-  puts '- ' * 36
+  puts ' -' * 36
   print_left(15,'total')
   print_right( 5,    c=stats.map{|_,h| h[:test_count].to_i}.reduce(:+))
   print_right( 7,    a=stats.map{|_,h| h[:assertion_count].to_i}.reduce(:+))
-  print_right( 8,      stats.map{|_,h| h[:error_count]}.reduce(:+))
+  print_right( 3,      stats.map{|_,h| h[:failure_count]}.reduce(:+))
+  print_right( 3,      stats.map{|_,h| h[:error_count]}.reduce(:+))
+  print_right( 3,      stats.map{|_,h| h[:skip_count]}.reduce(:+))
   print_right( 9, t=f2(stats.map{|_,h| h[:took].to_f}.reduce(:+)))
   print_right( 9, f2(c / t.to_f))
   print_right( 9, f2(a / t.to_f))
@@ -97,7 +105,7 @@ def print_totals(stats)
 end
 
 def print_line
-  puts '-' * 71
+  puts '-' * 72
 end
 
 #- - - - - - - - - - - - - - - - - - - - -
