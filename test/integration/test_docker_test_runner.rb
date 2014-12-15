8ba9e568b4c3b21b274f6b6aadc95321a3504b70
Jon Jagger <jon@jaggersoft.com>
2014-12-15 11:11:09 +0000

    DockerTestRunner test refactoring so I can adapt .run

--- test/integration/test_docker_test_runner.rb
+++ test/integration/test_docker_test_runner.rb
@@ -1,31 +1,33 @@
 #!/usr/bin/env ruby
 
 require_relative '../cyberdojo_test_base'
-require_relative 'externals'
 
 class DockerTestRunnerTests < CyberDojoTestBase
 
-  include Externals
   include TimeNow
+
+  def externals(runner)
+    {
+      :disk   => OsDisk.new,
+      :git    => Git.new,
+      :runner => runner
+    }
+  end
+
+  def runner
+    assert Docker.installed?
+    DockerTestRunner.new
+  end
 
   def setup
     super
-    ext = externals
-    runner = ext[:runner]
-    assert_equal "DockerTestRunner", runner.class.name
-
-    #I can wrap runner so runner.run (as called
-    #in avatar.test) can be adapted and call
-    #adapted runner.inner_run when I need to bypass
-    #the inner timeout
-
-    @dojo = Dojo.new(root_path,ext)
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test "DockerTestRunner when outer and inner commands do not timeout" do
-    kata = make_kata(@dojo, 'C-assert')
+    dojo = Dojo.new(root_path,externals(runner))
+    kata = make_kata(dojo, 'C-assert')
     lion = kata.start_avatar(['lion'])
     visible_files = lion.tags[0].visible_files
     delta = {
@@ -42,7 +44,8 @@ class DockerTestRunnerTests < CyberDojoTestBase
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test "DockerTestRunner when inner command times out" do
-    kata = make_kata(@dojo, 'C-assert')
+    dojo = Dojo.new(root_path,externals(runner))
+    kata = make_kata(dojo, 'C-assert')
     lion = kata.start_avatar(['lion'])
     visible_files = lion.tags[0].visible_files
     source = visible_files['hiker.c']
@@ -61,5 +64,10 @@ class DockerTestRunnerTests < CyberDojoTestBase
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  #I can wrap runner so runner.run (as called
+  #in avatar.test) can be adapted and call
+  #adapted runner.inner_run when I need to bypass
+  #the inner timeout
 
 end
