0cb6801bf5b9761186bb8e19e370276f2f85df1b
Jon Jagger <jon@jaggersoft.com>
2014-12-15 10:59:23 +0000

    DockerTestRunner and inner-command times out

--- test/integration/test_docker_test_runner.rb
+++ test/integration/test_docker_test_runner.rb
@@ -21,6 +21,8 @@ class DockerTestRunnerTests < CyberDojoTestBase
     @dojo = Dojo.new(root_path,ext)
   end
 
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   test "DockerTestRunner when outer and inner commands do not timeout" do
     kata = make_kata(@dojo, 'C-assert')
     lion = kata.start_avatar(['lion'])
@@ -36,5 +38,27 @@ class DockerTestRunnerTests < CyberDojoTestBase
     assert_equal '', `docker ps -a -q`
   end
 
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test "DockerTestRunner when inner command times out" do
+    kata = make_kata(@dojo, 'C-assert')
+    lion = kata.start_avatar(['lion'])
+    visible_files = lion.tags[0].visible_files
+    source = visible_files['hiker.c']
+    visible_files['hiker.c'] = source.sub('{', '{for(;;);')
+    delta = {
+      :changed => [ 'hiker.c' ],
+      :unchanged => visible_files.keys - [ 'hiker.c' ],
+      :deleted => [ ],
+      :new => [ ]
+    }
+    rags,_,_ = lion.test(delta, visible_files)
+    output = lion.tags[1].visible_files['output']
+    assert output.start_with?('Unable to complete the tests in 15 seconds')
+    assert_equal 'amber', rags[-1]['colour']
+    assert_equal '', `docker ps -a -q`
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 end
