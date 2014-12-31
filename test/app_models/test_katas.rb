441e8f2c54f0ec5516f896717f7fd292f23e672c
Fred <jon@jaggersoft.com>
2014-12-31 19:12:06 +0000

    refactoring

--- test/app_models/test_katas.rb
+++ test/app_models/test_katas.rb
@@ -32,22 +32,30 @@ class KatasTests < ModelTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test 'katas.complete fails when no candidates' do
+  test 'complete(id=nil) is ""' do
+    assert_equal "", @dojo.katas.complete(nil)
+  end
+
+  test 'complete(id="") is ""' do
+    assert_equal '', @dojo.katas.complete('')
+  end
+
+  test 'complete(id): unchanged when no candidates' do
     assert_equal '23', @dojo.katas.complete('23')
   end
 
-  test 'katas.complete fails when id is less than 4 chars in length' do
+  test 'complete(id): id unchanged when id is less than 4 chars in length' do
     id = make_kata.id[0..2]
     assert_equal 3, id.length
     assert_equal id, @dojo.katas.complete(id)
   end
 
-  test 'katas.completes when id is 4 chars long' do
+  test 'complete(id): id unchanged when 4 chars long' do
     id = make_kata.id
     assert_equal id, @dojo.katas.complete(id[0..3])
   end
 
-  test 'katas.complete fails when 2+ matches' do
+  test 'complete(id): id unchanged when 2+ matches' do
     prefix = '12345678A'
     id1 = prefix + 'B'
     kata1 = make_kata(id1)
