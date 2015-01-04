7a76762fd863cbc1f9691047626d249ccd6ccabc
Michael Birtwell <michael.birtwell@gmail.com>
2015-01-04 17:17:21 +0000

    Add C++ Boost.Test config
    Just a copy of google test for now

---
+++ languages/C++-Boost.Test/hiker.tests.cpp
@@ -0,0 +1,13 @@
+#include "hiker.hpp"
+#include <gtest/gtest.h>
+
+using namespace ::testing;
+
+TEST(Hiker, Life_the_universe_and_everything)
+{
+    // The initial code and test files are completely
+    // *unrelated* to the chosen exercise. They are
+    // merely a simple example to start you off.
+    // You should now delete this comment!
+    ASSERT_EQ(42, answer());
+}
