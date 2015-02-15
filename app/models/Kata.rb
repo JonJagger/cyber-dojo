00e02e97bcb538484356e043ee0bb5a3bdb7ff32
Fred <jon@jaggersoft.com>
2015-02-15 19:01:38 +0000

    refactoring

--- app/models/Kata.rb
+++ app/models/Kata.rb
@@ -9,11 +9,9 @@ class Kata
   attr_reader :katas, :id
 
   def start_avatar(avatar_names = Avatars.names.shuffle)
-    started_avatar_names = avatars.each.collect { |avatar| avatar.name }
-    free_avatar_names = avatar_names - started_avatar_names
-    avatar = nil
-    if free_avatar_names != [ ]
-      avatar = Avatar.new(self,free_avatar_names[0])
+    free_names = avatar_names - started_avatar_names
+    if free_names != [ ]
+      avatar = Avatar.new(self,free_names[0])
       avatar.start
     end
     avatar
@@ -35,25 +33,25 @@ class Kata
     Avatars.new(self)
   end
 
-  def created
-    Time.mktime(*manifest['created'])
-  end
-
   def age(now = Time.now.to_a[0..5].reverse)
     return 0 if !active?
     return (Time.mktime(*now) - earliest_light).to_i
   end
 
+  def created
+    Time.mktime(*manifest_property)
+  end
+
   def visible_files
-    manifest['visible_files']
+    manifest_property
   end
 
   def language
-    dojo.languages[manifest['language']]
+    dojo.languages[manifest_property]
   end
 
   def exercise
-    dojo.exercises[manifest['exercise']]
+    dojo.exercises[manifest_property]
   end
 
 private
@@ -61,8 +59,17 @@ private
   include ExternalDiskDir
   include ExternalGit
 
+  def manifest_property
+    property_name = (caller[0] =~ /`([^']*)'/ and $1)
+    manifest[property_name]
+  end
+
+  def manifest_filename
+    'manifest.json'
+  end
+  
   def manifest
-    @manifest ||= JSON.parse(dir.read('manifest.json'))
+    @manifest ||= JSON.parse(dir.read(manifest_filename))
   end
 
   def earliest_light
@@ -73,7 +80,7 @@ private
   end
 
   def dojo
-    @katas.dojo
+    katas.dojo
   end
 
   def outer(id)
@@ -82,6 +89,10 @@ private
 
   def inner(id)
     id[2..-1] # '6A3327FE'
+  end
+
+  def started_avatar_names
+    avatars.each.collect { |avatar| avatar.name }
   end
 
 end
