97a288ef7d21d91f7ae3c94e6aba50dfa2d66687
Jon Jagger <jon@jaggersoft.com>
2015-02-25 05:45:13 +0000

    refactoring

--- app/models/Avatar.rb
+++ app/models/Avatar.rb
@@ -33,8 +33,7 @@ class Avatar
   end
 
   def tags
-    (0..increments.length).map { |n| Tag.new(self,n) }
-    #Tags.new(self)
+    Tags.new(self)
   end
 
   def lights
@@ -154,61 +153,4 @@ end
 
 
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# tags
-# lights
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# When a new avatar enters a dojo, kata.start_avatar()
-# will do a 'git commit' + 'git tag' for tag 0 (Zero).
-# This initial tag is *not* recorded in the
-# increments.json file which starts as [ ]
-#
-# All subsequent 'git commit' + 'git tag' commands
-# correspond to a gui action and store an entry in
-# the increments.json file.
-# eg
-# [
-#   {
-#     'colour' => 'red',
-#     'time' => [2014, 2, 15, 8, 54, 6],
-#     'number' => 1
-#   },
-# ]
-#
-# At the moment the only gui action that creates an
-# increments.json file entry is a [test] event.
-#
-# However, I may create finer grained tags than
-# just [test] events...
-#    o) creating a new file
-#    o) renaming a file
-#    o) deleting a file
-#    o) editing a file (and opening a different file)
-#
-# If this happens the difference between a Tag.new
-# and a Light.new will be more pronounced and I will
-# need something like this (where non test events
-# will have a new non red/amber/green colour) ...
-#
-# def lights
-#   rag = ['red','amber','green']
-#   increments.select{ |inc|
-#     rag.include?(inc.colour)
-#   }.map { |inc|
-#     Light.new(self,inc)
-#   }
-# end
-#
-# ------------------------------------------------------
-# Invariants
-#
-# If the latest tag is N then increments.length == N
-#
-# The inclusive upper bound for n in avatar.tags[n] is
-# always the current length of increments.json (even if
-# that is zero) which is also the latest tag number.
-#
-# The inclusive lower bound for n in avatar.tags[n] is
-# zero. When an animal does a diff of [1] what is run is
-#   avatar.tags[was_tag=0].diff(now_tag=1)
-#
+
--- app/models/Tags.rb
+++ app/models/Tags.rb
@@ -1,3 +1,4 @@
+# comments at end of file
 
 class Tags
   include Enumerable
@@ -12,7 +13,11 @@ class Tags
 
   def each
     return enum_for(:each) unless block_given?
-    tags.each { |tag| yield tag }
+    (0...length).each { |n| yield Tag.new(self,n) }
+  end
+
+  def length
+    1 + tags.length
   end
 
 private
@@ -28,3 +33,64 @@ private
   end
 
 end
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# tags vs lights
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# When a new avatar enters a dojo, kata.start_avatar()
+# will do a 'git commit' + 'git tag' for tag 0 (Zero).
+# This initial tag is *not* recorded in the
+# increments.json file which starts as [ ]
+#
+# All subsequent 'git commit' + 'git tag' commands
+# correspond to a gui action and store an entry in
+# the increments.json file.
+# eg
+# [
+#   {
+#     'colour' => 'red',
+#     'time' => [2014, 2, 15, 8, 54, 6],
+#     'number' => 1
+#   },
+# ]
+#
+# At the moment the only gui action that creates an
+# increments.json file entry is a [test] event.
+#
+# However, I may create finer grained tags than
+# just [test] events...
+#    o) creating a new file
+#    o) renaming a file
+#    o) deleting a file
+#    o) editing a file (and opening a different file)
+#
+# If this happens the difference between a Tag.new
+# and a Light.new will be more pronounced and I will
+# need something like this (where non test events
+# will have a new non red/amber/green colour) ...
+#
+# def lights
+#   rag = ['red','amber','green']
+#   increments.select{ |inc|
+#     rag.include?(inc.colour)
+#   }.map { |inc|
+#     Light.new(self,inc)
+#   }
+# end
+#
+# ------------------------------------------------------
+# Invariants
+#
+# If the latest tag is N then increments.length == N
+#
+# The inclusive upper bound for n in avatar.tags[n] is
+# always the current length of increments.json (even if
+# that is zero) which is also the latest tag number.
+#
+# The inclusive lower bound for n in avatar.tags[n] is
+# zero. When an animal does a diff of [1] what is run is
+#   avatar.diff(0,1)
+# which is a diff between
+#   avatar.tags[0] and avatar.tags[1]
+#
+
