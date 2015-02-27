fb8235cb3f27d44d96f762f57ee8ab76db409810
Jon Jagger <jon@jaggersoft.com>
2015-02-27 11:51:00 +0100

    refactoring

--- app/assets/javascripts/cyber-dojo_dialog_enter.js
+++ app/assets/javascripts/cyber-dojo_dialog_enter.js
@@ -3,9 +3,9 @@
 var cyberDojo = (function(cd, $) {
   "use strict";
 
-  cd.dialog_enter = function(title, id, ok, avatarName, dialogHtml) {
+  cd.dialog_enter = function(id, avatarName, dialogHtml) {
     var i18nButtons =  { };
-    i18nButtons[ok] = function() {
+    i18nButtons['ok'] = function() {
       var url = '/kata/edit/' + id + '?' +
                 'avatar=' + avatarName;
       window.open(url);
@@ -14,7 +14,7 @@ var cyberDojo = (function(cd, $) {
     return $('<div class="dialog">')
       .html(dialogHtml)
       .dialog({
-        title: cd.dialogTitle(title),
+        title: cd.dialogTitle('enter'),
         autoOpen: false,
         width: 350,
         modal: true,
