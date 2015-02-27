91861f54ea8404bdbbfff71120599c8716f457c5
Jon Jagger <jon@jaggersoft.com>
2015-02-27 14:17:08 +0000

    refactoring

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -2,27 +2,24 @@
 class SetupController < ApplicationController
 
   def show
+    @id = id
+    @title = 'create'
+
     @languages,languages_names = read_languages
     @exercises_names,@instructions = read_exercises
         
     @languages_display_names = @languages.map{|array| array[0]}
-    
-    @selected_language_index = choose_language(@languages_display_names, id, dojo.katas)
-    @selected_exercise_index = choose_exercise(@exercises_names, id, dojo.katas)
-    @id = id
-    @title = 'create'
-    
-    @split = ::LanguagesDisplayNamesSplitter.new(@languages_display_names, @selected_language_index)
-    @selected_language_index = @split.language_selected_index        
+    @initial_language_index = choose_language(@languages_display_names, id, dojo.katas)
+    @initial_exercise_index = choose_exercise(@exercises_names, id, dojo.katas)    
+    @split = ::LanguagesDisplayNamesSplitter.new(@languages_display_names, @initial_language_index)
+    @initial_language_index = @split.language_selected_index
   end
 
   def save
     language = dojo.languages[params['language'] + '-' + params['test']]
     exercise = dojo.exercises[params['exercise']]
     kata = dojo.katas.create_kata(language, exercise)
-    render :json => {
-      :id => kata.id.to_s
-    }
+    render :json => { :id => kata.id.to_s }
   end
 
 private
