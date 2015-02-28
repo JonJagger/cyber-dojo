7e5b326f6dc4bc4a0d4a92b25fb21d0353f45309
Jon Jagger <jon@jaggersoft.com>
2015-02-28 16:37:59 +0000

    split pull.sh in two so I can run tests on MacBookPro and Ubuntu

--- admin_scripts/pull.sh
+++ admin_scripts/pull.sh
@@ -12,32 +12,11 @@ if [ $ret -ne 0 ]; then
   exit
 fi
 
-apt-get install -y acl
-
 echo "refreshing exercises/ cache"
 $cyberDojoHome/exercises/cache.rb
 
 echo "refreshing languages/ cache"
 $cyberDojoHome/languages/cache.rb
-
-# cyber-dojo creates folders under katas
-echo "chown/chgrp www-data katas"
-chmod g+rwsx katas
-setfacl -d -m group:www-data:rwx $cyberDojoHome/katas
-setfacl -m group:www-data:rwx $cyberDojoHome/katas
-
-# ensure pulled files have correct rights
-for folder in app config exercises languages lib log notes public script spec test
-do
-  echo "chown www-data:www-data ${folder}"
-  chown -R www-data:www-data $cyberDojoHome/$folder
-done
-
-# tests create folders under tests/cyber-dojo/katas
-echo "chown www-data test/cyber-dojo"
-chmod g+rwsx $cyberDojoHome/test/cyber-dojo
-setfacl -d -m group:www-data:rwx $cyberDojoHome/test/cyber-dojo
-setfacl -m group:www-data:rwx $cyberDojoHome/test/cyber-dojo
 
 echo "poking rails"
 bundle install
@@ -45,13 +24,3 @@ bundle install
 echo "restarting apache"
 service apache2 restart
 
-echo "chown www-data:www-data *"
-chown www-data:www-data $cyberDojoHome/*
-
-echo "chown www-data:www-data .*"
-chown www-data:www-data $cyberDojoHome/.*
-
-echo "chown -R www-data:www-data tmp"
-rm -rf tmp/cache
-mkdir -p tmp/cache
-chown -R www-data:www-data $cyberDojoHome/tmp
