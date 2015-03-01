aa6b04bd9f5bdc111022609a4d8497140ecb1df6
Jon Jagger <jon@jaggersoft.com>
2015-03-01 08:49:28 +0000

    refactoring

--- lib/Git.rb
+++ lib/Git.rb
@@ -5,58 +5,54 @@ class Git
     if !File.directory?(path)
       raise "Cannot `cd #{path};git init` because #{path} does not exist"
     end
-    run(path, 'init', args)
+    cd_run(path, 'init', args)
   end
 
   def config(path, args)
-    run(path, 'config', args)
+    cd_run(path, 'config', args)
   end
 
   def add(path, args)
-    run(path, 'add', quoted(args))
+    cd_run(path, 'add', quoted(args))
   end
 
   def rm(path, args)
-    run(path, 'rm', quoted(args))
+    cd_run(path, 'rm', quoted(args))
   end
 
   def commit(path, args)
-    run(path, 'commit', args)
+    cd_run(path, 'commit', args)
   end
 
   def gc(path, args)
-    run(path, 'gc', args)
+    cd_run(path, 'gc', args)
   end
 
   def tag(path, args)
-    run(path, 'tag', args)
+    cd_run(path, 'tag', args)
   end
 
   def show(path, args)
-    run(path, 'show', args)
+    cd_run(path, 'show', args)
   end
 
   def diff(path, args)
-    run(path, 'diff', args)
+    cd_run(path, 'diff', args)
   end
 
 private
 
   include Cleaner
 
-  def run(path, command, args)
-    log = [ ]
+  def cd_run(path, command, args)
     cd_cmd = "cd #{path}"
     git_cmd = "git #{command} #{args}"
-    cmd = [cd_cmd,git_cmd].map{|s| stderr2stdout(s)}.join(shell_separator)
-    IO.popen(cmd).each do |line|
-      log << line
-    end.close
+    cmds = [cd_cmd,git_cmd].map{ |cmd| stderr2stdout(cmd) }.join(shell_separator)
+    log = [ ]
+    IO.popen(cmds).each{ |line| log << line }.close
     status = $?.exitstatus
     if status != success
-      log << "cmd=#{cmd}"
-      log << "$?.exitstatus=#{status}"
-      log << "output=#{log}"
+      log = [ cmds ] << "FAILURE: $?.exitstatus=#{status}"
     end
     clean(log.join(''))
   end
