d324cd24e71f6ac18dc22313311bfc75851be318
Fred <jon@jaggersoft.com>
2015-03-11 21:50:03 +0000

    notes update

--- admin_scripts/show_kata_language_names.rb
+++ admin_scripts/show_kata_language_names.rb
@@ -22,41 +22,32 @@ names.keys.sort.each do |name|
   p "#{name} #{names[name].size} #{names[name][0]}"
 end
 
-# I think the languages/ sub-folder needs more work.
+# The languages/ sub-folder needs more work.
 # Some existing dojos have language:"Java-1.8_Junit" 
 # in their manifest with the - part of the name
 # Also  Ruby-1.9.3 ---> Ruby1.9.3
 # Also  Ruby-2.1.3 ---> Ruby2.1.3
 # Do some dojo's have a manifest language: entry with , as a separator
-#    and some with a - separator. I think they do. 
-#    Run admin_scripts/show_kata_language_names.rb on main server
+#    and some with a - separator? 
 #
-# Rule: - and , are not allowed in a language folder name or a test folder name
-# Idea: parse a dojo's manifest's language: entry backwards, looking for - or ,
-#       "Java-1.8, assert"
-# does not parse to 
-#       "Java", "-1.8, assert"
-# but to 
-#       "Java-1.8", "assert"
-# Then I might be able to have a separate translation function for the language
-# on its own. (Later refactoring).
+# TODO: Run this script on main server to gather all used language: entries
 #
-# Idea: From now on, store a versionless language name in a kata's
+# RULE: - and , are not allowed in a language folder name or a test folder name
+#
+# IDEA: From now on, store a versionless language name in a kata's
 #       manifest.json file. Then use Languages.rename to map
 #       to folder name which may have the latest version number.
 #       Only way ambiguity can arise is if there are two versions of
 #       the same language+test, eg g++4.8.1-assert and g++4.9-assert
 #       So never do that. And aim to have two versions of the same
 #       language only when upgrading, or when the tests are split 
-#       some in one version and some in another version.
+#       some in one version and some in another version. Eg
+#       C++ (g++4.8.1 and g++4.9) and Ruby (1.9.3 and 2.1.3)
 #
-# Idea: Better still, split the language: entry in the katas manifest.json
-#       file into one entry for the language_name and one entry for the test_name.
-#       These entries are display_names.
-#       They are used to populate the setup page.
-#
-# Idea: Write a script to fix up all the katas on the main server
-#       so the language entry in their manifests are fixed.
+# TODO: Write a script to fix up all the katas on the main server
+#       so the language entry in their manifests is fixed and is
+#       split into two entries, one for the language name and one for 
+#       the test name.
 #
 
 
