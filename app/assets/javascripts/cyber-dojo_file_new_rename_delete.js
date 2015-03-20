18cfe494da0ecd5a77887b846148127a8f442efa
Fred <jon@jaggersoft.com>
2015-03-20 08:57:18 +0000

    tweak CSS on file new/rename/delete dialog

--- app/assets/javascripts/cyber-dojo_file_new_rename_delete.js
+++ app/assets/javascripts/cyber-dojo_file_new_rename_delete.js
@@ -10,39 +10,41 @@ var cyberDojo = (function(cd, $) {
   cd.deleteFile = function(title) {
     var filename = cd.currentFilename();
     var div = $(cd.divPanel(''));
-	var deleter = $('<div>')
-	  .html(div)
-	  .dialog({
-		autoOpen: false,
-		width: 350,
-		title: cd.dialogTitle(title),
-		modal: true,
-		buttons: {
-		  ok: function() {
-			cd.doDelete(filename);
-			$(this).remove();
-		  },
-		  cancel: function() {
-			$(this).remove();
+	  var deleter = $('<div>')
+	    .html(div)
+	    .dialog({
+		    autoOpen: false,
+		    width: 350,
+		    title: cd.dialogTitle(title),
+		    modal: true,
+		    buttons: {
+		    ok: function() {
+			    cd.doDelete(filename);
+			    $(this).remove();
+		    },
+		    cancel: function() {
+			    $(this).remove();
+		    }
 		  }
-		}
 	  });
     var input = $('<input>', {
       type: 'text',
+      id: 'delete_filename',
+      name: 'delete_filename',      
       value: filename,
-	  disabled: "disabled"
+	    disabled: "disabled"
     });
     div.append(input);
-	deleter.dialog('open');
+	  deleter.dialog('open');
   };
 
   // -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
   cd.doDelete = function(filename) {
-	// Also used in cyber-dojo_dialog_diff.js (revert)
+	  // Also used in cyber-dojo_dialog_history.js (revert)
     cd.fileDiv(filename).remove();
     var filenames = cd.rebuildFilenameList();
-	var i = cd.nonBoringFilenameIndex(filenames);
+	  var i = cd.nonBoringFilenameIndex(filenames);
     cd.loadFile(filenames[i]);
   };
 
@@ -62,54 +64,56 @@ var cyberDojo = (function(cd, $) {
       value: newFilename
     });
     var okButton =
-	{
-	  id: 'new_file_ok',
-	  text: 'ok',
-	  disabled: !cd.isValidFilename(newFilename),
-	  click: function() {
-		var newFilename = $.trim(input.val())
-		cd.newFileContent(newFilename, '');
-		$(this).remove();
+	  {
+	    id: 'new_file_ok',
+	    text: 'ok',
+	    disabled: !cd.isValidFilename(newFilename),
+	    click: function() {
+		    var newFilename = $.trim(input.val())
+		    cd.newFileContent(newFilename, '');
+		    $(this).remove();
+	    }
+	  };
+	  var cancelButton =
+	  {
+	    id: 'new_file_cancel',
+	    text: 'cancel',
+	    click: function() {
+		  $(this).remove();
 	  }
 	};
-	var cancelButton =
-	{
-	  id: 'new_file_cancel',
-	  text: 'cancel',
-	  click: function() {
-		$(this).remove();
-	  }
-	};
-    var newFileDialog = $('<div id="new_file_dialog">')
-      .html(div)
-      .dialog({
-		autoOpen: false,
-		width: 350,
-		title: cd.dialogTitle(title),
-		modal: true,
-		buttons: [ okButton, cancelButton ]
-      });
+  
+  // -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
+  var newFileDialog = $('<div id="new_file_dialog">')
+    .html(div)
+    .dialog({
+		  autoOpen: false,
+		  width: 350,
+		  title: cd.dialogTitle(title),
+		  modal: true,
+		  buttons: [ okButton, cancelButton ]
+    });
     div.append(input);
 
-	input.keyup(function(event) {
+  	input.keyup(function(event) {
       var ok = $('#new_file_ok');
-	  newFilename = $.trim(input.val());
+	    newFilename = $.trim(input.val());
       event.preventDefault();
-	  if (cd.isValidFilename(newFilename))  {
+	    if (cd.isValidFilename(newFilename))  {
         ok.button('enable');
-		if (event.keyCode === $.ui.keyCode.ENTER) {
+		    if (event.keyCode === $.ui.keyCode.ENTER) {
           cd.newFileContent(newFilename, '');
-		  newFileDialog.remove();
-		}
-	  } else {
+		      newFileDialog.remove();
+		    }
+	    } else {
         ok.button('disable');
-	  }
+	    }
     });
 
-	// Don't refactor to newFileDialog.dialog('open')
-	// If you do that the dialog only works the first time. See
-	// http://praveenbattula.blogspot.co.uk/2009/08/jquery-dialog-open-only-once-solution.html
+	  // Don't refactor to newFileDialog.dialog('open')
+	  // If you do that the dialog only works the first time. See
+	  // http://praveenbattula.blogspot.co.uk/2009/08/jquery-dialog-open-only-once-solution.html
     $('#new_file_dialog').dialog('open');
     input[0].setSelectionRange(0, newFilename.length);
   };
@@ -141,49 +145,52 @@ var cyberDojo = (function(cd, $) {
       value: oldFilename
     });
     var okButton =
-	{
-	  id: 'rename_file_ok',
-	  text: 'ok',
-	  disabled: !cd.isValidFilename(oldFilename),
-	  click: function() {
-		var newFilename = $.trim(input.val());
-		cd.renameFileFromTo(oldFilename, newFilename);
-		$(this).remove();
+	  {
+	    id: 'rename_file_ok',
+	    text: 'ok',
+	    disabled: !cd.isValidFilename(oldFilename),
+	    click: function() {
+		    var newFilename = $.trim(input.val());
+    		cd.renameFileFromTo(oldFilename, newFilename);
+		    $(this).remove();
+	    }
+	  };
+	  var cancelButton =
+	  {
+  	  id: 'rename_file_cancel',
+	    text: 'cancel',
+	    click: function() {
+		  $(this).remove();
 	  }
 	};
-	var cancelButton =
-	{
-	  id: 'rename_file_cancel',
-	  text: 'cancel',
-	  click: function() {
-		$(this).remove();
-	  }
-	};
-    var renameFileDialog = $('<div id="rename_file_dialog">')
-      .html(div)
-      .dialog({
-		    autoOpen: false,
-		    width: 350,
-		    title: cd.dialogTitle(title),
-		    modal: true,
-		    buttons: [ okButton, cancelButton ]
-      });
+  
+  // -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+  var renameFileDialog = $('<div id="rename_file_dialog">')
+    .html(div)
+    .dialog({
+		  autoOpen: false,
+		  width: 350,
+		  title: cd.dialogTitle(title),
+		  modal: true,
+		  buttons: [ okButton, cancelButton ]
+    });
 
     div.append(input);
 
-	input.keyup(function(event) {
+	  input.keyup(function(event) {
 	  var newFilename = $.trim(input.val());
       var ok = $('#rename_file_ok');
       event.preventDefault();
-	  if (cd.isValidFilename(newFilename))  {
+	    if (cd.isValidFilename(newFilename))  {
         ok.button('enable');
-		if (event.keyCode === $.ui.keyCode.ENTER) {
-		 cd.renameFileFromTo(oldFilename, newFilename);
-		 renameFileDialog.remove();
-		}
-	  } else {
+		    if (event.keyCode === $.ui.keyCode.ENTER) {
+		      cd.renameFileFromTo(oldFilename, newFilename);
+		      renameFileDialog.remove();
+		    }
+	    } else {
         ok.button('disable');
-	  }
+	    }
     });
 
     // Don't refactor to renamer.dialog('open')
@@ -207,7 +214,7 @@ var cyberDojo = (function(cd, $) {
     var caretPos = oldFile.caret();
     $(oldFile).closest('tr').remove();
 
-	cd.newFileContent(newFilename, content);
+	  cd.newFileContent(newFilename, content);
     var div = cd.fileDiv(newFilename);
     cd.rebuildFilenameList();
     cd.loadFile(newFilename);
@@ -217,7 +224,7 @@ var cyberDojo = (function(cd, $) {
     //   fc.scrollLeft(left);
     // here does _not_ work. I use animate instead with a
     // very fast duration==1 and that does work!
-    newFile.animate({scrollTop: scrollTop, scrollLeft: scrollLeft}, 1);
+    newFile.animate({scrollTop:scrollTop, scrollLeft:scrollLeft}, 1);
     newFile.caret(caretPos);
   };
 
@@ -233,4 +240,5 @@ var cyberDojo = (function(cd, $) {
   };
 
   return cd;
+  
 })(cyberDojo || {}, $);
