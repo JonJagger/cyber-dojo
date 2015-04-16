647d873aff02abc0140987172eed0cd644e9636c
Fred <jon@jaggersoft.com>
2015-04-16 16:30:42 +0200

    working towards getting 2nd test/app_controllers/test_setup test passing

--- test/app_controllers/test_setup.rb
+++ test/app_controllers/test_setup.rb
@@ -4,18 +4,18 @@ require_relative 'controller_test_base'
 
 class SetupControllerTest < ControllerTestBase
 
-  def X_setup
-    super
-    set_disk_class_name('DiskFake')
-  end
-
   test 'setup uses cached languages and exercises if present' do
+    
+    #FIX: this overwrites the cache.json files on the ramdisk. 
+    
     languages.dir.write('cache.json', [ 'C++, catch', 'Java, JMock' ])
     exercises.dir.write('cache.json', {
       'fake-Print-Diamond'  => 'fake-Print-Diamond instructions',
       'fake-Roman-Numerals' => 'fake-Roman-Numerals instructions'
     })
+    
     get 'setup/show'    
+    
     assert_response :success
     assert /data-language\=\"C++/.match(html), "C++"
     assert /data-language\=\"Java/.match(html), "Java"
@@ -27,56 +27,34 @@ class SetupControllerTest < ControllerTestBase
   
   # - - - - - - - - - - - - - - - - - - - - - -
 
-=begin
   test 'setup/show chooses language and exercise of kata ' +
        'whose 10-char id is passed in URL ' +
        '(to encourage repetition)' do
-    stub_dojo
-
-    # setup_languages
-    languages_names = [
-      'fakeC++-catch',
-      'fakeC#-NUnit',
-      'fakeJava1.8-JUnit',
-      'fakeRuby1.9.3-TestUnit',
-    ].sort
-
-    languages_names.each do |language_name|
-      stub_language(language_name, 'fakeTestFrameworkName')
-    end
-
-    # setup_exercises
-    exercises_names = [
-      'fakePrintDiamond',
-      'fakeRecentlyUsedList',
-      'fakeRomanNumerals',
-      'fakeYatzy',
-    ].sort
-
-    exercises_names.each do |exercise_name|
-      stub_exercise(exercise_name)
-    end
-
-    language_name = languages_names[2]
-    exercise_name = exercises_names[1]
-    id = '1234512345'
-
-    stub_kata(id, language_name, exercise_name)
+         
+    # this fails because the exercises/ and languages/ folders
+    # contain cache.json files which are used
+    
+    set_runner_class_name('StubTestRunner')
+    dojo.runner.stub_runnable(true)
+    languages_names = languages.each.map{|language| language.name}
+    exercises_names = exercises.each.map{|exercise| exercise.name}
+    language_name = languages_names.shuffle[0]
+    exercise_name = exercises_names.shuffle[0]
+    id = create_kata(language_name,exercise_name)
 
     get 'setup/show', :id => id[0...10]
+    
     assert_response :success
-
     md = /var selectedExercise = \$\('#exercise_' \+ (\d+)/.match(html)
     selected_exercise = exercises_names[md[1].to_i]
     assert_equal exercise_name, selected_exercise, 'exercise'
-
     md = /var selectedLanguage = \$\('#language_' \+ (\d+)/.match(html)
     selected_language = languages_names[md[1].to_i]
     assert_equal language_name, selected_language, 'language'
   end
 
   #- - - - - - - - - - - - - - - - - - -
-
+=begin
   # another test to verify language/exercise default
   # to that of kata's id when id is only 6 chars long.
 
