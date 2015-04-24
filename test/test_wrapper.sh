391ded886ad1a6a70289f5bd29b201718f225c7d
Jon Jagger <jon@jaggersoft.com>
2015-04-24 19:01:10 +0100

    added tail rationale comments to test/test_runner.sh

--- test/test_wrapper.sh
+++ test/test_wrapper.sh
@@ -1,18 +1,18 @@
 
-# ExecuteAround test-runner which
+# ExecuteAround test-runner which...
 # 1. checks if tests alter the current git user!
 #     we don't want any confusion between the git repo created
 #     in a test (for an animal) and the main git repo of cyber-dojo!
 # 2. collect and processes coverage stats
 # 
-# Assumed to be run from /var/www/cyber-dojo/test/*/ folder
-# Either by running a single test 
-#   $ cd /var/www/cyber-dojo/test/lib
-#   $ ./git_test.rb
-# Or by running all the tests in one folder
+# Programmed for these three cases...
+# 1. running a single test (you must in the test's folder)
+#    $ cd /var/www/cyber-dojo/test/lib
+#    $ ./git_test.rb
+# 2. running all the tests in one folder (you must be in that folder)
 #   $ cd /var/www/cyber-dojo/test/lib
 #   $ ./run_all.sh
-# Or by running all the tests
+# 3. running all the tests in all the folders (you must be in test folder)
 #   $ cd /var/www/cyber-dojo/test
 #   $ ./run_all.sh
 
@@ -20,9 +20,15 @@ GIT_USER_NAME_BEFORE=`git config user.name`
 
 rm -rf ../../coverage/.resultset.json
  
+# Ok. Something odd here. Ruby (on my mac book) is *not* ignoring
+# the first shebang line in test/*_test.rb files.
+# So I'm stripping the first shebang line using tail. Yeuch!
+# 
+# filename/line-numbers reported in failing test will only be 
+# helpful for running single tests.
+
 cat $@ | tail -n +2 > temp.xx
 ruby temp.xx 2>&1 | tee log.tmp
-rm temp.xx
 
 cp -R ../../coverage/* .
 ruby ../print_coverage_percent.rb index.html $1 | tee -a log.tmp
