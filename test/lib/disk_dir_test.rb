cd80986929bba58eee21b25d4663062b2839062d
Jon Jagger <jon@jaggersoft.com>
2015-04-28 09:20:37 +0100

    test/lib/disk_test.rb refactoring

--- test/lib/disk_dir_test.rb
+++ test/lib/disk_dir_test.rb
@@ -6,59 +6,64 @@ class DiskTests < LibTestBase
 
   def setup
     super
-    set_runner_class_name('DummyTestRunner')
-    assert_equal 'Disk', get_disk_class_name    
-    `rm -rf #{dir}`
-    `mkdir -p #{dir}`
+    `rm -rf #{path}`
+    `mkdir -p #{path}`
   end
 
+  def disk
+    @disk ||= Disk.new
+  end
+  
+  def path
+    File.expand_path('../..', File.dirname(__FILE__)) + '/temp/'
+  end
+  
   def dir
-    root_path = File.expand_path('../..', File.dirname(__FILE__)) + '/'
-    root_path + 'temp/'
+    disk[path]
   end
   
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test '[dir] always has path ending in /' do
+  test 'disk[...].path always ends in /' do
     assert_equal "ABC/", disk['ABC'].path
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'exists?(dir) false when dir does not exist, true when it does' do
-    `rm -rf #{dir}`
-    assert !disk[dir].exists?
-    disk[dir].make
-    assert disk[dir].exists?
+  test 'disk[path].exists? false when path does not exist, true when it does' do
+    `rm -rf #{path}`
+    assert !dir.exists?
+    dir.make
+    assert dir.exists?
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'exists?(filename) false when file exists, true when it does' do
-    `rm -rf #{dir}`
+  test 'disk[path].exists?(filename) false when file exists, true when it does' do
+    `rm -rf #{path}`
     filename = 'hello.txt'
-    assert !disk[dir].exists?(filename)
-    disk[dir].write(filename, "content")
-    assert disk[dir].exists?(filename)
+    assert !dir.exists?(filename)
+    dir.write(filename, "content")
+    assert dir.exists?(filename)
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'read reads back what was written' do
+  test 'disk[path].read() reads back what was written' do
     expected = "content"
-    disk[dir].write('filename', expected)
-    actual = disk[dir].read('filename')
+    dir.write('filename', expected)
+    actual = dir.read('filename')
     assert_equal expected, actual
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'lock throws exception, does not execute block, ' +
+  test 'disk.lock throws exception, does not execute block, ' +
        'and result is nil, when path does not exist' do
     block_run = false
     exception_thrown = false
     begin
-      result = disk.lock('dir_does_not_exist') do
+      result = disk.lock('path_does_not_exist') do
         block_run = true
       end
     rescue
@@ -72,11 +77,11 @@ class DiskTests < LibTestBase
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'when lock is obtained block is executed ' +
+  test 'when dir.lock is obtained block is executed ' +
        'and result is result of block' do
     block_run = false
     begin
-      result = disk[dir].lock {
+      result = dir.lock {
         block_run = true; 'Hello'
       }
       assert block_run, 'block_run'
@@ -86,14 +91,14 @@ class DiskTests < LibTestBase
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'outer lock is blocking so inner lock blocks' do
+  test 'outer dir.lock is blocking so inner lock blocks' do
     outer_run = false
     inner_run = false
-    disk[dir].lock do
+    dir.lock do
       outer_run = true
 
       inner_thread = Thread.new {
-        disk[dir].lock do
+        dir.lock do
           inner_run = true
         end
       }
@@ -111,15 +116,15 @@ class DiskTests < LibTestBase
 
   test 'holding lock on parent dir does not prevent ' +
        'acquisition of lock on child dir' do
-    parent_dir = dir + 'parent' + disk.dir_separator
-    child_dir = parent_dir + 'child' + disk.dir_separator
-    `mkdir #{parent_dir}`
-    `mkdir #{child_dir}`
+    parent_path = path + 'parent' + disk.dir_separator
+    child_path = parent_path + 'child' + disk.dir_separator
+    `mkdir #{parent_path}`
+    `mkdir #{child_path}`
     parent_run = false
     child_run = false
-    disk[parent_dir].lock do
+    disk[parent_path].lock do
       parent_run = true
-      disk[child_dir].lock do
+      disk[child_path].lock do
         child_run = true
       end
     end
@@ -131,9 +136,9 @@ class DiskTests < LibTestBase
 
   test 'symlink' do
     expected = 'content'
-    disk[dir].write('filename', expected)
-    oldname = dir + 'filename'
-    newname = dir + 'linked'
+    dir.write('filename', expected)
+    oldname = path + 'filename'
+    newname = path + 'linked'
     disk.symlink(oldname, newname)
     assert !File.symlink?(oldname)
     assert File.symlink?(newname)
@@ -160,9 +165,8 @@ class DiskTests < LibTestBase
        'and the file in it' do
     pathed_filename = 'f1/f2/wibble.txt'
     content = 'Hello world'
-    disk[dir].write(pathed_filename, content)
-
-    full_pathed_filename = dir + pathed_filename
+    dir.write(pathed_filename, content)
+    full_pathed_filename = path + pathed_filename
     assert File.exists?(full_pathed_filename),
           "File.exists?(#{full_pathed_filename})"
     assert_equal content, IO.read(full_pathed_filename)
@@ -191,26 +195,26 @@ class DiskTests < LibTestBase
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'dir?(.) is true' do
-    assert disk.dir?(dir + '.')
+  test 'disk.dir?(.) is true' do
+    assert disk.dir?(path + '.')
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'dir?(..) is true' do
-    assert disk.dir?(dir + '..')
+  test 'disk.dir?(..) is true' do
+    assert disk.dir?(path + '..')
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'dir?(not-a-dir) is false' do
+  test 'disk.dir?(not-a-dir) is false' do
     assert !disk.dir?('blah-blah')
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'dir?(a-dir) is true' do
-    assert disk.dir?(dir)
+  test 'disk.dir?(a-dir) is true' do
+    assert disk.dir?(path)
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -225,22 +229,22 @@ class DiskTests < LibTestBase
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'Dir.each_dir does not give filenames' do
-    disk[dir].make
-    disk[dir].write('beta.txt', 'content')
-    disk[dir + 'alpha'].make
-    disk[dir + 'alpha'].write('a.txt', 'a')
-    assert_equal ['alpha'], disk[dir].each_dir.entries
+  test 'disk[path].each_dir does not give filenames' do
+    disk[path].make
+    disk[path].write('beta.txt', 'content')
+    disk[path + 'alpha'].make
+    disk[path + 'alpha'].write('a.txt', 'a')
+    assert_equal ['alpha'], disk[path].each_dir.entries
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'Dir.each_dir.select' do
-    disk[dir + 'alpha'].make
-    disk[dir + 'beta'].make
-    disk[dir + 'alpha'].write('a.txt', 'a')
-    disk[dir + 'beta'].write('b.txt', 'b')
-    matches = disk[dir].each_dir.select { |dir|
+  test 'disk[path].each_dir.select' do
+    disk[path + 'alpha'].make
+    disk[path + 'beta'].make
+    disk[path + 'alpha'].write('a.txt', 'a')
+    disk[path + 'beta'].write('b.txt', 'b')
+    matches = disk[path].each_dir.select { |dir|
       dir.start_with?('a')
     }
     assert_equal ['alpha'], matches.sort
@@ -248,29 +252,29 @@ class DiskTests < LibTestBase
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'Dir.each_file' do
-    disk[dir + 'a'].write('c.txt', 'content')
-    disk[dir + 'a'].write('d.txt', 'content')
-    assert_equal ['c.txt','d.txt'], disk[dir+'a'].each_file.entries.sort
+  test 'disk[path].each_file' do
+    disk[path + 'a'].write('c.txt', 'content')
+    disk[path + 'a'].write('d.txt', 'content')
+    assert_equal ['c.txt','d.txt'], disk[path+'a'].each_file.entries.sort
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'Dir.each_file does not give dirs' do
-    disk[dir].make
-    disk[dir].write('beta.txt', 'content')
-    disk[dir + 'alpha'].make
-    disk[dir + 'alpha'].write('a.txt', 'a')
-    assert_equal ['beta.txt'], disk[dir].each_file.entries
+  test 'disk[path].each_file does not give dirs' do
+    disk[path].make
+    disk[path].write('beta.txt', 'content')
+    disk[path + 'alpha'].make
+    disk[path + 'alpha'].write('a.txt', 'a')
+    assert_equal ['beta.txt'], disk[path].each_file.entries
   end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  test 'Dir.each_file.select' do
-    disk[dir + 'a'].write('b.cpp', 'content')
-    disk[dir + 'a'].write('c.txt', 'content')
-    disk[dir + 'a'].write('d.txt', 'content')
-    matches = disk[dir+'a'].each_file.select {|filename|
+  test 'disk[path].each_file.select' do
+    disk[path + 'a'].write('b.cpp', 'content')
+    disk[path + 'a'].write('c.txt', 'content')
+    disk[path + 'a'].write('d.txt', 'content')
+    matches = disk[path+'a'].each_file.select {|filename|
       filename.end_with?('.txt')
     }
     assert_equal ['c.txt','d.txt'], matches.sort
@@ -279,8 +283,8 @@ class DiskTests < LibTestBase
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   def check_save_file(filename, content, expected_content, executable = false)
-    disk[dir].write(filename, content)
-    pathed_filename = dir + filename
+    dir.write(filename, content)
+    pathed_filename = path + filename
     assert File.exists?(pathed_filename),
           "File.exists?(#{pathed_filename})"
     assert_equal expected_content, IO.read(pathed_filename)
