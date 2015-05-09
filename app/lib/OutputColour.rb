1d28959f3454207cce99ddb63748102d1ae83409
Jon Jagger <jon@jaggersoft.com>
2015-05-09 08:12:15 +0100

    fixed ruby_rspec red/amber/green colour.of(output) error

--- app/lib/OutputColour.rb
+++ app/lib/OutputColour.rb
@@ -105,6 +105,8 @@ module OutputColour
   end
 
   def self.parse_ruby_rspec(output)
+    return :green if /(\d*) example(s?), 0 failures/.match(output)
+    return :red   if /(\d*) example(s?), (\d*) failure(s?)/.match(output)
     return :red   if /(\d*) scenario(s?) \((\d*) failed/.match(output)
     return :green if /(\d*) scenario(s?) \((\d*) passed/.match(output)
     return :amber
