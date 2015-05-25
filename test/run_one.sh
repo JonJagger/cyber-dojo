1e838eb96a5bc8d8e31ee16ae346badbcd092e16
Jon Jagger <jon@jaggersoft.com>
2015-05-25 09:09:13 +0100

    refactoring - delete dead file

--- test/run_one.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo
-echo "======$1======"
-
-rm -rf ../../coverage/.resultset.json
-../testrb . 2>&1 | tee log.tmp
-cp -R ../../coverage/* .
-ruby ../print_coverage_percent.rb index.html $1 | tee -a log.tmp
