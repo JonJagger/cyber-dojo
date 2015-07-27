bc1f025ba3a670a8dc623e6a1e8b2f272ce5bf72
Jon Jagger <jon@jaggersoft.com>
2015-07-27 05:52:01 +0100

    data-container notes update

--- admin_scripts/docker_crib.sh
+++ admin_scripts/docker_crib.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo
+echo 'to set shell env variables for boot2docker'
+echo '  eval "$(boot2docker shellinit)"'
+echo
 echo 'to build a container using a dockerfile in the current directory'
 echo ' $ docker build -t  container .'
 echo
 echo 'to run a container interactively'
 echo ' $ docker run -i -t container /bin/bash'
 echo
+echo 'to build a data-conly container'
+echo ' $ docker run -v /AB/CDE12345 --name="cd_ABCDE12345" git-base true'
 echo
 echo 'to stop and restart the docker daemon'
 echo ' $service docker stop'
--- admin_scripts/docker_ps_rm.sh
+++ admin_scripts/docker_ps_rm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Don't use this if there are data-only containers.
+
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
