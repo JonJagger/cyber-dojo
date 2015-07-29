453e28098fbaa70aea793d60b76a358196f027f8
Jon Jagger <jon@jaggersoft.com>
2015-07-29 08:41:46 +0100

    notes update

--- admin_scripts/docker_crib.sh
+++ admin_scripts/docker_crib.sh
@@ -5,7 +5,7 @@ echo 'to set shell env variables for boot2docker'
 echo '  eval "$(boot2docker shellinit)"'
 echo
 echo 'to build a container using a dockerfile in the current directory'
-echo ' $ docker build -t  container .'
+echo ' $ docker build -t container .'
 echo
 echo 'to run a container interactively'
 echo ' $ docker run -i -t container /bin/bash'
