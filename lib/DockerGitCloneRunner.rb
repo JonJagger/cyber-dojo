f48f5e6b02a2a89357c106984edef28c99740e83
Jon Jagger <jon@jaggersoft.com>
2015-08-11 22:08:48 +0200

    DockerGitCloneRunner - working with --net=host which is insecure

--- lib/DockerGitCloneRunner.rb
+++ lib/DockerGitCloneRunner.rb
@@ -14,19 +14,36 @@ class DockerGitCloneRunner
   end
 
   def runnable?(language)    
+    # - - - - - - -
+    # sym-linked support-files cannot be supported because
+    # a docker swarm solution cannot volume mount.
+    # - - - - - - -
+    # Approval style tests are disabled because their
+    # post-run .txt file retrieval is not trivial on
+    # docker swarm solution.
+    # - - - - - - -
     language.support_filenames == [] &&
       !language.display_name.end_with?('Approval')
   end
 
   def git_server
-    "192.168.59.103"
+    # Assumes:
+    # 1. user called git that cyber-dojo user can ssh into.
+    # 2. git-daemon running to publically serve repos
+    #    with a --base-path=/opt/git
+    # 3. port 9418 is open
+    '192.168.59.103'
   end
 
+  def opt_git_kata_path(kata)
+    '/opt/git' + kata_path(kata)
+  end
+  
   def kata_path(kata)
     id = kata.id.to_s
     outer = id[0..1]
     inner = id[2..-1]
-    "/opt/git/#{outer}/#{inner}"
+    "/#{outer}/#{inner}"
   end
   
   def started(avatar)
@@ -37,20 +54,20 @@ class DockerGitCloneRunner
       "git clone --bare #{avatar.name} #{avatar.name}.git",
       # scp -r says it makes directories as needed but it doesn't seem to
       # which is why I'm preceding the scp with the mkdir -p
-      "sudo -u cyber-dojo ssh git@#{git_server} 'mkdir -p #{kata_path(kata)}'",
-      "sudo -u cyber-dojo scp -r #{avatar.name}.git git@#{git_server}:#{kata_path(kata)}",
+      "sudo -u cyber-dojo ssh git@#{git_server} 'mkdir -p #{opt_git_kata_path(kata)}'",
+      "sudo -u cyber-dojo scp -r #{avatar.name}.git git@#{git_server}:#{opt_git_kata_path(kata)}",
       # allow git-daemon to serve it
-      "sudo -u cyber-dojo ssh git@#{git_server} 'touch #{kata_path(kata)}/#{avatar.name}.git/git-daemon-export-ok'",
+      "sudo -u cyber-dojo ssh git@#{git_server} 'touch #{opt_git_kata_path(kata)}/#{avatar.name}.git/git-daemon-export-ok'",
       "rm -rf #{avatar.name}.git",
       "cd #{avatar.path}",
-      "git remote add master git@#{git_server}:#{kata_path(kata)}/#{avatar.name}.git",
+      "git remote add master git@#{git_server}:#{opt_git_kata_path(kata)}/#{avatar.name}.git",
       "sudo -u cyber-dojo git push --set-upstream master master"
     ].join(';')
     o,es = bash(cmds)
   end
   
   def pre_test(avatar)
-    # if no visible files have changed this will be a no-op
+    # if no visible files have changed this will be a safe no-op
     cmds = [
       "cd #{avatar.path}",
       "sudo -u cyber-dojo git commit -am 'pre-test-push' --quiet"
@@ -60,7 +77,11 @@ class DockerGitCloneRunner
   end
   
   def post_commit_tag(avatar)
-    o,es = bash('sudo -u cyber-dojo git push master')
+    cmds = [
+      "cd #{avatar.path}",
+      'sudo -u cyber-dojo git push master'
+    ].join(';')
+    o,es = bash(cmds)
   end
 
   def run(sandbox, command, max_seconds)
@@ -69,21 +90,18 @@ class DockerGitCloneRunner
     cidfile = avatar.path + 'cidfile.txt'
     language = kata.language
 
-    # Unless the docker container has ssh credentials
-    # this will require entering the git password.
-    # Better option I think is to set up a git daemon on 
-    # the git server.
+    # Assumes git daemon on the git server.    
     cmds = [
-      "git clone git:#{git_server}/#{kata_path(kata)}/#{avatar.name}.git",
-      "cd #{avatar.name}/sandbox",
-      "#{command}"
+      "git clone git://#{git_server}#{kata_path(kata)}/#{avatar.name}.git /tmp/#{avatar.name} 2>&1 > /dev/null",
+      "cd /tmp/#{avatar.name}/sandbox && #{command}"
     ].join(';')
     
-    # network defaults to bridged. Would prefer it
-    # if I could restrict it soley to the git server.
+    # Using --net=host just to get something working. This is insecure.
+    # Would prefer to restrict it to just accessing the git server.
     docker_cmd = 
       "docker run" +
       " -u www-data" +
+      " --net=host"
       " --cidfile=#{quoted(cidfile)}" +
       " #{language.image_name}" +
       " /bin/bash -c" +
