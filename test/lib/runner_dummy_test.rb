53ab7441c02ae51998a0bff6a351178ae7948696
Matt <cadnce@users.noreply.github.com>
2015-08-13 00:30:31 +0100

    Updated docs and test to be inline with usage for 'Runner's

--- test/lib/runner_dummy_test.rb
+++ test/lib/runner_dummy_test.rb
@@ -20,7 +20,7 @@ class RunnerDummyTests < LibTestBase
   test 'run tells you how to use HostTestRunner' do
     output = @runner.run(nil,nil,nil)
     assert output.include?('to use DockerVolumeMountRunner')
-    assert output.include?('$ export CYBERDOJO_RUNNER_CLASS_NAME=DockerVolumeMountRunner')
+    assert output.include?('$ export CYBER_DOJO_RUNNER_CLASS_NAME=DockerVolumeMountRunner')
   end
 
 end
