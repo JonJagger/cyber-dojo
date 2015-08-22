05dafd36caf9fcbac09b007e0fb328d2a28d2434
Byran Wills-Heath <byran@adgico.co.uk>
2015-08-22 16:10:59 +0000

    Changed Groovy JUnit image to be based on the new cyberdojofoundation image

--- languages/Groovy/JUnit/cyber-dojo.sh
+++ languages/Groovy/JUnit/cyber-dojo.sh
@@ -1,6 +1,4 @@
 rm -f *.class
-GROOVY_HOME=/groovy-2.2.0
-PATH=$GROOVY_HOME/bin:$PATH
 groovyc *.groovy
 if [ $? -eq 0 ]; then
   java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') org.junit.runner.JUnitCore `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
