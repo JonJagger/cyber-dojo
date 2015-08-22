624216d965b6f36be5923f1eb0bdb56f078118f8
Byran Wills-Heath <byran@adgico.co.uk>
2015-08-22 16:31:32 +0000

    Changed Groovy Spock image to be based on the new cyberdojofoundation image
    This image currently isn't working

--- languages/Groovy/Spock/cyber-dojo.sh
+++ languages/Groovy/Spock/cyber-dojo.sh
@@ -1,6 +1,4 @@
 rm -f *.class
-GROOVY_HOME=/groovy-2.2.0
-PATH=$GROOVY_HOME/bin:$PATH
 groovyc -cp /groovy/spock-core-0.7-groovy-2.0.jar *.groovy
 if [ $? -eq 0 ]; then
   java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') org.junit.runner.JUnitCore `ls -1 *Spec*.class | sed 's/\(.*\)\..*/\1/'`
