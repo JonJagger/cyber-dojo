444d7e53590a4c0de8bc4d8b770ab4d1f3c6f495
Byran Wills-Heath <byran@adgico.co.uk>
2015-08-23 14:13:00 +0000

    Fixed Groovy Spock cyber-dojo.sh file that was missed in a previous commit

--- languages/Groovy/Spock/cyber-dojo.sh
+++ languages/Groovy/Spock/cyber-dojo.sh
@@ -1,5 +1,5 @@
 rm -f *.class
-groovyc -cp /groovy/spock-core-0.7-groovy-2.0.jar *.groovy
+groovyc -cp /groovy/spock-core-1.0-groovy-2.4.jar *.groovy
 if [ $? -eq 0 ]; then
   java -cp .:$(ls /groovy/*.jar | xargs | sed -e 's/ /:/g') org.junit.runner.JUnitCore `ls -1 *Spec*.class | sed 's/\(.*\)\..*/\1/'`
 fi
