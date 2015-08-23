8899d70ef00c30d0c7a31ca89b99e392f7c5070f
Byran Wills-Heath <byran@adgico.co.uk>
2015-08-23 01:17:52 +0000

    Modified build scripts to allow generating of shell scrips

--- admin_scripts/build_scripts/directory_build_image.rb
+++ admin_scripts/build_scripts/directory_build_image.rb
@@ -1,11 +1,15 @@
+require 'shellwords'
 
 def build_images(path)
   Dir.glob(path) do |file|
-    p File.dirname(file)
     Dir.chdir(File.dirname(file))
     exec = File.basename(file)
-#    p exec
-    `./#{exec}`
+
+#    p File.dirname(file)
+#    `./#{exec}`
+
+    print "cd " + Shellwords.shellescape(File.dirname(file)) + "\n"
+    print "./" + Shellwords.shellescape(exec) + "\n"
   end
 end
 
