aca754315e55d352dd18d150b16622390a1caaca
Jon Jagger <jon@jaggersoft.com>
2015-08-27 13:37:47 +0100

    refactoring_dojos/load.rb comment tidy up

--- admin_scripts/refactoring_dojos/load.rb
+++ admin_scripts/refactoring_dojos/load.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# You must retain the file-permissions (www-data:www-data)
+# of the refactoring dojos when unzipping them.
+# You do this with tar's p option.
+
 # has to be run from cyber-dojo dir
 # cd /var/www/cyber-dojo
 # ./admin_scripts/refactoring_dojos/load.rb
--- admin_scripts/refactoring_dojos/readme.txt
+++
@@ -1,6 +0,0 @@
-
-You must retain the file-permissions of
-the refactoring dojos when unzipping them.
-You do this with tar's p option.
-# cd /var/www/cyber-dojo
-# tar -xvpf refactoring_dojos.tar.gz
