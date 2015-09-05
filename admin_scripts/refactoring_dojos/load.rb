2d62e7b6f79810989c4b4cbac77ac5e6fc9c6f83
Jon Jagger <jon@jaggersoft.com>
2015-09-05 17:44:42 +0100

    correct refactoring_dojos load.rb script

--- admin_scripts/refactoring_dojos/load.rb
+++ admin_scripts/refactoring_dojos/load.rb
@@ -4,9 +4,4 @@
 # of the refactoring dojos when unzipping them.
 # You do this with tar's p option.
 
-# has to be run from cyber-dojo dir
-# cd /var/www/cyber-dojo
-# ./admin_scripts/refactoring_dojos/load.rb
-
-cd /var/www/cyber-dojo
-`tar -xvpf /var/www/cyber-dojo/admin_scripts/refactoring_dojos/refactoring_dojos.tgz`
+`tar -C /var/www/cyber-dojo -xvpf refactoring_dojos.tgz`
