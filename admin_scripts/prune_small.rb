9166f6708651c2dabfc385e96c668b5828793900
Jon Jagger <jon@jaggersoft.com>
2015-09-17 11:44:49 +0100

    activated prune_small script

--- admin_scripts/prune_small.rb
+++ admin_scripts/prune_small.rb
@@ -11,10 +11,20 @@
 #   $./prune_small.rb 1 7 true
 # to actually do the pruning
 
+# TODO: this should also look at the size of the diff, but as it is
+#       its useful for deleting dojos with *no* traffic-lights
+#       $./prune_small.rb 0 7 false
+#       $./prune_small.rb 0 7 true
+
 require_relative 'lib_domain'
 
+def max_traffic_light_count; ARGV[0].to_i; end
+
+def min_days_old; ARGV[1].to_i; end
+
+def do_delete; (ARGV[2] || "false"); end
+
 def prune_stats
-  n,max = 0,50  
   stats = { }
   dojo.katas.select{|kata|
     !refactoring_ids.include?(kata.id.to_s)
@@ -26,14 +36,13 @@ def prune_stats
       stats[lights_count] ||= [ ]
       stats[lights_count] << [kata.id,days_old]
     rescue
-      puts "Exception from #{kata.id.to_s}"      
+      puts "prune_small... Exception from #{kata.id.to_s}"      
     end
-    n += 1; break if n >= max
   end
   stats
 end
 
-def prune(do_delete)
+def prune
   tally_yes,tally_no = 0,0
   prune_stats.sort.each do |traffic_light_count,entries|
     entries.each do |id,days_old|
@@ -41,8 +50,7 @@ def prune(do_delete)
         tally_yes += 1
         rm_cmd = "rm -rf " + dojo.katas[id].path
         if do_delete == "true"
-          #system(rm_cmd)
-          print "#{rm_cmd}\n"
+          system(rm_cmd)
           print "."
         else
           print 'Will rm ' + id[0..1]+'/'+id[2..-1] +
@@ -56,6 +64,8 @@ def prune(do_delete)
     end
   end
   print "\n"
+  cf = (max_traffic_light_count === 0) ? '==' : '<='
+  puts "Looking for: (lights #{cf} #{max_traffic_light_count}) && (days_old >= #{min_days_old})"
   if do_delete == 'false'
     print "#{tally_yes} katas will be deleted\n"
     print "#{tally_no} katas will not be deleted\n"
@@ -67,12 +77,6 @@ end
 
 #- - - - - - - - - - - - - - - - - - - - - -
 
-max_traffic_light_count = ARGV[0].to_i
-min_days_old = ARGV[1].to_i
-do_delete = (ARGV[2] || "false")
-
-puts "Looking for (lights <= #{max_traffic_light_count}) && (days_old >= #{min_days_old})"
-
-prune(do_delete) { |traffic_light_count,days_old|
+prune { |traffic_light_count,days_old|
   traffic_light_count <= max_traffic_light_count and days_old >= min_days_old
 }
