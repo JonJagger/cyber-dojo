bbbf74ceee885287403fa22aa04585771133e95a
Klaas Reineke <klaas.reineke@gmail.com>
2015-09-17 13:33:54 +0100

    Created new javascript container based on Debian wheezy with 0.12.7 and jasmine 2.3

---
+++ languages/Javascript0.12.7/build-docker-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t cyberdojo/javascript-0.12.7  .
---
+++ languages/Javascript0.12.7/jasmine2.3/hiker-spec.js
@@ -0,0 +1,8 @@
+
+require('./hiker.js');
+
+describe("answer", function() {
+  it("to life the universe and everything", function() {
+    expect(answer()).toEqual(42);
+  });
+});
