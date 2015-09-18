4901946d294446f521dba045eefb7cf3439ed5b1
Jon Jagger <jon@jaggersoft.com>
2015-09-18 22:20:15 +0100

    minor test refactoring

--- test/lib/background_process_test.rb
+++ test/lib/background_process_test.rb
@@ -4,24 +4,23 @@ require_relative 'LibTestBase'
 
 class CurlOneSelfTests < LibTestBase
 
-  test 'D54BA5',
-  'A process started in the background does not block' do
-    background_process = BackgroundProcess.new
-
-    start_time = Time.now
-    background_process.start("sleep 10")
-    end_time = Time.now
-
-    run_time = (end_time - start_time) * 1000 # total run time in milliseconds
-
-    assert run_time < 1000, "Starting the background process took longer than 1 second to complete"
+  def setup
+    super
+    @background_process = BackgroundProcess.new  
   end
 
-  test '4e78b26f',
-  'Starting a background process that does not exist does not crash the host process' do
-    background_process = BackgroundProcess.new
+  test 'D54BA5',
+  'A process started in the background does not block' do
+    start_time = Time.now
+    @background_process.start('sleep 10')
+    end_time = Time.now
+    run_time = (end_time - start_time) * 1000 # total run time in milliseconds
+    assert run_time < 1000, 'Starting the background process took longer than 1 second to complete'
+  end
 
-    background_process.start("this-does-not-exist")
+  test '4E78B2',
+  'Starting a background process that does not exist does not leak an exception' do
+    @background_process.start('this-does-not-exist')
   end
 
 end
