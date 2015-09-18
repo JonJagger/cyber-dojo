cbe621bcc3e9fc8be19027caa7e359f050ef6184
Jon Jagger <jon@jaggersoft.com>
2015-09-18 22:35:43 +0100

    guard against invalid byte sequence when printing test coverage percent

--- test/print_coverage_percent.rb
+++ test/print_coverage_percent.rb
@@ -4,6 +4,9 @@ filter = ARGV[1]
 flat = filter.sub('/','')
 
 html = IO.popen("cat #{filename}").read
+# guard against invalid byte sequence
+html = html.encode('UTF-16', 'UTF-8', :invalid => :replace, :replace => '')
+html = html.encode('UTF-8', 'UTF-16')
 
 pattern = /<div class=\"file_list_container\" id=\"#{flat}\">
 \s*<h2>\s*<span class=\"group_name\">#{filter}<\/span>
