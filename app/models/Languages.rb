184cc44f98afa4d938804518d5634d7a816d43bf
Klaas Reineke <klaas.reineke@gmail.com>
2015-09-23 14:50:10 +0100

    Added javascript with qunit framework and mock support with sinon: fixed name in Languages -> removed chai

--- app/models/Languages.rb
+++ app/models/Languages.rb
@@ -1,17 +1,17 @@
 # See comments at end of file
 
 class Languages
-  
+
   include ExternalParentChain
   include Enumerable
-  
+
   def initialize(dojo,path)
     @parent,@path = dojo,path
     @path += '/' if !@path.end_with? '/'
   end
-  
+
   attr_reader :path
-  
+
   def each
     return enum_for(:each) unless block_given?
     languages.each { |language| yield language }
@@ -30,27 +30,27 @@ class Languages
     end
     was_name.split('-')
   end
-  
+
   def refresh_cache
     cache = { }
     dir.each_dir do |dir_name|
       disk[path + dir_name].each_dir do |test_dir_name|
         language = make_language(dir_name,test_dir_name)
         if language.exists?
-          cache[language.display_name] = { 
-            :dir_name => language.dir_name, 
+          cache[language.display_name] = {
+            :dir_name => language.dir_name,
             :test_dir_name => language.test_dir_name
           }
         end
-      end      
+      end
     end
     dir.write_json(cache_filename,cache)
   end
-  
+
 private
 
-  def new_name(name)    
-    # maps from a language&test display_name into a 
+  def new_name(name)
+    # maps from a language&test display_name into a
     # Language-Test name corresponding to its Language/Test/ *folder*
     # See comment at bottom of file.
     renames = {
@@ -64,7 +64,7 @@ private
       'Erlang'       => 'Erlang-eunit',
       'Go'           => 'Go-testing',
       'Haskell'      => 'Haskell-hunit',
-      'Java'         => 'Java-JUnit',      
+      'Java'         => 'Java-JUnit',
       'Javascript'   => 'Javascript-assert',
       'Perl'         => 'Perl-TestSimple',
       'PHP'          => 'PHP-PHPUnit',
@@ -94,12 +94,12 @@ private
 
       # replaced
       'R-stopifnot' => 'R-RUnit',
-      
+
       # rename to distinguish from [C (clang)]
       'C-assert'   => 'C (gcc)-assert',
       'C-Unity'    => 'C (gcc)-Unity',
       'C-CppUTest' => 'C (gcc)-CppUTest',
-      
+
       # rename to distinguish from [C++ (clang++)]
       'C++-assert'     => 'C++ (g++)-assert',
       'C++-Boost.Test' => 'C++ (g++)-Boost.Test',
@@ -117,16 +117,17 @@ private
       'C++ (g++)-GoogleTest' => 'g++4.8.4-GoogleTest',
       'C++ (g++)-Igloo'      => 'g++4.8.4-Igloo',
       'C++ (g++)-GoogleMock' => 'g++4.9-GoogleMock',
-                  
+
       # multiple language versions (1.9.3 & 2.1.3)
       'Ruby-Approval'      => 'Ruby1.9.3-Approval',
       'Ruby-Cucumber'      => 'Ruby1.9.3-Cucumber',
       'Ruby-TestUnit'      => 'Ruby1.9.3-TestUnit',
       'Ruby-Rspec'         => 'Ruby1.9.3-Rspec',
       'Ruby-MiniTest'      => 'Ruby2.1.3-MiniTest',
-      
+
       # multiple language versions
-      'Javascript-jasmine' => 'Javascript0.12.7-jasmine2.3'
+      'Javascript-jasmine' => 'Javascript0.12.7-jasmine2.3',
+      'Javascript-qunit+sinon' => 'Javascript0.12.7-qunit_sinon'
     }
     (renames[name] || name).split('-')
   end
@@ -135,7 +136,7 @@ private
     @languages ||= read_cache
   end
 
-  def read_cache    
+  def read_cache
     cache = [ ]
     JSON.parse(dir.read(cache_filename)).each do |display_name,language|
       cache << make_language(language['dir_name'],language['test_dir_name'],display_name)
@@ -150,18 +151,18 @@ private
   def cache_filename
     'cache.json'
   end
-  
+
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 # Refactoring [](name) to ...
 #
 #    dir_name,test_dir_name = renamed(name)
-#    languages.find {|language| 
-#      language.dir_name == dir_name && 
+#    languages.find {|language|
+#      language.dir_name == dir_name &&
 #      language.test_dir_name == test_dir_name
-#    }    
-# 
+#    }
+#
 # would be nice since it would make use of the cache
 # but breaks lots of tests because they use DirFake
 # without a languages cache.
@@ -181,19 +182,19 @@ end
 # an existing language and, when doing this, to take
 # advantage of upgrading the language to a newer version.
 #
-# For example 
+# For example
 #    Ruby1.9.3/Approval
 #    Ruby1.9.3/Cucumber
 #    Ruby1.9.3/Rspec
 #    Ruby1.9.3/TestUnit
 # existed when I wanted to add MiniTest as a new Ruby test-framework.
-# By then the latest Ruby version was 2.1.3 
+# By then the latest Ruby version was 2.1.3
 #    Ruby2.1.3/MiniTest
 #
 # I didn't want to have to upgrade the existing Ruby1.9.3 test-frameworks
 # to Ruby2.1.3. But... on the create page I wanted all the different
 # Ruby test-frameworks (from two different versions of Ruby) to appear
-# under the *same* language name in the language? column. 
+# under the *same* language name in the language? column.
 # This is why a language/test's  manifest.json file has a display_name entry.
 # It is the display_name that governs the language/test's names as they appear
 # on the create page. Not the folder names. Not the docker container image_name.
@@ -202,13 +203,13 @@ end
 # or need to upgrade their servers to use the latest docker containers for
 # the latest language/test even if it exists.
 # This means the cyberdojo docker index needs to keep old versions of
-# language/test docker containers even when newer ones exist. 
+# language/test docker containers even when newer ones exist.
 # Viz, a docker container name needs to have a version number in it.
 
 # Further, if you upgrade a language/test to a newer version
-# and delete the old version of the docker container from your server, 
+# and delete the old version of the docker container from your server,
 # you still want to be able to fork from a kata done in the old version.
-# And when you do such a fork you want the new kata to use the new version. 
+# And when you do such a fork you want the new kata to use the new version.
 # So what is stored in the kata's manifest is *not* the docker container's
 # image_name but the display_name.
 #
@@ -216,10 +217,10 @@ end
 # into a name in the form "Language-Test" which corresponds to its
 # Language/Test folder which in turn contains its manifest.json file.
 #
-# It's a bit fiddly because historically the language-&-test 
-# were *not* separated into distinct nested folders. 
-# You can still see the remnants of this in the language/test/manifest.json's 
-# display_name which contains the display name of the *both* the language 
+# It's a bit fiddly because historically the language-&-test
+# were *not* separated into distinct nested folders.
+# You can still see the remnants of this in the language/test/manifest.json's
+# display_name which contains the display name of the *both* the language
 # *and* the test-framework (separated by a comma).
 #
 # - - - - - - - - - - - - - - - - - - - - - - - -
