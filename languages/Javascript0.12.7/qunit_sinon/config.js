bf11abec470e6342330f68a3b31f2c80e992ae29
Klaas Reineke <klaas.reineke@gmail.com>
2015-09-23 14:45:28 +0100

    Added javascript with qunit framework and mock support with sinon: fixed config -> removed some js code that was not used and chai related

--- languages/Javascript0.12.7/qunit_sinon/config.js
+++ languages/Javascript0.12.7/qunit_sinon/config.js
@@ -5,18 +5,3 @@
 // You should require your dependencies in your '*-test.js' files
 // as you are used to, i.e.
 // require('./hiker.js')
-
-var chai = require('chai');
-var sinonChai = require('sinon-chai');
-
-global.sinon = require('sinon');
-
-// although you will probably only choose one style
-// assert, expect, and should are all here
-
-global.assert = chai.assert;
-global.expect = chai.expect;
-global.should = chai.should(); // Note that should has to be executed
-
-chai.config.includeStack = true;
-chai.use(sinonChai);
