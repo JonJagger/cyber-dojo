696ffd0d798a33b4fd1dce7a138fc1dd2d24b021
Jon Jagger <jon@jaggersoft.com>
2015-10-24 10:37:07 +0300

    make-ram-disk script needed for test now also sets required environment variable

--- admin_scripts/make_ram_disk.sh
+++ admin_scripts/make_ram_disk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ramDisk=$1
+ramDisk=/var/www/cyber-dojo/tmp/ram-disk
 
 umount $ramDisk
 rm -rf $ramDisk
@@ -9,6 +9,4 @@ mount -t tmpfs -o size=1m tmpfs $ramDisk
 chmod -R 777 $ramDisk
 chown -R www-data:www-data $ramDisk
 
-# eg
-# $ sudo ./make_ram_disk.sh /var/www/cyber-dojo/tmp/ram-disk
-# $ export CYBER_DOJO_TMP_ROOT=/var/www/cyber-dojo/tmp/ram-disk
+. export CYBER_DOJO_TMP_ROOT=$ramDisk
