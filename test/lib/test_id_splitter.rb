657d94b465de6427df63772e088a6bb8b7d3fbdb
Jon Jagger <jon@jaggersoft.com>
2015-10-29 22:02:58 +0000

    minor test refactor

--- test/lib/test_id_splitter.rb
+++ test/lib/test_id_splitter.rb
@@ -6,23 +6,15 @@ class IdSplitterTests < LibTestBase
 
   include IdSplitter
 
-  test '816297',
-  'outer(id) is first two chars of id' do
-    assert_equal '23', outer('23111966')
-  end
-
   test 'C45B5C',
-  'inner(id) if first two chars of id in uppercase' do
+  'outer(id) if first two chars of id in uppercase' do
+    assert_equal '23', outer('23111966')
     assert_equal 'AB', outer('ab111966')
-  end
-
-  test '579DDC',
-  'inner(id) is all but first two chars of id' do
-    assert_equal '111966', inner('23111966')
   end
 
   test '03AE98',
   'inner(id) is all but first two chars of id in uppercase' do
+    assert_equal '111966', inner('23111966')
     assert_equal 'AB1966', inner('23ab1966')
   end
 
