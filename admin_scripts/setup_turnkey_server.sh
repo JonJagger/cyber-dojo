363a16902c6ac759ff01143d47e25d4bc77c9b35
Jon Jagger <jon@jaggersoft.com>
2015-10-30 10:59:26 +0000

    ensure docker group exists in turnkey-setup script

--- admin_scripts/setup_turnkey_server.sh
+++ admin_scripts/setup_turnkey_server.sh
@@ -42,7 +42,6 @@ apt-get install -y docker-engine
 curl -L https://github.com/docker/machine/releases/download/v0.4.0/docker-machine_linux-amd64 > /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
+groupadd docker
 gpasswd -a www-data docker
 service docker restart
-
-
