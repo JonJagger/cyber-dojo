c51b4c47c0e2c2b6e4cdf5413445aa6da272b9f6
Jon Jagger <jon@jaggersoft.com>
2015-11-05 09:17:59 +0000

    show display-names when listing image names

--- admin_scripts/show_docker_repo_names.rb
+++ admin_scripts/show_docker_repo_names.rb
@@ -4,10 +4,10 @@ require 'json'
 
 cyber_dojo_root = File.expand_path('..', File.dirname(__FILE__))
 
-image_names = [ ]
+image_names = []
 Dir.glob("#{cyber_dojo_root}/languages/*/*/manifest.json") do |file|
   manifest = JSON.parse(IO.read(file))
-  image_names << manifest['image_name']
+  image_names << (manifest['image_name'] + ' == ' + manifest['display_name'])
 end
 
 puts image_names.sort
