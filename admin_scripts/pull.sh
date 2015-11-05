60df574218e32dd563707563320ccb9b8b42f86b
Jon Jagger <jon@jaggersoft.com>
2015-11-05 09:01:31 +0000

    create script to update all caches

--- admin_scripts/pull.sh
+++ admin_scripts/pull.sh
@@ -58,29 +58,7 @@ rm $cyberDojoHome/Gemfile.lock
 bundle install
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
-# refresh caches
-
-echo "refreshing languages cache"
-$cyberDojoHome/languages/refresh_cache.rb
-
-echo "refreshing exercises cache/"
-$cyberDojoHome/exercises/refresh_cache.rb
-
-if docker --version > /dev/null 2>&1; then
-  echo "docker is installed"
-  echo "refreshing DockerRunner cache"
-  $cyberDojoHome/lib/refresh_docker_runner_cache.rb
-else
-  echo "docker is NOT installed!!"
-fi
-
-if docker-machine --version > /dev/null 2>&1; then
-  echo "docker-machine is installed"
-  echo "refreshing DockerMachineRunner cache"
-  $cyberDojoHome/lib/refresh_docker_machine_runner_cache.rb
-else
-  echo "docker-machine is NOT installed"
-fi
+$cyberDojoHome/admin_scripts/refresh_all_caches.rb
 
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 echo "restarting apache"
