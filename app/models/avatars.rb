b4daa225c77368810adb0f5373fadee62a84103a
Jon Jagger <jon@jaggersoft.com>
2015-11-05 10:07:03 +0000

    add new avatars now 64

--- app/models/avatars.rb
+++ app/models/avatars.rb
@@ -2,8 +2,14 @@
 class Avatars
 
   def self.names
-    %w(alligator buffalo cheetah deer  elephant frog  gorilla hippo
-       koala     lion    moose   panda raccoon  snake wolf    zebra
+    %w(alligator antelope   bat     bear     bee      beetle       buffalo   butterfly
+       cheetah   crab       deer    dolphin  eagle    elephant     flamingo  fox
+       frog      gopher     gorilla heron    hippo    hummingbird  hyena     jellyfish
+       kangaroo  kingfisher koala   leopard  lion     lizard       lobster   moose
+       mouse     ostrich    owl     panda    parrot   peacock      penguin   porcupine
+       puffin    rabbit     raccoon ray      rhino    salmon       seal      shark
+       skunk     snake      spider  squid    squirrel starfish     swan      tiger
+       toucan    tuna       turtle  vulture  walrus   whale        wolf       zebra
     )
   end
 
