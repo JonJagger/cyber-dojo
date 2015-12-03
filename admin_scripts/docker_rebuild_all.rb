5b1355416a16e9dbf5872e33ef4d3df23fa346c9
Jon Jagger <jon@jaggersoft.com>
2015-12-04 00:04:04 +0100

    fix admin_scripts/docker_rebuild_all.rb so it ignores _docker_context folders

--- admin_scripts/docker_rebuild_all.rb
+++ admin_scripts/docker_rebuild_all.rb
@@ -5,7 +5,7 @@ def cyber_dojo_root
 end
 
 def build_docker_container_in(dir)
-   "cd '#{dir}'; ./build-docker-container.sh"
+  "cd '#{dir}'; ./build-docker-container.sh"
 end
 
 def print_and_run(command)
@@ -18,6 +18,7 @@ print_and_run build_docker_container_in("#{cyber_dojo_root}/languages/build-esse
 Dir.glob("#{cyber_dojo_root}/languages/*/").sort.each do |language_dir|
   print_and_run(build_docker_container_in(language_dir))
   Dir.glob("#{language_dir}*/").sort.each do |test_dir|
+    next if test_dir.end_with? '/_docker_context/'
     print_and_run(build_docker_container_in(test_dir))
   end
 end
