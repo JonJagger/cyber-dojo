5bf7838acd5161fd3662b6259ffcc0c084fb1a2f
Jon Jagger <jon@jaggersoft.com>
2015-12-08 08:55:02 +0000

    more host-disk-avatar-starter tweaks

--- lib/host_disk_avatar_starter.rb
+++ lib/host_disk_avatar_starter.rb
@@ -6,7 +6,7 @@ class HostDiskAvatarStarter
   end
 
   def start_avatar(avatar_names = Avatars.names.shuffle)
-    # Relies on mkdir being atomic on Unix non NFS file system
+    # Relies on mkdir being atomic on a POSIX non NFS file system
     avatar_names.each do |avatar_name|
       _, exit_status = bash.exec("mkdir #{avtar_name}")
       return avatar_name if exit_status == success
@@ -15,14 +15,13 @@ class HostDiskAvatarStarter
   end
 
   def started_avatars
-    dirs = bash_exec('ls -d */').split # [ 'hippo/', 'lion/' ]
-    dirs.map { |dir| dir[0..-2] }.select { |dir| Avatars.name.include? dir }
+    bash_exec('ls -d */').split.map(&:chop) & Avatars.name
   end
 
   private
 
   def bash_exec(command)
-    output = `cd #{@kata_path}; #{command}`
+    output = `cd #{@kata_path} && #{command}`
     return output, $?.exitstatus
   end
 
