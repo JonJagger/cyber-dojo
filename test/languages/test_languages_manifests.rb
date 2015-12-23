d3c0f61f25d5b9b528a21a3d3a5d21d54e7d3117
Jon Jagger <jon@jaggersoft.com>
2015-12-23 12:10:07 +0000

    add language test to check if file is not mentioned in visible_filenames

--- test/languages/test_languages_manifests.rb
+++ test/languages/test_languages_manifests.rb
@@ -70,6 +70,7 @@ class LanguagesManifestsTests < LanguagesTestBase
   def check_manifest(dir)
     @language = dir
     assert manifest_file_exists?
+    assert all_files_are_named_in_manifest?
     assert required_keys_exist?
     refute unknown_keys_exist?
     assert all_visible_files_exist?
@@ -93,6 +94,23 @@ class LanguagesManifestsTests < LanguagesTestBase
   def manifest_file_exists?
     unless File.exists? manifest_filename
       return false_puts_alert "#{manifest_filename} does not exist"
+    end
+    true_dot
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def all_files_are_named_in_manifest?
+    dir = File.dirname(manifest_filename)
+    files = Dir.entries(dir).reject { |entry| File.directory?(entry) }
+    # for some reason this does not reject _docker_context which is a dir
+    files -= [ '_docker_context', 'manifest.json' ]
+    files.each do |filename|
+      # shunit2 is only hidden file
+      next if filename == 'shunit2' && language_dir.end_with?('Bash/shunit2')
+      unless visible_filenames.include? filename
+        return false_puts_alert "#{filename} not present in visible_filenames"
+      end
     end
     true_dot
   end
