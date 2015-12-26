6d0a2090da92a9ded1923fec10f22ed568a0a329
Jon Jagger <jon@jaggersoft.com>
2015-12-26 18:52:07 +0000

    made new docker_machine_runner.sh live

--- lib/docker_machine_runner.sh
+++ lib/docker_machine_runner.sh
@@ -8,18 +8,26 @@ container_name=$3
 max_seconds=$4
 
 # local variables
-ip_node=`docker-machine ip ${node_name}`
 tmp_name=`uuidgen`
 kill=9
 
 # - - - - - - - - - - - - - -
-# 1. rsync avatar's files to the node
+# 1. scp avatar's files to the node
+# This relies on /tmp/cyber-dojo on the target node having the right
+# sticky-bit settings so files created underneath /tmp/cyber-dojo
+# can be seen by the www-data user (the --user in docker run)
+#
+#    $ ...ON THE NODE...
+#    $ cd /tmp
+#    $ mkdir cyber-dojo
+#    $ chmod 777 cyber-dojo
+#    $ chown www-data:www-data cyber-dojo
+#    $ chmod g+rwsx cyber-dojo
+#    $ apt-get install -y acl
+#    $ setfacl -d -m group:www-data:rwx cyber-dojo
+#    $ setfacl    -m group:www-data:rwx cyber-dojo
 
-rsync \
-  --archive \
-  --password-file=/home/cyber-dojo/rsyncd.password \
-  ${sandbox_path} \
-  cyber-dojo@${ip_node}::tmp/${tmp_name}
+docker-machine scp -r ${sandbox_path} ${node_name}:/tmp/cyber-dojo/${tmp_name}/
 
 # - - - - - - - - - - - - - -
 # 2. run avatar's test in node's container
@@ -30,7 +38,7 @@ docker run \
   --rm \
   --user=www-data \
   --net=none \
-  --volume=/tmp/${tmp_name}:/sandbox:rw \
+  --volume=/tmp/cyber-dojo/${tmp_name}:/sandbox:rw \
   --workdir=/sandbox  \
   ${container_name} \
   /bin/bash -c "timeout --signal=${kill} ${max_seconds}s ./cyber-dojo.sh 2>&1"
@@ -38,20 +46,14 @@ docker run \
 exit_status=$?
 
 # - - - - - - - - - - - - - -
-# 3. rsync avatar's files back from the node
-# turned off because this is no longer being run
-# as www-data but as cyber-dojo user which does
-# not have rights
-#rsync \
-#  --archive \
-#  --password-file=/home/cyber-dojo/rsyncd.password \
-#  cyber-dojo@${ip_node}::tmp/${tmp_name}/ \
-#  ${sandbox_path}
+# 3. scp avatar's files back from the node?
+# Turned off because not yet needed (eg in Approval style tests)
+# Note this script is being run as cyber-dojo user (not www-data)
 
 # - - - - - - - - - - - - - -
-# 4. remove temporary files from the node
+# 4. remove avatar's files from the node
 
-docker-machine ssh ${node_name} -- sudo rm -rf /tmp/${tmp_name} &
+docker-machine ssh ${node_name} -- sudo rm -rf /tmp/cyber-dojo/${tmp_name} &
 
 exit ${exit_status}
 
