ca7794083a238bb8770361168fab9a201c35894a
Jon Jagger <jon@jaggersoft.com>
2016-01-08 17:33:17 +0000

    don't use stale runner in refresh scripts

--- lib/docker_runner_refresh.rb
+++ lib/docker_runner_refresh.rb
@@ -15,10 +15,11 @@ else
   `chmod 444 #{config_filename}`
   `chown www-data:www-data #{config_filename}`
 
+  # Do not use runner anymore. It bound to the *old* config
   puts 'refreshing DockerRunner cache'
-  cache_filename = dojo.caches.path + runner.cache_filename
+  cache_filename = dojo.caches.path + dojo.runner.cache_filename
   `chmod --silent 666 #{cache_filename}`
-  runner.refresh_cache
+  dojo.runner.refresh_cache
   `chmod 444 #{cache_filename}`
   `chown www-data:www-data #{cache_filename}`
 end
