763cb1900195b6226a8f865cdd278b49a9bb4dac
Jon Jagger <jon@jaggersoft.com>
2016-01-09 12:03:24 +0000

    refactor params-maker to give better diagnostics

--- test/app_controllers/params_maker.rb
+++ test/app_controllers/params_maker.rb
@@ -11,21 +11,22 @@ class ParamsMaker
     end
   end
 
-  def new_file(filename,content)
-    refute { @visible_files.keys.include?(filename) }
+  def new_file(filename, content)
+    refute_file(filename, 'new_file')
     @visible_files[filename] = content
     @outgoing[filename] = hash(content)
   end
 
   def delete_file(filename)
-    assert { @visible_files.keys.include?(filename) }
+    assert_file(filename, 'delete_file')
     @visible_files.delete(filename)
     @outgoing.delete(filename)
   end
 
-  def change_file(filename,content)
-    assert { @visible_files.keys.include?(filename) }
-    refute { @visible_files[filename] == content }
+  def change_file(filename, content)
+    assert_file(filename, 'change_file')
+    message = [ info(filename, 'change_file'), "\t unchanged!" ].join("\n")
+    refute(message) { @visible_files[filename] == content }
     @visible_files[filename] = content
     @outgoing[filename] = hash(content)
   end
@@ -40,16 +41,45 @@ class ParamsMaker
 
 private
 
+  def assert_file(filename, cmd)
+    message = [
+      info(filename, cmd),
+      "\t no file with that name",
+      "\t filenames are: #{filenames}"
+    ].join("\n")
+    assert(message) { file?(filename) }
+  end
+
+  def refute_file(filename, cmd)
+    message = [
+      info(filename,cmd),
+      "\t already a file with that name"
+    ].join("\n")
+    refute(message) { file?(filename) }
+  end
+
+  def info(filename, cmd)
+    self.class.name + ".#{cmd}(#{filename})"
+  end
+
+  def file?(filename)
+    filenames.include?(filename)
+  end
+
+  def filenames
+    @visible_files.keys
+  end
+
+  def assert(message, &block)
+    fail message unless block.call
+  end
+
+  def refute(message, &block)
+    fail message if block.call
+  end
+
   def hash(content)
     Digest::MD5.hexdigest(content)
-  end
-
-  def assert(&block)
-    fail RuntimeError.new('DeltaMaker.assert') unless block.call
-  end
-
-  def refute(&block)
-    fail RuntimeError.new('DeltaMaker.refute') if block.call
   end
 
 end
