7899aa5691508400ee36851c0f810387e2514d0f
Jon Jagger <jon@jaggersoft.com>
2016-01-10 08:29:45 +0000

    drop needless cd_ prefix from test helper methods

--- test/test_external_helpers.rb
+++ test/test_external_helpers.rb
@@ -7,12 +7,12 @@ module TestExternalHelpers # mix-in
 
   module_function
 
-  # The config has to actually be written to because the controller
-  # tests go through the rails stack and thus create a new dojo
-  # object in a new thread.
+  # The config has to actually be written to the file system
+  # because controller tests go through the rails stack and
+  # thus create a new dojo object in a new thread.
   #
   # Calling setup twice in a row without an intervening teardown
-  # is very bad because an intervening set_katas_root() call (say)
+  # is bad because an intervening set_katas_root() call (say)
   # means the second setup will read the overwritten config
   # so the final teardown will restore the overwritten config! Oops.
 
@@ -31,34 +31,34 @@ module TestExternalHelpers # mix-in
 
   # - - - - - - - - - - - - - - - - - - -
 
-  def set_languages_root(value); root_set('languages', value); end
-  def set_exercises_root(value); root_set('exercises', value); end
-  def    set_caches_root(value); root_set(   'caches', value); end
-  def     set_katas_root(value); root_set(    'katas', value); end
+  def set_languages_root(value); set_root('languages', value); end
+  def set_exercises_root(value); set_root('exercises', value); end
+  def    set_caches_root(value); set_root(   'caches', value); end
+  def     set_katas_root(value); set_root(    'katas', value); end
 
-  def   set_runner_class(value); class_set(   'runner', value); end
-  def    set_shell_class(value); class_set(    'shell', value); end
-  def     set_disk_class(value); class_set(     'disk', value); end
-  def      set_git_class(value); class_set(      'git', value); end
-  def      set_log_class(value); class_set(      'log', value); end
-
-  # - - - - - - - - - - - - - - - - - - -
-
-  def get_languages_root; root_get('languages'); end
-  def get_exercises_root; root_get('exercises'); end
-  def    get_caches_root; root_get(   'caches'); end
-  def     get_katas_root; root_get(    'katas'); end
-
-  def   get_runner_class; class_get('runner'); end
-  def    get_shell_class; class_get( 'shell'); end
-  def     get_disk_class; class_get(  'disk'); end
-  def      get_git_class; class_get(   'git'); end
-  def      get_log_class; class_get(   'log'); end
+  def   set_runner_class(value); set_class(   'runner', value); end
+  def    set_shell_class(value); set_class(    'shell', value); end
+  def     set_disk_class(value); set_class(     'disk', value); end
+  def      set_git_class(value); set_class(      'git', value); end
+  def      set_log_class(value); set_class(      'log', value); end
 
   # - - - - - - - - - - - - - - - - - - -
 
-  def root_set(name, value)
-    fail_if_setup_not_called("root_set(#{name}, #{value})")
+  def get_languages_root; get_root('languages'); end
+  def get_exercises_root; get_root('exercises'); end
+  def    get_caches_root; get_root(   'caches'); end
+  def     get_katas_root; get_root(    'katas'); end
+
+  def   get_runner_class; get_class('runner'); end
+  def    get_shell_class; get_class( 'shell'); end
+  def     get_disk_class; get_class(  'disk'); end
+  def      get_git_class; get_class(   'git'); end
+  def      get_log_class; get_class(   'log'); end
+
+  # - - - - - - - - - - - - - - - - - - -
+
+  def set_root(name, value)
+    fail_if_setup_not_called("set_root(#{name}, #{value})")
     config = read_json_config
     config['root'][name] = value
     IO.write(config_filename, JSON.unparse(config))
@@ -66,18 +66,18 @@ module TestExternalHelpers # mix-in
     #TODO: what about caches?
   end
 
-  def class_set(name, value)
-    fail_if_setup_not_called("class_set(#{name}, #{value})")
+  def set_class(name, value)
+    fail_if_setup_not_called("set_class(#{name}, #{value})")
     config = read_json_config
     config['class'][name] = value
     IO.write(config_filename, JSON.unparse(config))
   end
 
-  def root_get(name)
+  def get_root(name)
     read_json_config['root'][name]
   end
 
-  def class_get(name)
+  def get_class(name)
     read_json_config['class'][name]
   end
 
