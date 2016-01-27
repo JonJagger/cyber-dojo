26f7d1dc457082d176e97e97cd6ca3d0456b78f2
Jon Jagger <jon@jaggersoft.com>
2016-01-27 10:04:11 +0000

    added comments on how to list all language-image names using a docker command; added comments on how to delete runner_cache.json inside web server (using docker command) so newly pulled language-image is seen

--- languages/docker_list_all_images.rb
+++ languages/docker_list_all_images.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/env ruby
 
+# This doesn't currently work if you are on a server built from the
+# docker-compose.yml file. However, if you've done $docker-compose up
+# and the languages-data-container is available you can do this...
+#
+# docker run --rm \
+#    --volumes-from cyber-dojo-languages-data-container:ro \
+#    ruby:2.1 bash -c ./var/www/cyber-dojo/languages/docker_list_all_images.rb
+#
+# After you have pulled a new cyberdojofoundation/language image
+# you have to delete the runner cache inside the web server
+# to force the web server to recreate it. Like this...
+#
+# docker exec cyberdojo_web_1 \
+#    rm /var/www/cyber-dojo/caches/runner_cache.json
+
 require 'json'
 
 cyber_dojo_root = File.expand_path('..', File.dirname(__FILE__))
