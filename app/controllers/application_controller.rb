0d6e8b4514f1e222279d4ef347913168a136c251
Jon Jagger <jon@jaggersoft.com>
2016-01-27 15:09:43 +0000

    added requires back to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ end
 
 #load rooted('all.rb')
 
+require 'json'
+require_relative '../../lib/all'
+require_relative '../../app/helpers/all'
+require_relative '../../app/lib/all'
+require_relative '../../app/models/all'
+
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
