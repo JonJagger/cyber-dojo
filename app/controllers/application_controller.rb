dcd2e1df0c723024c74754043b599a4a5b964b51
Jon Jagger <jon@jaggersoft.com>
2016-01-27 14:10:09 +0000

    put back needed require

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 
 require 'json'
 require_relative '../../lib/all'
+require_relative '../lib/all'
 #require_relative '../helpers/all'
-#require_relative '../lib/all'
 #require_relative '../models/all'
 
 class ApplicationController < ActionController::Base
