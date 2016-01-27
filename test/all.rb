12c8032e82d0efb7185f35bf7e79c7222cb1aefb
Jon Jagger <jon@jaggersoft.com>
2016-01-27 13:54:17 +0000

    moved requires into non root file

--- test/all.rb
+++ test/all.rb
@@ -1,3 +1,6 @@
 
-require_relative './../all'
+require 'json'
 require_relative './lib/all'
+require_relative './app/helpers/all'
+require_relative './app/lib/all'
+require_relative './app/models/all'
