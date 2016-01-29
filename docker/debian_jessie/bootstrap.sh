325c7dca76491e2e1247631b713ce22150c85bf6
Jon Jagger <jon@jaggersoft.com>
2016-01-29 21:21:29 +0000

    have to do curl's first

--- docker/debian_jessie/bootstrap.sh
+++ docker/debian_jessie/bootstrap.sh
@@ -5,13 +5,14 @@
 branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker
 
 curl -O ${branch}/debian_jessie/install_docker.sh
+curl -O ${branch}/debian_jessie/docker-compose.yml
+curl -O ${branch}/debian_jessie/app_up.sh
+curl -O ${branch}/docker-compose.yml
+
 chmod +x install_docker.sh
 install_docker.sh
 
 docker pull cyberdojofoundation/gcc_assert
 
-curl -O ${branch}/docker-compose.yml
-curl -O ${branch}/debian_jessie/docker-compose.yml
-curl -O ${branch}/debian_jessie/app_up.sh
 chmod +x app_up.sh
 ./app_up.sh
--- docker/ubuntu_trusty/bootstrap.sh
+++ docker/ubuntu_trusty/bootstrap.sh
@@ -5,13 +5,14 @@
 branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker
 
 curl -O ${branch}/ubuntu_trusty/install_docker.sh
+curl -O ${branch}/ubuntu_trusty/docker-compose.yml
+curl -O ${branch}/ubuntu_trusty/app_up.sh
+curl -O ${branch}/docker-compose.yml
+
 chmod +x install_docker.sh
 ./install_docker.sh
 
 docker pull cyberdojofoundation/gcc_assert
 
-curl -O ${branch}/docker-compose.yml
-curl -O ${branch}/ubuntu_trusty/docker-compose.yml
-curl -O ${branch}/ubuntu_trusty/app_up.sh
 chmod +x app_up.sh
 ./app_up.sh
