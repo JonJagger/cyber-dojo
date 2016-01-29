4e37e7573e84cec7d8f000981307a244864a7f8e
Jon Jagger <jon@jaggersoft.com>
2016-01-29 20:53:57 +0000

    refactor duplication

--- docker/bootstrap_debian_jessie.sh
+++ docker/bootstrap_debian_jessie.sh
@@ -2,13 +2,13 @@
 
 # Jessie = Debian 8
 
-branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache
+branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker
 
-curl -O ${branch}/docker/install_docker_on_debian_jessie.sh
-curl -O ${branch}/docker/images/app_pull.sh
-curl -O ${branch}/docker/docker-compose.debian_jessie.yml
-curl -O ${branch}/docker/docker-compose.yml
-curl -O ${branch}/docker/debian_jessie_up.sh
+curl -O ${branch}/install_docker_on_debian_jessie.sh
+curl -O ${branch}/images/app_pull.sh
+curl -O ${branch}/docker-compose.debian_jessie.yml
+curl -O ${branch}/docker-compose.yml
+curl -O ${branch}/debian_jessie_up.sh
 
 chmod +x install_docker_on_debian_jessie.sh
 chmod +x app_pull.sh
--- docker/bootstrap_ubuntu_trusty.sh
+++ docker/bootstrap_ubuntu_trusty.sh
@@ -2,13 +2,13 @@
 
 # Trust = Ubuntu 14.04
 
-branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache
+branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker
 
-curl -O ${branch}/docker/install_docker_on_ubuntu_trusty.sh
-curl -O ${branch}/docker/images/app_pull.sh
-curl -O ${branch}/docker/docker-compose.ubuntu_trusty.yml
-curl -O ${branch}/docker/docker-compose.yml
-curl -O ${branch}/docker/ubuntu_trusty_up.sh
+curl -O ${branch}/install_docker_on_ubuntu_trusty.sh
+curl -O ${branch}/images/app_pull.sh
+curl -O ${branch}/docker-compose.ubuntu_trusty.yml
+curl -O ${branch}/docker-compose.yml
+curl -O ${branch}/ubuntu_trusty_up.sh
 
 chmod +x install_docker_on_ubuntu_trusty.sh
 chmod +x app_pull.sh
