e127394c9c9003a633ed38e6b743055c0a0d991e
Jon Jagger <jon@jaggersoft.com>
2016-01-29 21:01:05 +0000

    refactoring

--- docker/bootstrap_debian_jessie.sh
+++ docker/bootstrap_debian_jessie.sh
@@ -5,18 +5,14 @@
 branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker
 
 curl -O ${branch}/install_docker_on_debian_jessie.sh
-curl -O ${branch}/images/app_pull.sh
 curl -O ${branch}/docker-compose.debian_jessie.yml
 curl -O ${branch}/docker-compose.yml
 curl -O ${branch}/debian_jessie_up.sh
 
 chmod +x install_docker_on_debian_jessie.sh
-chmod +x app_pull.sh
-chmod +x debian_jessie_up.sh
-
 ./install_docker_on_debian_jessie.sh
-./app_pull.sh
 
 docker pull cyberdojofoundation/gcc_assert
 
+chmod +x debian_jessie_up.sh
 ./debian_jessie_up.sh
--- docker/bootstrap_ubuntu_trusty.sh
+++ docker/bootstrap_ubuntu_trusty.sh
@@ -5,18 +5,14 @@
 branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker
 
 curl -O ${branch}/install_docker_on_ubuntu_trusty.sh
-curl -O ${branch}/images/app_pull.sh
 curl -O ${branch}/docker-compose.ubuntu_trusty.yml
 curl -O ${branch}/docker-compose.yml
 curl -O ${branch}/ubuntu_trusty_up.sh
 
 chmod +x install_docker_on_ubuntu_trusty.sh
-chmod +x app_pull.sh
-chmod +x ubuntu_trusty_up.sh
-
 ./install_docker_on_ubuntu_trusty.sh
-./app_pull.sh
 
 docker pull cyberdojofoundation/gcc_assert
 
+chmod +x ubuntu_trusty_up.sh
 ./ubuntu_trusty_up.sh
