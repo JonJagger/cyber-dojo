64a2de2372d8af156a555f90f78c2617f3ad32ad
Jon Jagger <jon@jaggersoft.com>
2016-01-31 09:14:56 +0000

    use two docker-compose extends files for osx; one for docker-in-docker volumes; one for all the volumes_from entries which *replace* the original service

--- docker/images/readme.txt
+++ docker/images/readme.txt
@@ -14,3 +14,6 @@ To build them all run ./app_build.sh
 There is also a rails folder to build a rails image which is the base image FROM
 which the web image is built. The split is so a new web image can be built without
 having to do a bundle-install (which takes time).
+
+There is also a test folder to build a data-container holding all the tests.
+This allows you to run tests *inside* the container.
--- docker/os/osx_yosemite/test_app_up.sh
+++ docker/os/osx_yosemite/test_app_up.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Brings up cyber-dojo +tests volume in OSX Docker Quickstart Terminal.
+# Brings up cyber-dojo plus tests volume in OSX Docker Quickstart Terminal.
 # Assumes yml files are in their respective git repo folders.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd ${DIR}
 
-cd ../..
-cd images/test
-./build_docker_image.sh
-
-cd ../..
-cd images/web
-./build_docker_image.sh
-
-cd ../..
-cd os/osx_yosemite
-
+# remove existing containers
 docker rm -f $(docker ps -aq)
 
-docker-compose -f docker-compose.osx-test.yml up &
+# remove existing images
+../../rm_untagged_images.sh
+
+# get test changes
+cd ../../images/test
+./build_docker_image.sh
+
+# get code changes
+cd ../../images/web
+./build_docker_image.sh
+
+# bring up cyber-dojo + tests
+cd ../../os/osx_yosemite
+docker-compose -f ../docker-compose.yml -f docker-compose.osx.yml -f docker-compose.osx-test.yml up &
 
 popd
