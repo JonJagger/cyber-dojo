dba266dfc257c466d1e8b4a3a2bf5f19f8dff0f4
Jon Jagger <jon@jaggersoft.com>
2016-02-01 09:57:33 +0000

    refactoring

--- docker/os/debian_jessie/bootstrap.sh
+++ docker/os/debian_jessie/bootstrap.sh
@@ -3,7 +3,7 @@
 # Installs docker and cyber-dojo onto a raw Jessie (Debian 8) node.
 # Use curl to get this file, chmod +x it, then run it.
 
-branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker/os
+branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker/os/debian_jessie
 
 # Volumes are currently still relative to /var/www/cyber-dojo folder
 # to maintain backward compatibility with
@@ -11,10 +11,10 @@ branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/
 
 mkdir -p /var/www/cyber-dojo
 
-curl -O ${branch}/debian_jessie/install_docker.sh
-curl -O ${branch}/debian_jessie/docker-compose.debian_jessie.yml
-curl -O ${branch}/debian_jessie/app_up.sh
-curl -O ${branch}/docker-compose.yml
+curl -O ${branch}/../docker-compose.yml
+curl -O ${branch}/install_docker.sh
+curl -O ${branch}/docker-compose.debian_jessie.yml
+curl -O ${branch}/app_up.sh
 
 chmod +x install_docker.sh
 ./install_docker.sh
--- docker/os/ubuntu_trusty/bootstrap.sh
+++ docker/os/ubuntu_trusty/bootstrap.sh
@@ -3,7 +3,7 @@
 # Installs docker and cyber-dojo onto a raw Trusty (ubuntu 14.04) node.
 # Use curl to get this file, chmod +x it, then run it.
 
-branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker/os
+branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/docker/os/ubuntu_trusty
 
 # Volumes are currently still relative to /var/www/cyber-dojo folder
 # to maintain backward compatibility with
@@ -11,10 +11,10 @@ branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/
 
 mkdir -p /var/www/cyber-dojo
 
-curl -O ${branch}/ubuntu_trusty/install_docker.sh
-curl -O ${branch}/ubuntu_trusty/docker-compose.ubunt_trusty.yml
-curl -O ${branch}/ubuntu_trusty/app_up.sh
-curl -O ${branch}/docker-compose.yml
+curl -O ${branch}/../docker-compose.yml
+curl -O ${branch}/install_docker.sh
+curl -O ${branch}/docker-compose.ubunt_trusty.yml
+curl -O ${branch}/app_up.sh
 
 chmod +x install_docker.sh
 ./install_docker.sh
