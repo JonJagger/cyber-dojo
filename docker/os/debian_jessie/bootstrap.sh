e0ea654f44955f87fc133591d4418e36d4d162d2
Jon Jagger <jon@jaggersoft.com>
2016-02-02 06:32:41 +0000

    bootstrap now shows install/app-up options but does not run them; allows you to not always do docker re-install

--- docker/os/debian_jessie/bootstrap.sh
+++ docker/os/debian_jessie/bootstrap.sh
@@ -11,17 +11,21 @@ branch=https://raw.githubusercontent.com/JonJagger/cyber-dojo/runner-auto-cache/
 
 mkdir -p /var/www/cyber-dojo
 
-curl -O ${branch}/../docker-compose.yml
 curl -O ${branch}/install_docker.sh
-curl -O ${branch}/docker-compose.debian_jessie.yml
 curl -O ${branch}/app_up.sh
+curl -O ${branch}/docker-compose.debian_jessie.yml
+curl -O ${branch}/../docker-compose.yml
 
 chmod +x install_docker.sh
 chmod +x app_up.sh
 
-# do these seprately?
-./install_docker.sh
-
+# Hack: Ensure there is one language image so create page works
 docker pull cyberdojofoundation/gcc_assert
 
-./app_up.sh
+echo
+echo 'To (re)install docker'
+echo '   $ ./install_docker.sh '
+echo
+echo 'To bring the cyber-dojo app up'
+echo '   $ ./app_up.sh'
+echo
