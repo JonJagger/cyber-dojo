3db6b4c265083cd235d9ac98be0a91bfa5ecc494
Jon Jagger <jon@jaggersoft.com>
2016-02-03 13:55:10 +0000

    test refactoring

--- test/test_hex_id_helpers.rb
+++ test/test_hex_id_helpers.rb
@@ -9,8 +9,8 @@ module TestHexIdHelpers # mix-in
 
   module ClassMethods
 
-    @@args = (ARGV.sort.uniq - ['--']).map(&:upcase)  # eg  2DD6F3 or 2dd
-    @@seen = []
+    @@args = (ARGV.sort.uniq - ['--']).map(&:upcase)  # eg  2DD6F3 eg 2dd
+    @@seen_ids = []
 
     def test(id, *lines, &block)
       diagnostic = "'#{id}',#{lines.join}"
@@ -18,25 +18,23 @@ module TestHexIdHelpers # mix-in
       is_hex_id = id.chars.all? { |ch| hex_chars.include? ch }
       has_empty_line = lines.any? { |line| line.strip == '' }
       has_space_line = lines.any? { |line| line.strip != line }
-
-      raise "non hex-ID: #{diagnostic}" unless id != ''
+      raise  "no hex-ID: #{diagnostic}" if id == ''
       raise "bad hex-ID: #{diagnostic}" unless is_hex_id
       raise "empty line: #{diagnostic}" if has_empty_line
       raise "space line: #{diagnostic}" if has_space_line
-
       no_args = @@args == []
-      an_arg_is_part_of_id = @@args.any?{ |arg| id.include?(arg) }
-      if no_args || an_arg_is_part_of_id
-        raise "duplicate hex_ID: #{diagnostic}" if @@seen.include?(id)
-        @@seen << id
+      any_arg_is_part_of_id = @@args.any?{ |arg| id.include?(arg) }
+      if no_args || any_arg_is_part_of_id
+        raise "duplicate hex_ID: #{diagnostic}" if @@seen_ids.include?(id)
+        @@seen_ids << id
         name = lines.join(' ')
         define_method("test_'#{id}',\n #{name}\n".to_sym, &block)
       end
     end
 
     ObjectSpace.define_finalizer(self, proc {
-      seen_args = @@args.find_all { |arg| @@seen.any? { |id| id.include?(arg) } }
-      unseen_args = @@args - seen_args
+      unseen_arg = lambda { |arg| @@seen_ids.none? { |id| id.include?(arg) } }
+      unseen_args = @@args.find_all { |arg| unseen_arg.call(arg) }
       if unseen_args != []
         # can't raise in a finalizer
         message = 'the following test id arguments were *not* found'
