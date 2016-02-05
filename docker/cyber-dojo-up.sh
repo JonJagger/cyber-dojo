204e61a3b34ef825885e78c11130d3fe0fddcc0d
Jon Jagger <jon@jaggersoft.com>
2016-02-05 18:43:57 +0000

    push accidentally overridden file

--- docker/cyber-dojo-up.sh
+++ docker/cyber-dojo-up.sh
@@ -1,1 +1,13 @@
-Not Found
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd ${DIR} > /dev/null
+
+export CYBER_DOJO_ROOT=${1:-/var/www/cyber-dojo}
+export CYBER_DOJO_UP=${2:-production}
+
+docker-compose \
+  --file docker-compose.yml \
+  up &
+
+popd > /dev/null
