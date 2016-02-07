278418636c8561e49d48fe1ff2cf7829cdd77031
Jon Jagger <jon@jaggersoft.com>
2016-02-07 11:38:54 +0000

    added notes on whether to keep or change language image names as reflected in manifest.json files

--- docker/show_instructions.sh
+++ docker/show_instructions.sh
@@ -16,11 +16,19 @@ echo ':   $ ./build-katas-image.sh'
 echo ':'
 echo '--------------------------------------------'
 echo
+
+# NB: if install is on top of old-style server I need
+#     to update the old images if the new image (alpine based)
+#     has the same name. This is also a once only operation.
+#
+
+# NB: Better to just install a common selection...
 echo 'install the docker image for your chosen languages, eg'
 echo '  $ docker pull cyberdojofoundation/gcc_assert'
 echo '  $ docker pull cyberdojofoundation/java_junit'
 echo '  $ docker pull cyberdojofoundation/csharp_nunit'
 echo
+
 echo bring up cyber-dojo
 echo   $ ./cyber-dojo-up.sh
 echo
