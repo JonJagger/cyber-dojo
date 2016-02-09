adde7a1b8d2a968583c7b4def4ffb5464652ee1b
Jon Jagger <jon@jaggersoft.com>
2016-02-09 17:25:26 +0000

    dropped .sh from scripts and started making cyber-dojo script work like cdf script

--- languages/list_all_images.rb
+++ languages/list_all_images.rb
@@ -9,20 +9,26 @@ require 'json'
 
 cyber_dojo_root = File.expand_path('..', File.dirname(__FILE__))
 
+longest_test_name=""
+
 image_names = {}
 Dir.glob("#{cyber_dojo_root}/languages/*/*/manifest.json") do |file|
   manifest = JSON.parse(IO.read(file))
   language_name, test_name = manifest['display_name'].split(',').map { |s| s.strip }
-  image_name = manifest['image_name']
+  longest_test_name = test_name if test_name.size > longest_test_name.size
+  full_image_name = manifest['image_name']
+  image_name=full_image_name.split('/')[1].strip
   image_names[language_name] ||= {}
   image_names[language_name][test_name] = image_name
 end
 
+puts "#"
 image_names.sort.map do |language_name,tests|
-  puts language_name
+  puts "# #{language_name}"
   tests.sort.map do |name, image_name|
-    puts "  " + name + " -> " + image_name
+    dots = '.' * (longest_test_name.size - name.size)
+    puts '#   ' + dots + ' ' + name + ' -> ' + image_name
   end
-  puts
 end
+puts "#"
 
