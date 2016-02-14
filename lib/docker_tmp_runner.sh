a4d45c35d6f6b399b1af28f6911c3528980d903e
Jon Jagger <jon@jaggersoft.com>
2016-02-14 19:38:50 +0000

    collapse two docker-execs into one

--- lib/docker_tmp_runner.sh
+++ lib/docker_tmp_runner.sh
@@ -3,21 +3,21 @@
 # Called from lib/docker_tmp_runner.rb DockerTmpRunner.run()
 # Also See comments in lib/host_shell.rb
 #
-# The docker run/exec commands all use sh and not bash because
-# the web container is based off Alpine based which doesn't have bash.
-# UPDATE: That's no longer true. I've installed bash since the test
-# scripts are all bash based. But no harm in using sh.
+# The docker run/exec commands use sh not bash because the web
+# container is Alpine-linux based which doesn't have bash.
+# UPDATE: That's no longer true. The test scripts are all bash
+# based to I've installed bash. But no harm in using sh.
 
 FILES_PATH=$1     # Where source files have been saved to
 IMAGE=$2          # eg cyberdojofoundation/gcc_assert
 MAX_SECONDS=$3    # eg 10
 
-USER=nobody               # user who runs cyber-dojo.sh inside test container
-TMP_PATH=/tmp/cyber-dojo  # Where source files are untarred to to inside test container
+USER=nobody               # user who runs cyber-dojo.sh inside container
+TMP_PATH=/tmp/cyber-dojo  # Where user runs cyber-dojo.sh inside container
 
-# 1. Run the container
-# --detached so we can follow with exec's
-# --interactive because we pipe the tar file in
+# 1. Start the container running
+# --detached so we can follow with exec
+# --interactive because we tar pipe into it
 # --net=none for security
 # --user=nobody for security
 CID=$(sudo docker run \
@@ -27,30 +27,26 @@ CID=$(sudo docker run \
   --user=$USER \
     $IMAGE sh)
 
-# 2. Tar pipe the source files into the container
-# http://blog.extracheese.org/2010/05/the-tar-pipe.html
-# --user=root means root *inside* the container so it can chown
+# 2. After max_seconds, forcibly shut down the container.
+(sleep $MAX_SECONDS && sudo docker rm --force $CID &> /dev/null) &
 
+# 3. Tar pipe files into the container and run cyber-dojo.sh as user=nobody
+# http://blog.extracheese.org/2010/05/the-tar-pipe.html
+# 2>&1 captures compilation failure output
 (cd $FILES_PATH && tar -cf - .) \
   | sudo docker exec \
       --interactive \
-      --user=root \
+      --user=$USER \
       $CID \
       sh -c \
         "mkdir $TMP_PATH \
-      && tar -xf - -C $TMP_PATH \
-      && chown -R $USER $TMP_PATH"
+      && cd $TMP_PATH \
+      && tar -xf - -C . \
+      && ./cyber-dojo.sh 2>&1"
 
-# 3. After max_seconds, forcibly shut down the container.
-(sleep $MAX_SECONDS && sudo docker rm --force $CID &> /dev/null) &
-
-# 4. Run cyber-dojo.sh in the container as user=nobody
-# The 2>&1 redirect captures compilation failure output
-sudo docker exec --user=$USER $CID sh -c "cd $TMP_PATH && ./cyber-dojo.sh 2>&1"
-EXIT_STATUS=$?
-
-# 5. If the container isn't running, the sleep process killed it
+# 4. If the container isn't running, the sleep woke and killed it
 # https://gist.github.com/ekristen/11254304
+EXIT_STATUS=$?
 RUNNING=$(docker inspect --format="{{ .State.Running }}" $CID)
 if [ "$RUNNING" != "true" ]; then
   EXIT_STATUS=137 # (128=timed-out) + (9=killed)
