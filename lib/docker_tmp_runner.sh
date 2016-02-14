30d08c8d0da5f4c2567dcd50879667bfc620f61c
Jon Jagger <jon@jaggersoft.com>
2016-02-14 18:40:54 +0000

    avoid using physical tar file docker-tmp-runner by using a tar-pipe; neat

--- lib/docker_tmp_runner.sh
+++ lib/docker_tmp_runner.sh
@@ -2,21 +2,18 @@
 
 # Called from lib/docker_tmp_runner.rb DockerTmpRunner.run()
 # Also See comments in lib/host_shell.rb
+#
+# The docker run/exec commands all use sh and not bash because
+# the web container is based off Alpine based which doesn't have bash.
+# UPDATE: That's no longer true. I've installed bash since the test
+# scripts are all bash based. But no harm in using sh.
 
 FILES_PATH=$1     # Where source files have been saved to
 IMAGE=$2          # eg cyberdojofoundation/gcc_assert
 MAX_SECONDS=$3    # eg 10
 
-TAR_PATH=`mktemp`.tgz          # Source files from $FILES_PATH are tarred into this
-TMP_PATH=/tmp/cyber-dojo       # Where source files are untarred to to inside test container
-USER=nobody                    # user who runs cyber-dojo.sh inside test container
-
-# The docker run/exec commands all use sh and not bash because
-# the web container is based off Alpine based which doesn't have bash.
-
-# Create tar file from source files
-cd $FILES_PATH
-tar -zcf $TAR_PATH .
+USER=nobody               # user who runs cyber-dojo.sh inside test container
+TMP_PATH=/tmp/cyber-dojo  # Where source files are untarred to to inside test container
 
 # Run the container
 # --detached so we can follow with exec's
@@ -30,22 +27,19 @@ CID=$(sudo docker run \
   --user=$USER \
     $IMAGE sh)
 
-# Get the source files into the running container
-# --user=root means root *inside* the container so it can chmod
-# tar [-] means get input from the catted tar-file
-# tar -C PATH means eXtract the tar's files into PATH
-cat $TAR_PATH \
+# Tar pipe the source files into the running container
+# http://blog.extracheese.org/2010/05/the-tar-pipe.html
+# --user=root means root *inside* the container so it can chown
+
+(cd $FILES_PATH && tar -cf - .) \
   | sudo docker exec \
       --interactive \
       --user=root \
       $CID \
       sh -c \
         "mkdir $TMP_PATH \
-      && tar zxf - -C $TMP_PATH \
+      && tar -xf - -C $TMP_PATH \
       && chown -R $USER $TMP_PATH"
-
-# Tar file has done its job so delete it
-rm $TAR_PATH
 
 # After max_seconds, forcibly shut down the container.
 (sleep $MAX_SECONDS && sudo docker rm --force $CID &> /dev/null) &
