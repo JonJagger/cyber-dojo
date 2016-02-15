72b6e6abae4757074287c04805c75725fb78160b
Jon Jagger <jon@jaggersoft.com>
2016-02-15 21:57:24 +0000

    basic shell to give you two katas data-container options

--- docker/katas/build-image.sh
+++ docker/katas/build-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-CYBER_DOJO_KATAS_ROOT=$1      # where they are copied to in image. Required.
+CYBER_DOJO_KATAS_ROOT=$1      # where files are copied to in image. Required.
 KATAS_DIR=$2                  # where katas are copied from on host. Optional
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
@@ -10,6 +10,10 @@ MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 build_empty_katas_data_container()
 {
+  # for building a new server - no existing /var/www/cyber-dojo/katas folder
+  #
+  # ./build-image.sh /usr/src/cyber-dojo/app/katas
+
   CONTEXT_DIR=.
   pushd ${MY_DIR} > /dev/null
 
@@ -29,21 +33,30 @@ build_empty_katas_data_container()
 
 build_full_katas_data_container()
 {
-  # check KATAS_DIR exists (like cyber-dojo script)
+  # for building a replacement server - an existing /var/www/cyber-dojo/katas folder
+  #
+  # ./build-image.sh /usr/src/cyber-dojo/app/katas /var/www/cyber-dojo/test/app_models
+
+  # TODO: check KATAS_DIR exists (like cyber-dojo script)
   # On OSX Docker-Quickstart-Terminal this script has to run on defult
   # docker-machine scp it?
 
   CONTEXT_DIR=${KATAS_DIR}
   pushd ${MY_DIR} > /dev/null
-  #cp ./Dockerfile ${CONTEXT_DIR}
+  cp ./Dockerfile    ${CONTEXT_DIR}
+  cp ./.dockerignore ${CONTEXT_DIR}
 
-  echo 'pwd'
-  pwd
-  echo 'ls'
-  ls
-  echo 'docker build... FULL katas'
+  docker build \
+    --build-arg=CYBER_DOJO_KATAS_ROOT=${CYBER_DOJO_KATAS_ROOT} \
+    --tag=cyberdojofoundation/${PWD##*/} \
+    --file=${CONTEXT_DIR}/Dockerfile \
+    ${CONTEXT_DIR}
 
-  #rm ${CONTEXT_DIR}/Dockerfile
+  docker create --name cyber-dojo-katas cyberdojofoundation/katas
+  docker run --rm -it --volumes-from=cyber-dojo-katas cyberdojofoundation/user-base sh
+
+  rm ${CONTEXT_DIR}/Dockerfile
+  rm ${CONTEXT_DIR}/.dockerignore
   popd > /dev/null
 
 }
@@ -55,6 +68,12 @@ if [ "${KATAS_DIR}" == "" ]; then
 else
   build_full_katas_data_container
 fi
+
+# docker-compose.yml entry looks like this...
+#
+#katas:
+#  build: ./docker/katas_image       <<<<< where Dockerfile is
+#  entrypoint: /bin/sh
 
 
 
