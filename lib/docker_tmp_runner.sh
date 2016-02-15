32053354bdf685c986fc307b75560c5eb2ac5ed3
Jon Jagger <jon@jaggersoft.com>
2016-02-15 17:13:11 +0000

    reformat docker_tmp_runner.rb

--- lib/docker_tmp_runner.sh
+++ lib/docker_tmp_runner.sh
@@ -8,24 +8,30 @@ IMAGE=$2          # eg cyberdojofoundation/gcc_assert
 MAX_SECONDS=$3    # eg 10
 
 # 1. Start the container running
-# -d == detached; get the CID for [sleep && docker rm] before [docker exec]
-# -i == interactive; we tar-pipe
+# --detach; get the CID for [sleep && docker rm] before [docker exec]
+# --interactive; we tar-pipe later
 # --net=none for security
 # --user=nobody for security
-CID=$(sudo docker run -d -i --net=none --user=nobody $IMAGE sh)
+CID=$(sudo docker run --detach \
+                      --interactive \
+                      --net=none \
+                      --user=nobody $IMAGE sh)
 
-# 2. After max_seconds, forcibly shut down the container.
+# 2. After max_seconds, remove the container.
 (sleep $MAX_SECONDS && sudo docker rm --force $CID) &
 
 # 3. Tar pipe files into the container and run cyber-dojo.sh
-(cd $FILES_PATH && tar -cf - .) | sudo docker exec -i $CID \
-  sh -c "mkdir /tmp/cyber-dojo \
-      && cd /tmp/cyber-dojo \
-      && tar -xf - -C . \
-      && ./cyber-dojo.sh 2>&1"
+(cd $FILES_PATH && tar -cf - .) \
+  | sudo docker exec --interactive \
+                     --user=nobody \
+      $CID \
+      sh -c "mkdir /tmp/cyber-dojo \
+          && cd /tmp/cyber-dojo \
+          && tar -xf - -C . \
+          && ./cyber-dojo.sh 2>&1"
 EXIT_STATUS=$?
 
-# 4. If the container isn't running, the sleep woke and killed it
+# 4. If the container isn't running, the sleep woke and removed it
 RUNNING=$(sudo docker inspect --format="{{ .State.Running }}" $CID)
 if [ "$RUNNING" != "true" ]; then
   exit 137 # (128=timed-out) + (9=killed)
