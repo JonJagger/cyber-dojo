ab0da23cf31ffd4dfc9d8cd55ab17daeb8ad472a
Jon Jagger <jon@jaggersoft.com>
2016-02-15 17:31:14 +0000

    final docker_tmp_runner.rb polishing (maybe)

--- lib/docker_tmp_runner.sh
+++ lib/docker_tmp_runner.sh
@@ -3,9 +3,9 @@
 # Also See comments in lib/host_shell.rb
 # http://blog.extracheese.org/2010/05/the-tar-pipe.html
 
-FILES_PATH=$1     # Where source files have been saved to
-IMAGE=$2          # eg cyberdojofoundation/gcc_assert
-MAX_SECONDS=$3    # eg 10
+SRC_DIR=$1     # Where source files are
+IMAGE=$2       # What they'll run in, eg cyberdojofoundation/gcc_assert
+MAX_SECS=$3    # How long they've got, eg 10
 
 # 1. Start the container running
 # --detach; get the CID for [sleep && docker rm] before [docker exec]
@@ -18,15 +18,17 @@ CID=$(sudo docker run --detach \
                       --user=nobody $IMAGE sh)
 
 # 2. After max_seconds, remove the container.
-(sleep $MAX_SECONDS && sudo docker rm --force $CID) &
+(sleep $MAX_SECS && sudo docker rm --force $CID) &
 
 # 3. Tar pipe files into the container and run cyber-dojo.sh
-(cd $FILES_PATH && tar -cf - .) \
+TMP_DIR=/tmp/cyer-dojo
+
+(cd $SRC_DIR && tar -cf - .) \
   | sudo docker exec --interactive \
                      --user=nobody \
       $CID \
-      sh -c "mkdir /tmp/cyber-dojo \
-          && cd /tmp/cyber-dojo \
+      sh -c "mkdir $TMP_DIR \
+          && cd $TMP_DIR \
           && tar -xf - -C . \
           && ./cyber-dojo.sh 2>&1"
 EXIT_STATUS=$?
