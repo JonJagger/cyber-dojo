d57d5f0f3c7264d31b7003c5f4f94ef6586e2f9c
Jon Jagger <jon@jaggersoft.com>
2016-02-18 11:19:40 +0000

    rails-controller failure doesn't occur if katas-data-container's KATAS_ROOT is /usr/src/cyber-dojo/katas (it does if its /usr/src/cyber-dojo/app/katas for reasons I do not understand)

--- docker/katas/create-empty-katas-container.sh
+++ docker/katas/create-empty-katas-container.sh
@@ -4,5 +4,10 @@ set -e
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 pushd ${MY_DIR} > /dev/null
-./build-image.sh /usr/src/cyber-dojo/app/katas
+
+# This has to be the same as KATAS_DEFAULT in docker/cyber-dojo script
+CYBER_DOJO_KATAS_ROOT=/usr/src/cyber-dojo/katas
+
+./build-image.sh ${CYBER_DOJO_KATAS_ROOT}
+
 popd > /dev/null
