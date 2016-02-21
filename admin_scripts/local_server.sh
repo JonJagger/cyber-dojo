05218fe5873fe985ba85045f3060dff3825ea1c3
Jon Jagger <jon@jaggersoft.com>
2016-02-21 11:01:23 +0000

    delete dead comments

--- admin_scripts/local_server.sh
+++ admin_scripts/local_server.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# TODO: Get working.
-# This is because runner rb's make_cache does this
-#     output, _ = shell.exec('sudo -u docker-runner sudo docker images')
-# and the
-#  [sudo -u docker-runner sudo]
-# won't work on a local-host
-# Make that a server VAR?
-
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 RUNNER=${1:-DockerKatasDataContainerRunner}
 
@@ -28,7 +20,7 @@ export CYBER_DOJO_GIT_CLASS=HostGit
 export CYBER_DOJO_KATAS_ROOT=${HOME}/katas
 export CYBER_DOJO_KATAS_CLASS=HostDiskKatas
 
-# TODO: runner can be plain volume-mounting runner
+# TODO: runner can be plain volume-mounting runner. No need for tar-pipes.
 export CYBER_DOJO_RUNNER_CLASS=${RUNNER}
 export CYBER_DOJO_RUNNER_SUDO=''
 
