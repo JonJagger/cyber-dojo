814001dd8710d579387a94d9d176aa1e55945976
Jon Jagger <jon@jaggersoft.com>
2016-02-21 10:01:21 +0000

    refactoring

--- test/test_external_helpers.rb
+++ test/test_external_helpers.rb
@@ -28,17 +28,17 @@ module TestExternalHelpers # mix-in
 
   # - - - - - - - - - - - - - - - - - - -
 
-  def unset_languages_root; unset(dojo.env_name('languages', 'root')); end
-  def unset_exercises_root; unset(dojo.env_name('exercises', 'root')); end
-  def    unset_caches_root; unset(dojo.env_name('caches',    'root')); end
-  def     unset_katas_root; unset(dojo.env_name('katas',     'root')); end
+  def unset_languages_root; unset_root('languages'); end
+  def unset_exercises_root; unset_root('exercises'); end
+  def    unset_caches_root; unset_root(   'caches'); end
+  def     unset_katas_root; unset_root(    'katas'); end
 
-  def unset_runner_class; unset(dojo.env_name('runner', 'class')); end
-  def  unset_katas_class; unset(dojo.env_name('katas',  'class')); end
-  def  unset_shell_class; unset(dojo.env_name('shell',  'class')); end
-  def   unset_disk_class; unset(dojo.env_name('disk',   'class')); end
-  def    unset_git_class; unset(dojo.env_name('git',    'class')); end
-  def    unset_log_class; unset(dojo.env_name('log',    'class')); end
+  def unset_runner_class; unset_class('runner'); end
+  def  unset_katas_class; unset_class( 'katas'); end
+  def  unset_shell_class; unset_class( 'shell'); end
+  def   unset_disk_class; unset_class(  'disk'); end
+  def    unset_git_class; unset_class(   'git'); end
+  def    unset_log_class; unset_class(   'log'); end
 
   # - - - - - - - - - - - - - - - - - - -
 
@@ -47,30 +47,40 @@ module TestExternalHelpers # mix-in
   def    set_caches_root(value); set_root(   'caches', value); end
   def     set_katas_root(value); set_root(    'katas', value); end
 
-  def   set_runner_class(value); set_class(   'runner', value); end
-  def    set_katas_class(value); set_class(    'katas', value); end
-  def    set_shell_class(value); set_class(    'shell', value); end
-  def     set_disk_class(value); set_class(     'disk', value); end
-  def      set_git_class(value); set_class(      'git', value); end
-  def      set_log_class(value); set_class(      'log', value); end
+  def   set_runner_class(value); set_class('runner', value); end
+  def    set_katas_class(value); set_class( 'katas', value); end
+  def    set_shell_class(value); set_class( 'shell', value); end
+  def     set_disk_class(value); set_class(  'disk', value); end
+  def      set_git_class(value); set_class(   'git', value); end
+  def      set_log_class(value); set_class(   'log', value); end
 
   # - - - - - - - - - - - - - - - - - - -
 
-  def get_languages_root; dojo.fenv('languages', 'root'); end
-  def get_exercises_root; dojo.fenv('exercises', 'root'); end
-  def    get_caches_root; dojo.fenv('caches',    'root'); end
-  def     get_katas_root; dojo.fenv('katas',     'root'); end
+  def get_languages_root; get_root('languages'); end
+  def get_exercises_root; get_root('exercises'); end
+  def    get_caches_root; get_root(   'caches'); end
+  def     get_katas_root; get_root(    'katas'); end
 
-  def   get_runner_class; dojo.fenv('runner', 'class'); end
-  def    get_katas_class; dojo.fenv('katas',  'class'); end
-  def    get_shell_class; dojo.fenv('shell',  'class'); end
-  def     get_disk_class; dojo.fenv('disk',   'class'); end
-  def      get_git_class; dojo.fenv('git',    'class'); end
-  def      get_log_class; dojo.fenv('log',    'class'); end
+  def   get_runner_class; get_class('runner'); end
+  def    get_katas_class; get_class( 'katas'); end
+  def    get_shell_class; get_class( 'shell'); end
+  def     get_disk_class; get_class(  'disk'); end
+  def      get_git_class; get_class(   'git'); end
+  def      get_log_class; get_class(   'log'); end
 
   # - - - - - - - - - - - - - - - - - - -
 
   def unset(var); ENV.delete(var); end
+
+  def unset_root(name)
+    unset(dojo.env_name(name, 'root'))
+  end
+
+  def unset_class(name)
+    unset(dojo.env_name(name, 'class'))
+  end
+
+  # - - - - - - - - - - - - - - - - - - -
 
   def set_root(key, value)
     fail_if_setup_not_called("set_root(#{key}, #{value})")
@@ -81,6 +91,16 @@ module TestExternalHelpers # mix-in
   def set_class(key, value)
     fail_if_setup_not_called("set_class(#{key}, #{value})")
     ENV[dojo.env_name(key, 'class')] = value
+  end
+
+  # - - - - - - - - - - - - - - - - - - -
+
+  def get_root(name)
+    dojo.fenv(name, 'root')
+  end
+
+  def get_class(name)
+    dojo.fenv(name, 'class')
   end
 
   # - - - - - - - - - - - - - - - - - - -
