e45d014467a0ddae774406e6f10bb43cc0a408d6
Jon Jagger <jon@jaggersoft.com>
2016-02-24 15:00:41 +0000

    store katas created by test run off tmp/ so they're gitignored

--- local-server.sh
+++ local-server.sh
@@ -23,7 +23,7 @@ export CYBER_DOJO_GIT_CLASS=HostGit
 # TODO: where should katas_root point to for a local server?
 #       HOME/katas saves to host disk
 #       /usr/src/cyber-dojo/katas  saves to data-container
-export CYBER_DOJO_KATAS_ROOT=${HOME}/katas
+export CYBER_DOJO_KATAS_ROOT=${HOME}/tmp/katas
 export CYBER_DOJO_KATAS_CLASS=HostDiskKatas
 
 # TODO: On a local-server runner can be plain volume-mounting runner. No need for tar-pipes.
