373ca80147bef6905ec20fc4a750ea646a96ff5e
Jon Jagger <jon@jaggersoft.com>
2016-03-03 09:52:48 +0000

    added missing app_controllers enter/show tests

--- test/app_controllers/test_dojo.rb
+++ test/app_controllers/test_dojo.rb
@@ -4,7 +4,9 @@ require_relative './app_controller_test_base'
 
 class DojoControllerTest < AppControllerTestBase
 
-  test 'BF7103',
+  prefix = '103'
+
+  test prefix+'BF7',
   'index without id' do
     get 'dojo/index'
     assert_response :success
@@ -12,7 +14,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test '120957',
+  test prefix+'957',
   'index with id' do
     get 'dojo/index', id:'1234512345'
     assert_response :success
@@ -20,7 +22,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test '91778E',
+  test prefix+'78E',
   'check_id exists=false when no kata for id' do
     @id = 'abcdef'
     check_id
@@ -28,7 +30,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test '51690C',
+  test prefix+'90C',
   'check_id exists=true when id.length ~ 6 and kata exists' do
     [5,6,7].each do |n|
       create_kata
@@ -42,14 +44,29 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test 'AF2F15',
+  test prefix+'8EE',
+  'show with no id' do
+    get '/enter/show'
+  end
+
+  #- - - - - - - - - - - - - - - -
+
+  test prefix+'E3E',
+  'show with an id' do
+    create_kata
+    get '/enter/show', { :id => @id }
+  end
+
+  #- - - - - - - - - - - - - - - -
+
+  test prefix+'F15',
   'start with no id raises' do
     assert_raises { start }
   end
 
   #- - - - - - - - - - - - - - - -
 
-  test '9AFB84',
+  test prefix+'B84',
   'start with empty string id raises' do
     @id = ''
     assert_raises { start }
@@ -57,7 +74,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test 'E16A79',
+  test prefix+'A79',
   'start with id that does not exist raises' do
     @id = 'ab00ab11ab'
     assert_raise { start }
@@ -65,7 +82,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test '812BEE',
+  test prefix+'BEE',
   'enter with id that does exist => !full,avatar_name' do
     create_kata
     start
@@ -76,7 +93,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test 'C0F2AE',
+  test prefix+'2AE',
   'enter succeeds once for each avatar name, then dojo is full' do
     create_kata
     Avatars.names.each do |avatar_name|
@@ -93,7 +110,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test '3B15BD',
+  test prefix+'5BD',
   'continue with id that exists but is empty' do
     create_kata
     continue
@@ -103,7 +120,7 @@ class DojoControllerTest < AppControllerTestBase
 
   #- - - - - - - - - - - - - - - -
 
-  test 'CFFDEB',
+  test prefix+'DEB',
   'continue with id that exists and is not empty' do
     create_kata
     start
