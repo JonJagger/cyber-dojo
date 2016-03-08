7d939748f882ac56200b24ec80f12e3d1a15d300
Byran Wills-Heath <byran@adgico.co.uk>
2016-03-08 05:04:04 +0000

    Changed clang++ image to be based on the gpp-5.3 image

--- languages/C++ (clang++)/_docker_context/scripts/install-gsl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Microsoft/GSL/archive/master.zip
-
-unzip master.zip
-cp -r GSL-master/include /usr/local
-
-rm -rf GSL-master
-rm -f master.zip
--- languages/C++ (clang++)/_docker_context/scripts/remove-tools.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-apt-get remove -y wget unzip software-properties-common
