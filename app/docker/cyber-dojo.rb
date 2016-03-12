42ef1a720e83bccda98e6b9b5529cdf7953f9353
Jon Jagger <jon@jaggersoft.com>
2016-03-12 13:17:41 +0000

    do creation of katas-data-container in shell script on host and not in ruby inside container

--- app/docker/cyber-dojo.rb
+++ app/docker/cyber-dojo.rb
@@ -1,5 +1,11 @@
 
 # TODO: convert this all to Ruby
+# TODO: When docker commands are run from inside a container I think they will need a sudo
+# TODO: I need to split the upgrade command
+#       upgrade server
+#       upgrade languages
+#       so you can keep a custom server.
+#       Or maybe upgrade just does languages?
 # TODO: check with works from any dir (downloads docker-compose.yml)
 # TODO: add command to backup katas-data-container to .tgz file
 # TODO: create katas-DATA-CONTAINER only if RUNNER=DockerTarPipeRunner?
@@ -9,14 +15,12 @@ $me = 'cyber-dojo.rb'
 $my_dir = File.expand_path(File.dirname(__FILE__))
 
 $docker_hub_username='cyberdojofoundation'
-
-#$home = '/usr/src/cyber-dojo'  # home folder *inside* the server image
+$docker_compose_file = 'docker-compose.yml'
+$docker_compose_cmd = "docker-compose --file=#{$my_dir}/#{$docker_compose_file}"
+$home = '/usr/src/cyber-dojo'  # home folder *inside* the server image
 
 #RUNNER_DEFAULT=DockerTarPipeRunner
 #RUNNER=${RUNNER_DEFAULT}         # see app/models/dojo.rb
-
-#KATAS_DEFAULT=/var/www/cyber-dojo/katas
-#KATAS=${KATAS_DEFAULT}           # where katas are stored on the *host*
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -27,15 +31,16 @@ def help
     "     #{$me} help",
     '',
     'COMMAND(server):',
-    # TODO: backup a katas-ID data-container
+    '     backup               Backup all practice sessions',
+    '     backup ID            Backup one practice session',
     '     down                 Stops and removes server',
     '     up                   Creates and starts the server',
     '',
     'COMMAND(languages):',
+    '     catalog              Lists all languages',
     '     images               Lists pulled languages',
     '     pull IMAGE           Pulls language IMAGE',
-    '     catalog              Lists all languages',
-    '     rmi IMAGE            Removes a pulled language IMAGE',
+    '     remove IMAGE         Removes a pulled language IMAGE',
     '     upgrade              Pulls the latest server and languages',
     ''
   ].each { |line| puts line }
@@ -43,9 +48,25 @@ end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+def run(command)
+  puts command
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def backup(id)
+  puts 'TODO: backup'
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 def up
-  puts 'TODO:one_time_create_katas_data_container'
-  puts 'TODO:pull_common_languages_if_none'
+  puts ">>>>>>>REVERSED LOGIC IN PLACE<<<<<<"
+  return if pulled_images == []
+  puts 'No language images pulled'
+  puts 'Pulling a small starting collection of common language images'
+  pull('gcc_assert')
+  pull('ruby_mini_test')
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -62,7 +83,9 @@ def catalog
 end
 
 def in_catalog?(image)
-  catalog.split("\n").drop(0).map{ |line| line.split[2] }.include?(image)
+  all = catalog.split("\n").drop(1)
+  lines = all.map{ |line| line.split[-1] }
+  lines.include?(image)
 end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -94,21 +117,16 @@ end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-$docker_compose_file = 'docker-compose.yml'
-$docker_compose_cmd = "docker-compose --file=#{$my_dir}/#{$docker_compose_file}"
-
 def services
   `#{$docker_compose_cmd} config --services 2> /dev/null`.split
 end
 
 def upgrade
   services.each do |image|
-    cmd = "docker pull #{$docker_hub_username}/#{image}"
-    `#{cmd}`
+    run "docker pull #{$docker_hub_username}/#{image}"
   end
   pulled_images.each do |image|
-    cmd = "docker pull #{$docker_hub_username}/#{image}"
-    `#{cmd}`
+    run "docker pull #{$docker_hub_username}/#{image}"
   end
 end
 
@@ -127,7 +145,7 @@ end
 
 def pull(image)
   if in_catalog?(image)
-    `docker pull #{$docker_hub_username}/#{image}:latest`
+    run "docker pull #{$docker_hub_username}/#{image}:latest"
   else
     bad_image(image)
   end
@@ -135,15 +153,15 @@ end
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def rmi(image)
+def remove(image)
   if in_catalog?(image)
-    `docker rmi #{$docker_hub_username}/#{image}`
+    run "docker rmi #{$docker_hub_username}/#{image}"
   else
     bad_image(image)
   end
 end
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 if ARGV.length == 0
   puts 'no command entered'
@@ -151,12 +169,9 @@ if ARGV.length == 0
   exit
 end
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 options = {}
 arg = ARGV[0].to_sym
-case arg
-when :help, :down, :up, :images, :catalog, :upgrade, :pull, :rmi
+if [:help, :backup, :down, :up, :images, :catalog, :upgrade, :pull, :remove].include? arg
   options[arg] = true
 else
   puts "#{$me}: #{arg} ?"
@@ -165,101 +180,10 @@ else
 end
 
 help if options[:help]
+backup(ARGV[1]) if options[:backup]
 up if options[:up]
 puts catalog if options[:catalog]
 images if options[:images]
 upgrade if options[:upgrade]
 pull(ARGV[1]) if options[:pull]
-rmi(ARGV[1]) if options[:rmi]
-
-
-
-=begin
-
-    # - - - - - - - up options - - - - - - - -
-    katas=*)
-      KATAS="${ARG#*=}"
-      ;;
-    runner=*)
-      RUNNER="${ARG#*=}"
-      ;;
-
-#========================================================================================
-# katas data-container is decoupled from cyber-dojo script
-#========================================================================================
-
-KATAS_DATA_CONTAINER=cdf-katas-DATA-CONTAINER
-
-def create_empty_katas_data_container
-  #
-  # docker build \
-  #     --build-arg=CYBER_DOJO_KATAS_ROOT=${HOME}/katas \
-  #     --tag=${DOCKER_HUB_USERNAME}/katas \
-  #     --file=./app/docker/Dockerfile.katas-data-container.empty \
-  #     ${KATAS}
-  #
-  # rm ${KATAS}/Dockerfile
-  #
-  # docker run \
-  #     --name ${KATAS_DATA_CONTAINER} \
-  #     ${DOCKER_HUB_USERNAME}/katas \
-  #     echo 'cdfKatasDC'
-end
-
-# - - - - - - - - - - - - - - - - - - - - - - - - -
-
-def create_full_katas_data_container
-  #
-  # docker build \
-  #     --build-arg=CYBER_DOJO_KATAS_ROOT=${HOME}/katas \
-  #     --tag=${DOCKER_HUB_USERNAME}/katas \
-  #     --file=./app/docker/Dockerfile.katas-data-container.copied \
-  #     ${KATAS}
-  #
-  # rm ${KATAS}/Dockerfile
-  #
-  # docker run \
-  #     --name ${KATAS_DATA_CONTAINER} \
-  #     ${DOCKER_HUB_USERNAME}/katas \
-  #     echo 'cdfKatasDC'
-end
-
-# - - - - - - - - - - - - - - - - - - - - - - - - -
-
-def one_time_create_katas_data_container
-  if [ ! -d "${KATAS}" ]; then
-    if [ "${KATAS}" != "${KATAS_DEFAULT}" ]; then
-      echo "${ME}: katas=${KATAS} ? ${KATAS} directory does not exist"
-      echo "See ${ME} help"
-      exit
-    else # no dir at default location. Assume new server
-       create_empty_katas_data_container
-    fi
-  else # katas dir exists, copy into data-container
-     create_full_katas_data_container
-  fi
-end
-
-docker ps -a | grep ${katas_DATA_CONTAINER} > /dev/null
-if [ $? != 0 ]; then
-  one_time_create_katas_data_container
-fi
-
-#========================================================================================
-# languages
-#========================================================================================
-
-def pull_common_languages_if_none
-  CATALOG=$(catalog)
-  PULLED=$(pulled_language_images)
-  if [ -z "${PULLED}" ]; then
-    echo 'No language images pulled'
-    echo 'Pulling a small starting collection of common language images'
-    IMAGE=gcc_assert
-    pull
-    #IMAGE=ruby_mini_test
-    #pull
-  fi
-end
-
-=end
+remove(ARGV[1]) if options[:remove]
