250708e890161116de84a7a7743686a9d56fe22a
Olivier Lafleur <olivier.lafleur@gmail.com>
2016-03-27 15:09:29 -0400

    Change HikerTest file to more idiomatic Scala

--- app/languages/Scala/scalatest/hiker_tests.scala
+++ app/languages/Scala/scalatest/hiker_tests.scala
@@ -1,11 +1,7 @@
-
 import org.scalatest.FunSuite
 
 class HikerSuite extends FunSuite {
-
   test("the answer to life the universe and everything") {
-    val douglas = new Hiker
-    assert(douglas.answer() === (42))
+    assert(Hiker.answer == 42)
   }
-
 }
