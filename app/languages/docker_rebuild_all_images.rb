235b3891ad53636767b25269aa5d750e588c088d
Jon Jagger <jon@jaggersoft.com>
2016-03-30 16:44:54 +0100

    refactor script to rebuild all language images - test containers may not be based on their parents container so have to build all base language container before building any test container

--- app/languages/docker_rebuild_all_images.rb
+++ app/languages/docker_rebuild_all_images.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-cyber_dojo_root = File.expand_path('..', File.dirname(__FILE__))
+cyber_dojo_root = File.expand_path('../..', File.dirname(__FILE__))
 
 def docker_context_dir_name
   '_docker_context'
@@ -15,11 +15,19 @@ def print_and_run(command)
   `#{command}`
 end
 
-print_and_run build_docker_image_in("#{cyber_dojo_root}/languages/build-essential")
-# alpine_base
+print_and_run build_docker_image_in("#{cyber_dojo_root}/app/languages/build-essential")
+print_and_run build_docker_image_in("#{cyber_dojo_root}/app/languages/alpine-base")
 
-Dir.glob("#{cyber_dojo_root}/languages/*/").sort.each do |language_dir|
+# images in test folders are *not* always based on the image in their parent
+# language folder. When they are not the reason is usually
+# 1. to inherit common libraries
+# 2. to keep image sizes down
+
+Dir.glob("#{cyber_dojo_root}/app/languages/*/").sort.each do |language_dir|
   print_and_run(build_docker_image_in(language_dir))
+end
+
+Dir.glob("#{cyber_dojo_root}/app/languages/*/").sort.each do |language_dir|
   Dir.glob("#{language_dir}*/").sort.each do |test_dir|
     next if test_dir.end_with? "#{docker_context_dir_name}/"
     print_and_run(build_docker_image_in(test_dir))
