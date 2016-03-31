f647e9ad884373d3758b77cccfba0f0f54e33b74
Jon Jagger <jon@jaggersoft.com>
2016-03-31 14:38:40 +0100

    install-docker.sh now installs specific version of docker binary (1.10.3) to match version of docker binary inside the web image

--- app/docker/install-docker.sh
+++ app/docker/install-docker.sh
@@ -5,13 +5,14 @@ set -e
 # This script is part of the server installation instructions
 # described at http://blog.cyber-dojo.org/2016/03/running-your-own-cyber-dojo-server.html
 
-echo 'installing docker'
-curl -sSL https://get.docker.com/ | sh
+echo 'installing docker 1.10.3'
+curl -L https://get.docker.com/builds/Linux/x86_64/docker-1.10.3 > /usr/local/bin/docker
+chmod +x /usr/local/bin/docker
 
-echo 'installing docker-machine'
+echo 'installing docker-machine 0.6.0'
 curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
-echo 'installing docker-compose'
+echo 'installing docker-compose 1.6.2'
 curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
