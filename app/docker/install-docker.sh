b1c8e8db63615da4bf8ebf4213eb317635e1f5e0
Jon Jagger <jon@jaggersoft.com>
2016-04-17 12:23:42 +0100

    fixed web image's Dockerfile for docker 1.11.0

--- app/docker/install-docker.sh
+++ app/docker/install-docker.sh
@@ -12,6 +12,6 @@ echo 'installing docker-machine 0.6.0'
 curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
-echo 'installing docker-compose 1.6.2'
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+echo 'installing docker-compose 1.7.0'
+curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
