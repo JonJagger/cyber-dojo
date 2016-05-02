c3491c63fed382cca0eee5b53ef6aa01bf05a8fe
Jon Jagger <jon@jaggersoft.com>
2016-05-02 08:57:02 +0100

    deleted dead files

--- admin_scripts/pull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'pull.sh git-based installation is now obselete'
-echo 'please see http://blog.cyber-dojo.org/2016/03/running-your-own-cyber-dojo-server.html'
---
+++ admin_scripts/stats/readme.txt
@@ -0,0 +1,4 @@
+
+These scripts have not been run in quite a while
+and probably won't work without a small amount
+of attention.
