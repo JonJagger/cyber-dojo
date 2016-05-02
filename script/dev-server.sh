32f7f69ddc6e221450b2afe1dab4f4db11e6a7ea
Jon Jagger <jon@jaggersoft.com>
2016-05-02 10:08:34 +0100

    added readme for developers

--- script/dev-server.sh
+++ script/dev-server.sh
@@ -21,13 +21,9 @@ export CYBER_DOJO_DISK_CLASS=HostDisk
 export CYBER_DOJO_LOG_CLASS=StdoutLog
 export CYBER_DOJO_GIT_CLASS=HostGit
 
-# TODO: where should katas_root point to for a local server?
-#       HOME/katas saves to host disk
-#       /usr/src/cyber-dojo/katas  saves to data-container
 export CYBER_DOJO_KATAS_ROOT=${HOME}/tmp/katas
 export CYBER_DOJO_KATAS_CLASS=HostDiskKatas
 
-# TODO: On a local-server runner can be plain volume-mounting runner. No need for tar-pipes.
 export CYBER_DOJO_RUNNER_CLASS=${RUNNER}
 export CYBER_DOJO_RUNNER_SUDO=''
 export CYBER_DOJO_RUNNER_TIMEOUT=10
