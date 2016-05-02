548d9ef513ceedbd40afb7776a6df3a8364d583a
Jon Jagger <jon@jaggersoft.com>
2016-05-02 10:38:17 +0100

    modified install-docker script so it works if non root user sudo's it

--- app/docker/install-docker.sh
+++ app/docker/install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# These commands are copied directly from the docker website.
+# These commands are from the docker website.
 # This script is part of the server installation instructions
 # described at http://blog.cyber-dojo.org/2016/03/running-your-own-cyber-dojo-server.html
 
@@ -9,9 +9,11 @@ echo 'installing docker'
 curl -sSL https://get.docker.com/ | sh
 
 echo 'installing docker-machine 0.6.0'
-curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine
+curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > docker-machine
+mv docker-machine /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
 echo 'installing docker-compose 1.7.0'
-curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+mv docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
